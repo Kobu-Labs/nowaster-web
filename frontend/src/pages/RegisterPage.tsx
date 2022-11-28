b315fd27152887824824f260246a5588c6eca1f2
Kobu <thekobu@gmail.com>
2022-11-28 19:52:56 +0200

    feat: add login and register pages
    ## What was implemented ?
    login and register pages
    
    ## How was it implemented ?
    using react, tailwind, typescript
    
    Closes #21

---
+++ frontend/src/pages/RegisterPage.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+import RegisterForm from "../components/RegisterForm";
+
+export const RegisterPage: FC = () => {
+  return (
+    <div>
+      <p> RegisterPage </p>
+      <RegisterForm></RegisterForm>
+    </div>
+  );
+};
