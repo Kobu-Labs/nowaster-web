e389dc69b51e350a2839dfc771df1882e175be17
Kobu <thekobu@gmail.com>
2022-11-30 19:52:56 +0100

    feat: add logout page
    ## What was implemented ?
    logout page
    
    ## How was it implemented ?
    using react, tailwind, typescript
    
    Closes #22

--- frontend/src/pages/LogoutPage.tsx
+++ frontend/src/pages/LogoutPage.tsx
@@ -1,9 +1,11 @@
 import { FC } from "react";
+import Logout from "../components/Logout";
 
 export const LogoutPage: FC = () => {
   return (
     <div>
       <p> LogoutPage </p>
+      <Logout></Logout>
     </div>
   );
 };
