3591b90c9a6a74fbba792af8d5285aaae26a9fda
Kobu <thekobu@gmail.com>
2022-12-05 19:52:56 +0200

    refactor: introduce better controller validation flow
    ## What was implemented ?
    refactor code to be more concise and readable
    
    ## How was it implemented ?
    nextjs middleware with zod validation

---
+++ backend/src/controllers/middleware/responseUtil.ts
@@ -0,0 +1,34 @@
+import type { Response } from "express";
+import type z from "zod";
+import { UserVisibleError } from "../../repositories/types";
+
+export function handleErrorResp(status: number, res: Response, msg: string): Response {
+  return res.status(status).send({
+    status: "error",
+    data: {},
+    message: msg,
+  });
+}
+
+export function handleResultErrorResp(status: number, res: Response, err: Error): Response {
+  return res.status(status).send({
+    status: "error",
+    data: {},
+    message: err instanceof UserVisibleError ? err.message : "Something went wrong"
+  });
+}
+
+export function handleOkResp<TData>(data: TData, res: Response, msg?: string): Response {
+  return res.send({
+    status: "success",
+    data: data,
+    message: msg,
+  });
+}
+
+export function handleValidationErrorResp(error: z.ZodError, res: Response): Response {
+  return res.status(400).send({
+    status: "error",
+    message: `Validation error: ${error.issues.map(issue => issue.message).join(";")}`,
+  });
+}
---
+++ backend/src/controllers/middleware/validation.ts
@@ -0,0 +1,33 @@
+import type { RequestHandler } from "express";
+import { ZodError, ZodSchema } from "zod";
+import { handleErrorResp, handleValidationErrorResp } from "./responseUtil";
+
+type Validations<TParams, TBody, TQuery> = {
+    params?: ZodSchema<TParams>;
+    body?: ZodSchema<TBody>;
+    query?: ZodSchema<TQuery>;
+};
+
+export const validate =
+    <TParams = unknown, TBody = unknown, TQuery = unknown, TAny=unknown>(
+    validation: Validations<TParams, TBody, TQuery>
+  ): RequestHandler<TParams, TAny, TBody, TQuery> =>
+    async (req, res, next) => {
+      try {
+        if (validation.body) {
+          req.body = validation.body.parse(req.body);
+        }
+        if (validation.params) {
+          req.params = validation.params.parse(req.params);
+        }
+        if (validation.query) {
+          req.query = validation.query.parse(req.query);
+        }
+        return next();
+      } catch (error) {
+        if (error instanceof ZodError) {
+          return handleValidationErrorResp(error, res);
+        }
+        return handleErrorResp(500, res, `Unknown error ${error}`);
+      }
+    };
