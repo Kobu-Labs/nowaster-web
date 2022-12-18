7da2fde57d30c1ebd30ba33ba45124d6f7652f70
Kobu <thekobu@gmail.com>
2022-12-18 19:52:56 +0100

    fix: inconsitent styling between register and login form
    ## What was implemented ?
    fixed registration form styles to be consistent with login form
    
    ## How was it implemented ?
    react, tailwind, typescript
    
    Closes #30

--- frontend/src/components/AvatarPicker.tsx
+++ frontend/src/components/AvatarPicker.tsx
@@ -1,25 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const IMAGE_PATHS = [
-  '/avatars/av1.svg',
-  '/avatars/av3.svg',
-  '/avatars/av4.svg',
-  '/avatars/av5.svg',
-  '/avatars/av6.svg',
-  '/avatars/av7.svg',
-  '/avatars/av8.svg',
-  '/avatars/av9.svg',
-  '/avatars/av10.svg',
-  '/avatars/av12.svg',
-  '/avatars/av13.svg',
-  '/avatars/av14.svg',
-  '/avatars/av15.svg',
-  '/avatars/av16.svg'
+  "/avatars/av1.svg",
+  "/avatars/av3.svg",
+  "/avatars/av4.svg",
+  "/avatars/av5.svg",
+  "/avatars/av6.svg",
+  "/avatars/av7.svg",
+  "/avatars/av8.svg",
+  "/avatars/av9.svg",
+  "/avatars/av10.svg",
+  "/avatars/av12.svg",
+  "/avatars/av13.svg",
+  "/avatars/av14.svg",
+  "/avatars/av15.svg",
+  "/avatars/av16.svg",
 ];
 
 interface AvatarPickerProps {
-    onAvatarSelect: (selectedAvatar: string) => void;
-  }
+  onAvatarSelect: (selectedAvatar: string) => void;
+}
 
 const AvatarPicker: React.FC<AvatarPickerProps> = ({ onAvatarSelect }) => {
   const [selectedImage, setSelectedImage] = useState<string>(IMAGE_PATHS[0]);
@@ -31,7 +31,9 @@ const AvatarPicker: React.FC<AvatarPickerProps> = ({ onAvatarSelect }) => {
   };
 
   const handleNextClick = () => {
-    setStart((prevStart) => (prevStart < IMAGE_PATHS.length - 3 ? prevStart + 1 : prevStart));
+    setStart((prevStart) =>
+      prevStart < IMAGE_PATHS.length - 3 ? prevStart + 1 : prevStart
+    );
   };
 
   const handlePrevClick = () => {
@@ -40,9 +42,15 @@ const AvatarPicker: React.FC<AvatarPickerProps> = ({ onAvatarSelect }) => {
 
   return (
     <div>
-      <label className="text-indigo-500">Pick your avatar:</label> 
-      <div className='flex items-center justify-center'>  
-        <button className='bg-indigo-500 hover:bg-indigo-600 text-white font-semibold h-12' onClick={handlePrevClick} type='button'>&#60;-</button>
+      <label>Pick your avatar:</label>
+      <div className="flex items-center justify-center">
+        <button
+          className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg"
+          onClick={handlePrevClick}
+          type="button"
+        >
+          &#60;-
+        </button>
         {IMAGE_PATHS.slice(start, start + 3).map((imagePath, index) => (
           <img
             key={index}
@@ -50,15 +58,24 @@ const AvatarPicker: React.FC<AvatarPickerProps> = ({ onAvatarSelect }) => {
             alt={`Option ${index + 1}`}
             onClick={() => handleImageSelect(imagePath)}
             style={{
-              cursor: 'pointer',
-              width: '75px',
-              height: 'auto',
-              margin: '10px',
-              outline: selectedImage === imagePath ? '2px solid blue' : 'none',
+              cursor: "pointer",
+              width: "75px",
+              height: "auto",
+              margin: "10px",
+              padding: "5px",
+              borderRadius: "0.5rem",
+              outline:
+                selectedImage === imagePath ? "2px solid #2563eb" : "none",
             }}
           />
         ))}
-        <button className='bg-indigo-500 hover:bg-indigo-600 text-white font-semibold h-12' onClick={handleNextClick} type='button'>-&#62;</button>
+        <button
+          className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg"
+          onClick={handleNextClick}
+          type="button"
+        >
+          -&#62;
+        </button>
       </div>
     </div>
   );
