7d9b3da6cc9f35d3d9ac23a0a69eb715a3684bc3
Kobu <thekobu@gmail.com>
2022-12-18 19:52:56 +0200

    fix: username validation
    ## What was implemented ?
    username is now validated on frontend that it contains only alphanumerical characters
    
    ## How was it implemented ?
    react, typescript
    
    Closes #33

--- frontend/src/validation/registrationSubmit.ts
+++ frontend/src/validation/registrationSubmit.ts
@@ -2,13 +2,16 @@ import { z } from "zod";
 
 export const registrationSchema = z.object({
   email: z.string().email({ message: "Invalid email address" }),
-  username: z.string().min(3, "Username must be at least 3 characters"),
+  username: z
+    .string()
+    .min(3, "Username must be at least 3 characters")
+    .regex(/^\w+$/, "Username must contain only alphanumeric characters"),
   password: z.string().min(6, "Password must be at least 6 characters"),
 });
 
 export type UserLoginSubmit = {
-  email: string,
-  password: string,
-}
+  email: string;
+  password: string;
+};
 
 export type UserRegistrationSubmit = z.infer<typeof registrationSchema>;
