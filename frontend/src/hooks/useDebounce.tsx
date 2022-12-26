44b1e1d22a47da7757f93380ba94d5f168671887
Kobu <thekobu@gmail.com>
2022-12-26 19:52:56 +0100

    feat: create generic search component
    ## What was implemented ?
    generic search component
    
    ## How was it implemented ?
    allows to pass search function and function to handle returned data
    
    Closes #41

---
+++ frontend/src/hooks/useDebounce.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+const useDebounce = (value: any, delay: number) => {
+    const [debouncedValue, setDebouncedValue] = useState(value)
+
+    useEffect(() => {
+        const id = setTimeout(() => {
+            setDebouncedValue(value)
+        }, delay)
+
+        return () => {
+            clearTimeout(id)
+        }
+
+    }, [value, delay])
+
+    return debouncedValue
+}
+
+export default useDebounce;
