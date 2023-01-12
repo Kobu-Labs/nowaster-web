3f9b7103d07e15c452f676d2c1f7dcc669b12049
Kobu <thekobu@gmail.com>
2023-01-12 19:52:56 +0200

    feat: initialize frontend with nextjs and shadcn

---
+++ next-frontend/.storybook/main.ts
@@ -0,0 +1,26 @@
+import type { StorybookConfig } from "@storybook/nextjs"
+
+const config: StorybookConfig = {
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-styling",
+      options: {
+        postcss: {
+          implementation: require.resolve("postcss"),
+        },
+      },
+    },
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+}
+export default config
