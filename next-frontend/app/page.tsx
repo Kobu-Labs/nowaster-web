4cc538ab57ef12cb97810a536895bc1909b94a99
Kobu <thekobu@gmail.com>
2023-01-15 19:52:56 +0200

    feat: initialize shadcn basic components

--- next-frontend/app/page.tsx
+++ next-frontend/app/page.tsx
@@ -1,20 +1,7 @@
 "use client"
-import Link from "next/link"
-
-import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-
 
 export default function IndexPage() {
   return (
-    <Tabs defaultValue="account" className="w-[400px]">
-      <TabsList>
-        <TabsTrigger value="account">Account</TabsTrigger>
-        <TabsTrigger value="password">Password</TabsTrigger>
-      </TabsList>
-      <TabsContent value="account">Make changes to your account here.</TabsContent>
-      <TabsContent value="password">Change your password here.</TabsContent>
-    </Tabs>
+    <span>Hello, world!</span>
   )
 }
