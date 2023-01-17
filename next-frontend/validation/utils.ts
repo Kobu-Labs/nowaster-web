c37dde90b967688ad1167ce76364409b51b0774c
Kobu <thekobu@gmail.com>
2023-01-17 19:52:57 +0100

    feat: add RegisterForm component

---
+++ next-frontend/validation/utils.ts
@@ -0,0 +1,6 @@
+import { z } from "zod";
+
+export const userNameSchema = z
+  .string()
+  .min(3, "Username must be at least 3 characters")
+  .regex(/^\w+$/, "Username must contain only alphanumeric characters")
