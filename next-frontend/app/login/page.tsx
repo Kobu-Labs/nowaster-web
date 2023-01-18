d842248e59dbb882ad44fecb89392e07d0e00066
Kobu <thekobu@gmail.com>
2023-01-18 19:52:57 +0100

    feat: implement simple login page

--- next-frontend/app/login/page.tsx
+++ next-frontend/app/login/page.tsx
@@ -1,9 +1,24 @@
 "use client"
 
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { LoginForm } from "@/stories/LoginForm/LoginForm"
+import { RegisterForm } from "@/stories/RegisterForm/RegisterForm"
 
 export default function LoginPage() {
   return (
-    <LoginForm></LoginForm>
+    <div className="flex items-center justify-center mt-24">
+      <Tabs defaultValue="login" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="register">Register</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">
+          <LoginForm></LoginForm>
+        </TabsContent>
+        <TabsContent value="register">
+          <RegisterForm />
+        </TabsContent>
+      </Tabs>
+    </div>
   )
 }
