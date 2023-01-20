f0be26ab3fb8baa4fda70c84f1340df4a0f51b0d
Kobu <thekobu@gmail.com>
2023-01-20 19:52:57 +0100

    feat: implement HistoryCard and a subject tag components

---
+++ next-frontend/stories/HistoryCard/HistoryCard.stories.tsx
@@ -0,0 +1,60 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { HistoryCard } from './HistoryCard';
+
+const meta = {
+  title: 'HistoryCard',
+  component: HistoryCard,
+  tags: ['autodocs'],
+} satisfies Meta<typeof HistoryCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+
+export const TwoHours: Story = {
+  args: {
+    userId: "19i42894294",
+    tags: ["school", "pb138", "pb138/project", "focus"],
+    startTime: new Date(2023, 5, 27, 19, 0),
+    endTime: new Date(2023, 5, 27, 21, 0),
+    category: "pb138",
+    description: "Working on the project",
+  }
+}
+
+export const NinetyMinutes: Story = {
+  args: {
+    userId: "19i42894294",
+    tags: ["school", "pb138", "pb138/project", "focus"],
+    startTime: new Date(2023, 5, 27, 19, 0),
+    endTime: new Date(2023, 5, 27, 20, 30),
+    category: "pb138",
+    description: "Working on the project",
+  }
+}
+
+
+export const HalfHour: Story = {
+  args: {
+    userId: "19i42894294",
+    tags: ["school", "pb138", "pb138/project", "focus"],
+    startTime: new Date(2023, 5, 27, 20, 0),
+    endTime: new Date(2023, 5, 27, 20, 30),
+    category: "pb138",
+    description: "Working on the project",
+  }
+}
+
+
+export const NineMinutesNineSeconds: Story = {
+  args: {
+    userId: "19i42894294",
+    tags: ["school", "pb138", "pb138/project", "focus"],
+    startTime: new Date(2023, 5, 27, 20, 0, 0),
+    endTime: new Date(2023, 5, 27, 20, 9, 9),
+    category: "pb138",
+    description: "Working on the project",
+  }
+}
+
+
---
+++ next-frontend/stories/SessionTag/SessionTag.tsx
@@ -0,0 +1,33 @@
+import { Badge } from "@/components/ui/badge"
+import { FC } from "react"
+
+type SessionTagProps = {
+  color?: keyof typeof colors
+  value: string
+}
+
+const colors = {
+  "blue": "bg-sky-500",
+  "red": "bg-rose-400",
+  "violet": "bg-violet-600",
+  "amber": "bg-amber-600",
+  "green": "bg-green-400",
+  "emerald": "bg-emerald-500",
+  "cyan": "bg-cyan-400",
+  "fuchsia": "bg-fuchsia-400",
+}
+
+const getColor = (color: keyof typeof colors | undefined): string => {
+  if (color === undefined) {
+    const values = Object.values(colors);
+    return values[values.length * Math.random() << 0];
+  }
+
+  return colors[color]
+}
+
+export const SessionTag: FC<SessionTagProps> = (props) => {
+  return (
+    <Badge className={"text-white " + getColor(props.color)}>{props.value}</Badge>
+  )
+}
