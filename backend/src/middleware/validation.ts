205d477ee7a52d6dd15e18b69910e2162b9255ff
Kobu <thekobu@gmail.com>
2023-01-22 19:52:57 +0200

    fix: create utils folder

--- backend/src/middleware/validation.ts
+++ backend/src/middleware/validation.ts
@@ -1,6 +1,6 @@
 import type { RequestHandler } from "express";
 import { ZodError, ZodSchema } from "zod";
-import { handleErrorResp, handleValidationErrorResp } from "./responseUtil";
+import { handleErrorResp, handleValidationErrorResp } from "../controllers/utils/handleResponse";
 
 type Validations<TParams, TBody, TQuery> = {
     params?: ZodSchema<TParams>;
