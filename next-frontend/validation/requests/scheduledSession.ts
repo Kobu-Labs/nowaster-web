30045b2054b9c7cbec6f7caf0d2db33d90db1435
Kobu <thekobu@gmail.com>
2023-02-02 19:52:57 +0200

    feat: implement basic zod validation for scheduled session

---
+++ next-frontend/validation/requests/scheduledSession.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+
+export const createScheduledSchema = z.object({
+  category: z.string().nonempty(),
+  description: z.string()
+    .max(50)
+    .nullable(),
+  startTime: z.coerce.date(),
+  endTime: z.coerce.date()
+});
+
+export const readByUserScheduledSchema = z.object({
+  userId: z.string().uuid(),
+});
+
+export const readManyScheduledSchema = z.object({
+    limit : z.coerce.number().optional()
+})
+
+export const updateScheduledSchema = z.object({
+  id: z.string().uuid(),
+  category: z.string().nonempty().optional(),
+  description: z.string()
+    .max(50)
+    .optional(),
+  startTime: z.coerce.date().optional(),
+  endTime: z.coerce.date().optional()
+});
+
+
+export const deleteScheduledSchema = z.object({
+  id: z.string().uuid(),
+});
