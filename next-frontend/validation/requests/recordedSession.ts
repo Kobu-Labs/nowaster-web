5c4a6dd7af09c72e2bac5b41b0d94c9bf0b7d3bf
Kobu <thekobu@gmail.com>
2023-02-03 19:52:57 +0100

    feat: implement basic zod validation for recorded session

---
+++ next-frontend/validation/requests/recordedSession.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+
+
+export const createRecordedSchema = z.object({
+  category: z.string().nonempty(),
+  description: z.string()
+    .max(50)
+    .nullable(),
+  startTime: z.coerce.date()
+});
+
+export const readManyRecordedSchema = z.object({
+  limit: z.coerce.number().optional()
+})
+
+export const readByIdRecordedSchema = z.object({
+  id: z.string().uuid(),
+});
+
+export const updateRecordedSchema = z.object({
+  id: z.string().uuid(),
+  category: z.string().nonempty().optional(),
+  description: z.string()
+    .max(50)
+    .optional(),
+  startTime: z.coerce.date().optional()
+});
+
+export const deleteRecordedSchema = z.object({
+  id: z.string().uuid(),
+});
