1719c55257c7c2ef6c963393791b880e4e01fdef
Kobu <thekobu@gmail.com>
2023-07-06 13:11:09 +0200

    feat: add tags into scheduled entity creation

--- backend/src/repositories/scheduled_entity/create.ts
+++ backend/src/repositories/scheduled_entity/create.ts
@@ -3,12 +3,36 @@ import type { ScheduledEntity } from "@prisma/client";
 import client from "../client";
 import { Result } from "@badrap/result";
 
-type CreateScheduledParams = Omit<ScheduledEntity, "id">
+type CreateScheduledParams = Omit<ScheduledEntity, "id"> & { tags: string[] }
 
-
+// tags passed in must already exists
 const create = async (params: CreateScheduledParams): AsyncResult<ScheduledEntity> => {
   try {
-    const scheduledEntity = await client.scheduledEntity.create({ data: params });
+    const tags = await client.tag.findMany({
+      where: {
+        label: {
+          in: params.tags
+        }
+      }
+    });
+
+    const preprocessedTags = tags.map((t) => {
+      return { tagId: t.id };
+    });
+
+
+    const scheduledEntity = await client.scheduledEntity.create({
+      data: {
+        tags: {
+          create: preprocessedTags
+        },
+        startTime: params.startTime,
+        endTime: params.endTime,
+        description: params.description,
+        category: params.category
+      },
+    });
+
     return Result.ok(scheduledEntity);
   } catch (error) {
     return Result.err(error as Error);
