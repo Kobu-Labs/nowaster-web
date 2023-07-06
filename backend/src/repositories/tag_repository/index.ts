898c7da74ae1bc07025f4dde7b1e48ed12664bf7
Kobu <thekobu@gmail.com>
2023-07-06 13:08:23 +0200

    feat: finilize Tag repository

---
+++ backend/src/repositories/tag_repository/index.ts
@@ -0,0 +1,14 @@
+import create from "./create";
+import read from "./read";
+import remove from "./remove";
+import update from "./update";
+
+const tagRepo = {
+  create,
+  read,
+  remove,
+  update
+};
+
+export default tagRepo
+
