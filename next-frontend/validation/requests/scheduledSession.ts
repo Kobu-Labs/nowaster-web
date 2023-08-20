df25ff4b4e8cad6edbeb0c3f18e160d9d9654d68
Kobu <thekobu@gmail.com>
2023-08-20 19:17:47 +0200

    feat: add tags field to scheduled session creation request

--- next-frontend/validation/requests/scheduledSession.ts
+++ next-frontend/validation/requests/scheduledSession.ts
@@ -1,16 +1,17 @@
 import { z } from "zod";
 
 export const createScheduledSchema = z.object({
-  category: z.string().nonempty(),
+  category: z.string().nonempty({ message: "category cannot be empty" }),
   description: z.string()
     .max(50)
-    .nullable(),
+    .optional(),
   startTime: z.coerce.date(),
-  endTime: z.coerce.date()
+  endTime: z.coerce.date(),
+  tags: z.array(z.string()),
 });
 
 export const readByUserScheduledSchema = z.object({
-    limit : z.coerce.number().optional()
+  limit: z.coerce.number().optional()
 });
 
 export const updateScheduledSchema = z.object({
