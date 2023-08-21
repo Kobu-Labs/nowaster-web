3a8e5d86e746d1060aad848c8a9303402484a5a2
Kobu <thekobu@gmail.com>
2023-08-21 19:17:48 +0200

    feat: add HasId schema

--- next-frontend/validation/utils.ts
+++ next-frontend/validation/utils.ts
@@ -4,3 +4,5 @@ export const userNameSchema = z
   .string()
   .min(3, "Username must be at least 3 characters")
   .regex(/^\w+$/, "Username must contain only alphanumeric characters")
+
+export const HasID = z.object({ id: z.string() });
