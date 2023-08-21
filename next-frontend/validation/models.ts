edf7091f9fde0066ef38361fbe640d9579719717
Kobu <thekobu@gmail.com>
2023-08-21 19:17:49 +0200

    fix: delete optional from start and end dates

--- next-frontend/validation/models.ts
+++ next-frontend/validation/models.ts
@@ -1,18 +1,27 @@
 import { z } from "zod";
+import { HasID } from "./utils";
+
+export const TagSchema = z.object({
+  label: z.string()
+})
+
+export type Tag = z.infer<typeof TagSchema>
 
 export const RecordedSessionSchema = z.object({
-  startTime: z.coerce.date().optional(),
+  startTime: z.coerce.date(),
   category: z.string(),
-  description: z.string().optional()
+  description: z.string().nullable()
 });
 export type RecordedEntity = z.infer<typeof RecordedSessionSchema>;
 
 
 export const ScheduledSessionSchema = z.object({
-  startTime: z.coerce.date().optional(),
-  endTime: z.coerce.date().optional(),
+  startTime: z.coerce.date(),
+  endTime: z.coerce.date(),
   category: z.string(),
-  description: z.string().optional(),
-  tags: z.array(z.string()),
+  description: z.string().nullable(),
+  tags: z.array(TagSchema.merge(HasID)),
 });
 export type ScheduledSession = z.infer<typeof ScheduledSessionSchema>;
+
+
