6a59831a56f30a7983704d1fa7ef904ad7715fab
Kobu <thekobu@gmail.com>
2023-08-21 19:17:48 +0200

    feat: update schema declaration for recorded session

--- next-frontend/models/RecordedSession.ts
+++
@@ -1,6 +0,0 @@
-export type RecordedEntity = {
-  id: string
-  startTime: Date
-  category: string
-  description: string | null
-}
--- next-frontend/models/ScheduledSession.ts
+++
@@ -1,8 +0,0 @@
-export type ScheduledEntity = {
-  id: string
-  startTime: Date
-  endTime: Date
-  category: string
-  description: string | null
-}
-
