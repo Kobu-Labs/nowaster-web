070eb3e871fdff08857d17e1e74f4893d8c9c708
Kobu <thekobu@gmail.com>
2023-08-31 17:14:56 +0200

    feat: implement streak api call on frontend

--- next-frontend/api/statisticsApi.ts
+++ next-frontend/api/statisticsApi.ts
@@ -1,9 +1,16 @@
-import { GetDashboardStatsResponse, getDashboardStatsResponseSchema } from "@/validation/responses/statistics";
+import { GetDashboardStatsResponse, getDashboardStatsResponseSchema, GetStreakDataResponse, getStreakDataResponseSchema } from "@/validation/responses/statistics";
 import { Result } from "@badrap/result";
 import baseApi, { handleResponse } from "./baseApi";
 
-export const getData = async (): Promise<Result<GetDashboardStatsResponse>> => {
+export const getDashboardData = async (): Promise<Result<GetDashboardStatsResponse>> => {
   const { data } = await baseApi.get("statistics/dashboard");
   return await handleResponse(data, getDashboardStatsResponseSchema)
 };
+
+
+export const getStreakData = async (): Promise<Result<GetStreakDataResponse>> => {
+  const { data } = await baseApi.get("statistics/streak");
+  return await handleResponse(data, getStreakDataResponseSchema)
+};
+
 
--- next-frontend/validation/responses/statistics.ts
+++ next-frontend/validation/responses/statistics.ts
@@ -1,6 +1,9 @@
 import { z } from "zod";
 
 export type GetDashboardStatsResponse = z.infer<typeof getDashboardStatsResponseSchema>;
+export type GetStreakDataResponse = z.infer<typeof getStreakDataResponseSchema>;
+
+export const getStreakDataResponseSchema = z.array(z.coerce.date())
 
 export const getDashboardStatsResponseSchema = z.object({
   streak: z.number(),
