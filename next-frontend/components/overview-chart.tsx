ea8fabf3c6783347420135203d4157020c7f16cb
Kobu <thekobu@gmail.com>
2023-09-01 19:12:16 +0200

    feat: implement functioning overview chart

--- next-frontend/components/overview-chart.tsx
+++ next-frontend/components/overview-chart.tsx
@@ -3,19 +3,68 @@
 import { ScheduledSessionApi } from "@/api"
 import { ScheduledSession } from "@/validation/models"
 import { useQuery } from "@tanstack/react-query"
-import { useState } from "react"
+import { addDays, addMonths, addSeconds, differenceInMinutes, format, getDate, getDay, getDaysInMonth, getMonth, startOfMonth, startOfWeek, startOfYear } from "date-fns"
 import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts"
 
-type Granularity = "today" | "week" | "month" | "year"
-type OverviewProps = {
-  granularity: Granularity
+export type Granularity = "week" | "month" | "year"
+
+const dateProcessors: {
+  [K in Granularity]: {
+    amount: () => number,
+    key: (value: Date) => string,
+    start: Date,
+    next: (value: Date) => Date,
+  }
+} = {
+  week: {
+    start: startOfWeek(Date.now()),
+    next: value => addDays(value, 1),
+    amount: () => 7,
+    key: (value: Date) => (1 + getDay(value)).toString()
+  },
+  month: {
+    start: startOfMonth(Date.now()),
+    next: value => addDays(value, 1),
+    amount: () => getDaysInMonth(Date.now()),
+    key: (value: Date) => getDate(value).toString()
+  },
+  year: {
+    start: startOfYear(Date.now()),
+    next: value => addMonths(value, 1),
+    amount: () => 12,
+    key: (value: Date) => (getMonth(value) + 1).toString()
+  }
 }
 
-const preprocessData = (data: (ScheduledSession & { id: string })[]): { month: string, minutes: number }[] => {
-  return data.map(e => {
-    const month = e.endTime.toLocaleString("default", { month: "short" })
-    const minutes = (e.endTime.getTime() - e.startTime.getTime()) / 60 / 60
-    return { month, minutes }
+type OverviewProps = {
+  granularity: Granularity,
+}
+
+const preprocessData = (granularity: Granularity, data: (ScheduledSession & { id: string })[]): { granularity: string, val: number }[] => {
+  const processor = dateProcessors[granularity]
+  let processed = data.reduce((value: { [month: string]: number }, item) => {
+    const key = processor.key(item.endTime)
+    if (!value[key]) {
+      value[key] = 0
+    }
+
+    value[key] += differenceInMinutes(item.endTime, item.startTime)
+    return value
+  }, {})
+
+
+  let i = 0;
+  let current = processor.start
+  while (i < processor.amount()) {
+    i++;
+    if (!processed[processor.key(current)]) {
+      processed[processor.key(current)] = 0
+    }
+    current = processor.next(current)
+  }
+
+  return Object.entries(processed).map(value => {
+    return { granularity: value[0], val: value[1] }
   })
 }
 
@@ -26,8 +75,6 @@ export function Overview(props: OverviewProps) {
     queryFn: async () => await ScheduledSessionApi.getSessions(),
   });
 
-  const [granularity, setGranularity] = useState<Granularity>("year")
-
   if (isLoading || isError) {
     return <div >Something bad happenned</div>
   }
@@ -37,9 +84,9 @@ export function Overview(props: OverviewProps) {
   }
   return (
     <ResponsiveContainer width="100%" height={500}>
-      <BarChart data={preprocessData(data.value)}>
+      <BarChart data={preprocessData(props.granularity, data.value)}>
         <XAxis
-          dataKey="month"
+          dataKey="granularity"
           stroke="#888888"
           fontSize={12}
           tickLine={false}
@@ -50,9 +97,9 @@ export function Overview(props: OverviewProps) {
           fontSize={12}
           tickLine={false}
           axisLine={false}
-          tickFormatter={(value) => `$${value}`}
+          tickFormatter={(value: number) => format(addSeconds(new Date(0), value * 60), "hh:mm:ss")}
         />
-        <Bar dataKey="minutes" fill="#e879f9" radius={[4, 4, 0, 0]} />
+        <Bar dataKey="val" fill="#e879f9" radius={[4, 4, 0, 0]} />
       </BarChart>
     </ResponsiveContainer>
   )
