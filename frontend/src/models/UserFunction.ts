90ec8f836a912ceb06610d6ba2c11c4c4e450501
Kobu <thekobu@gmail.com>
2023-09-13 18:50:08 +0200

    fix: delete legacy frontend code

--- frontend/src/models/UserFunction.ts
+++
@@ -1,5 +0,0 @@
-export type UserFunction = {
-  id: string
-  description: string
-  name: string
-}
--- frontend/src/validation/loginSubmit.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-
-export const loginSchema = z.object({
-  email: z.string().email({ message: "Invalid email address" }),
-  password: z.string(),
-});
