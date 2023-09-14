1875cf5571433809502ef9c49c62f52cda5b438a
Kobu <thekobu@gmail.com>
2023-09-14 17:36:55 +0200

    feat: add ability to call tag creation API

--- next-frontend/validation/responses/tags.ts
+++ next-frontend/validation/responses/tags.ts
@@ -4,5 +4,7 @@ import { HasID } from "../utils";
 
 
 export type ReadManyTagsResponse = z.infer<typeof readManyTagsResponseSchema>;
+export type CreateTagResponse = z.infer<typeof createTagResponseSchema>;
 
 export const readManyTagsResponseSchema = z.array(TagSchema.merge(HasID))
+export const createTagResponseSchema = TagSchema.merge(HasID)
