be75eb04ed998a383595db7a3b3fbf456b88d99f
Kobu <thekobu@gmail.com>
2023-09-14 17:38:25 +0200

    feat: allow for creation of tags from TagPicker

--- next-frontend/stories/TagPicker/TagPicker.tsx
+++ next-frontend/stories/TagPicker/TagPicker.tsx
@@ -1,15 +1,20 @@
 import { TagApi } from "@/api"
 import { Button } from "@/components/ui/button"
-import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem } from "@/components/ui/command"
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandSeparator } from "@/components/ui/command"
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { prefixBasedMatch } from "@/lib/searching"
 import { cn } from "@/lib/utils"
 import { Tag } from "@/validation/models"
-import { useQuery } from "@tanstack/react-query"
+import { CreateTagRequest } from "@/validation/requests/tags"
+import { CreateTagResponse } from "@/validation/responses/tags"
+import { Result } from "@badrap/result"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import { Check, ChevronsUpDown } from "lucide-react"
 import { FC, useState } from "react"
 import { SessionTag } from "../SessionTag/SessionTag"
 
-type TagWithId = Tag & {id: string}
+type TagWithId = Tag & { id: string }
 
 type TagPickerProps = {
   onTagSelected: (tag: TagWithId[]) => void
@@ -19,22 +24,55 @@ export const TagPicker: FC<TagPickerProps> = (props) => {
   const [isOpen, setIsOpen] = useState<boolean>(false)
   const [selectedTags, setSelectedTags] = useState<TagWithId[]>([])
   const [searchTerm, setSearchTerm] = useState<string>("")
-  const { data, isLoading, isError } = useQuery({
+
+  const { data: tags, isLoading, isError } = useQuery({
     queryKey: ["tags"],
     retry: false,
     queryFn: async () => await TagApi.readMany(),
   });
 
-  if (isLoading || isError ) {
+  const queryClient = useQueryClient()
+  const { mutate: createTag } = useMutation<Result<CreateTagResponse>, unknown, CreateTagRequest>({
+
+    mutationFn: async (params) => await TagApi.create(params),
+    retry: false,
+    onSuccess: (tagResult) => {
+      if (tagResult.isErr) {
+        return;
+      }
+
+      queryClient.invalidateQueries({ queryKey: ["tags"] })
+      handleSelectTag(tagResult.value)
+    }
+  })
+
+  if (isLoading || isError) {
     return <div >Something bad happenned</div>
   }
 
-  if (data.isErr){
-    return <div>{data.error.message}</div>
+  if (tags.isErr) {
+    return <div>{tags.error.message}</div>
+  }
+
+  const handleDeselectTag = (tag: TagWithId) => {
+    const newTags = selectedTags.filter(t => t.id !== tag.id)
+    setSelectedTags(newTags)
+    props.onTagSelected(newTags)
+
+  }
+  const handleSelectTag = (tag: TagWithId) => {
+    const newTags = [tag, ...selectedTags]
+    setSelectedTags(newTags)
+    props.onTagSelected(newTags)
   }
 
   return (
-    <Popover open={isOpen} onOpenChange={setIsOpen}>
+    <Popover open={isOpen} onOpenChange={(shouldOpen) => {
+      if (!shouldOpen) {
+        setSearchTerm("")
+      }
+      setIsOpen(shouldOpen)
+    }}>
       <PopoverTrigger asChild >
         <Button
           variant="outline"
@@ -50,38 +88,38 @@ export const TagPicker: FC<TagPickerProps> = (props) => {
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-[200px] p-0">
-        <Command>
+        <Command shouldFilter={false}>
           <CommandInput onValueChange={setSearchTerm} placeholder="Search tags" />
-          <CommandEmpty>
-            <button onClick={() => console.log("Creating " + searchTerm)}>
-              {"Create '" + searchTerm + "'"}
-            </button>
-          </CommandEmpty>
-          <CommandGroup>
-            {data.value.map((tag) => (
+          {searchTerm && tags.value.every(t => t.label !== searchTerm) &&
+            <CommandGroup>
               <CommandItem
-                key={tag.id}
-                onSelect={() => {
-                  if (selectedTags.indexOf(tag) === -1) {
-                    const newArray = [tag, ...selectedTags]
-                    setSelectedTags(newArray)
-                    props.onTagSelected(newArray)
-                  } else {
-                    const newArray = selectedTags.filter(t => t !== tag)
-                    setSelectedTags(newArray)
-                    props.onTagSelected(newArray)
-                  }
-                }}
+                className="flex"
+                onSelect={() => createTag({ label: searchTerm })}
               >
-                <Check
-                  className={cn(
-                    "mr-2 h-4 w-4",
-                    selectedTags.find(x => x === tag) ? "opacity-100" : "opacity-0"
-                  )}
-                />
-                <SessionTag value={tag.label} />
+                <p>Create</p>
+                <div className="grow"></div>
+                <SessionTag value={searchTerm} />
               </CommandItem>
-            ))}
+            </CommandGroup>
+          }
+          <CommandGroup>
+            <ScrollArea type="always" className="max-h-48 overflow-y-auto rounded-md border-none">
+              {tags.value.filter((tag) => prefixBasedMatch(tag.label, searchTerm)).map((tag) => (
+                <CommandItem
+                  value={tag.label}
+                  key={tag.id}
+                  onSelect={() => (selectedTags.some(t => t.id === tag.id)) ? handleDeselectTag(tag) : handleSelectTag(tag)}
+                >
+                  <Check
+                    className={cn(
+                      "mr-2 h-4 w-4",
+                      selectedTags.some(t => t.id === tag.id) ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                  <SessionTag value={tag.label} />
+                </CommandItem>
+              ))}
+            </ScrollArea>
           </CommandGroup>
         </Command>
       </PopoverContent>
