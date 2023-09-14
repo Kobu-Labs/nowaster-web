142b7237b619e69afb8bbabeab79ec11add9c742
Kobu <thekobu@gmail.com>
2023-09-14 17:38:25 +0200

    feat: allow for creation of tags from TagPicker

---
+++ next-frontend/lib/searching.ts
@@ -0,0 +1,14 @@
+export const prefixBasedMatch = (value: string, searchTerm: string): boolean => {
+
+  if (searchTerm.length > value.length) {
+    return false
+  }
+
+  for (let i = 0; i < searchTerm.length; i++) {
+    if (searchTerm[i] !== value[i]) {
+      return false
+    }
+  }
+
+  return true;
+}
