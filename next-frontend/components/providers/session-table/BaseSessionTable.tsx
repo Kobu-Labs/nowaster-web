2f747432653c38858a2323e5af1558440f9f51aa
Kobu <thekobu@gmail.com>
2023-09-19 22:21:39 +0200

    feat: introduce separate session table component

---
+++ next-frontend/components/providers/session-table/BaseSessionTable.tsx
@@ -0,0 +1,23 @@
+import { ScheduledSessionApi } from "@/api";
+import { DataTable } from "@/components/visualizers/DataTable";
+import { ScheduledSession } from "@/validation/models";
+import { GetSessionsRequest } from "@/validation/requests/scheduledSession";
+import { useQuery } from "@tanstack/react-query";
+import { ColumnDef } from "@tanstack/react-table";
+import { FC } from "react";
+
+type BaseSessionTableProps = {
+  columns: ColumnDef<ScheduledSession>[],
+  filter?: GetSessionsRequest,
+}
+
+export const BaseSessionTable: FC<BaseSessionTableProps> = (props) => {
+  const { data } = useQuery({
+    queryKey: ["sessions", props.filter],
+    retry: false,
+    queryFn: async () => await ScheduledSessionApi.getSessions(props.filter),
+    select: (data) => data.isOk ? data.value : []
+  });
+
+  return <DataTable columns={props.columns} data={data || []} />;
+};
