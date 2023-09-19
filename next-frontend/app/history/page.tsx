8255c29de6e0b9db28d2d0ccd07fa08d652ad167
Kobu <thekobu@gmail.com>
2023-09-19 22:21:39 +0200

    feat: introduce separate session table component

--- next-frontend/app/history/page.tsx
+++ next-frontend/app/history/page.tsx
@@ -1,30 +1,14 @@
 "use client";
 
-import { ScheduledSessionApi } from "@/api";
-import { DataTable } from "@/components/DataTable";
-import { columns } from "@/components/session-data-table/columns";
+import { BaseSessionTableColumns } from "@/components/providers/session-table/BaseColumns";
+import { BaseSessionTable } from "@/components/providers/session-table/BaseSessionTable";
 import { Card, CardContent } from "@/components/ui/card";
-import { useQuery } from "@tanstack/react-query";
 
 export default function HistoryPage() {
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ["sessions", "history"],
-    retry: false,
-    queryFn: async () => await ScheduledSessionApi.getSessions(),
-  });
-
-  if (isLoading || isError) {
-    return <div >Something bad happenned</div>;
-  }
-
-  if (data.isErr) {
-    return <div>{data.error.message}</div>;
-  }
-
   return (
     <Card >
       <CardContent className="m-8" >
-        <DataTable columns={columns} data={data.value} />
+        <BaseSessionTable columns={BaseSessionTableColumns} />
       </CardContent>
     </Card>
   );
