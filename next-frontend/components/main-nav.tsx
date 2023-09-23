fa3106cc107ea1d6c7a4478fc3e1938588318d31
Kobu <thekobu@gmail.com>
2023-09-23 15:15:25 +0200

    feat: highlight current page in navigation

--- next-frontend/components/main-nav.tsx
+++ next-frontend/components/main-nav.tsx
@@ -1,15 +1,19 @@
+"use client"
 import * as React from "react";
 import Link from "next/link";
 
 import { NavItem } from "@/types/nav";
 import { siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
+import { usePathname } from 'next/navigation'
 
 interface MainNavProps {
   items?: NavItem[]
 }
 
 export function MainNav({ items }: MainNavProps) {
+  const currentPath = usePathname();
+
   return (
     <div className="flex gap-6 md:gap-10">
       <Link href="/" className="flex items-center space-x-2">
@@ -25,7 +29,8 @@ export function MainNav({ items }: MainNavProps) {
                   href={item.href}
                   className={cn(
                     "flex items-center text-sm font-medium text-muted-foreground hover:text-white",
-                    item.disabled && "cursor-not-allowed opacity-80"
+                    item.disabled && "cursor-not-allowed opacity-80",
+                    currentPath === item.href && "text-white"
                   )}
                 >
                   {item.title}
