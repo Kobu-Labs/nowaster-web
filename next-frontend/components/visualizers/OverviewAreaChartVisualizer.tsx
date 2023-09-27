2c931964342e8ad7fe6df730e2a21fa7a3441e2f
Kobu <thekobu@gmail.com>
2023-09-27 21:22:15 +0200

    feat: introduce global colors for category labels

--- next-frontend/components/visualizers/OverviewAreaChartVisualizer.tsx
+++ next-frontend/components/visualizers/OverviewAreaChartVisualizer.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { Granularity, SessionsByCategory } from "@/lib/session-grouping";
-import { formatTime } from "@/lib/utils";
+import { formatTime, randomColor } from "@/lib/utils";
+import { categoryColors } from "@/state/categories";
 import { FC } from "react";
 import { Area, AreaChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import { useRecoilState } from "recoil";
 
 type OverviewChartProps = {
   granularity: keyof typeof Granularity,
@@ -14,8 +16,19 @@ type OverviewChartProps = {
 
 
 export const OverviewAreaChartVisualizer: FC<OverviewChartProps> = (props) => {
-  const colors: { [category: string]: string } = {};
-  props.categories.forEach((category) => colors[category] = "#" + Math.floor(Math.random() * 16777215).toString(16));
+  const [colors, setColors] = useRecoilState(categoryColors);
+  const result: { [label: string]: string } = {};
+
+
+  (props.categories).forEach(category => {
+    if (colors[category] === undefined) {
+      result[category] = randomColor();
+    }
+  });
+
+  if (Object.entries(result).length !== 0) {
+    setColors({ ...colors, ...result });
+  }
 
   return (
     <ResponsiveContainer width={"100%"} height={250} >
@@ -24,26 +37,39 @@ export const OverviewAreaChartVisualizer: FC<OverviewChartProps> = (props) => {
         <YAxis tickFormatter={(x) => formatTime(x)} />
         <Tooltip content={(data) => customTooltip(data, colors)} />
         {props.categories.map(category => {
-          /* BUG:  color switching - implement fix later */
-          return (<Area key={category} fill={colors[category]} type="monotone" stackId="1" dataKey={(v) => v[category] || 0} stroke={colors[category]} strokeWidth={4} fillOpacity={0.4} />);
+          return <Area
+            key={category}
+            fill={colors[category]}
+            type="monotone"
+            stackId="1"
+            dataKey={(v) => v[category] || 0}
+            stroke={colors[category]}
+            strokeWidth={4}
+            fillOpacity={0.4}
+          />;
         })}
       </AreaChart>
     </ResponsiveContainer>
   );
 };
 
-const customTooltip = (data: any, colors: any) => {
+const customTooltip = (data: any, colors: { [category: string]: string }) => {
   if (!data.payload) {
     return <div />;
   }
   const values: [string: number] = data.payload[0]?.payload;
 
-  /* BUG: tailwind classes cannot be computed dynamically - use style prop to set a variable, use the variable inside the className prop*/
   return values
     ? <div className="rounded-sm p-2">
       {Object.entries(values).map(([k, v]) => {
         if (k !== "granularity") {
-          return <p key={k} className={`text-[${colors[k]}]`}>{`${k}  ${formatTime(v)}`}</p>;
+          return <p
+            key={k}
+            style={{ "--legend-color": colors[k] } as React.CSSProperties}
+            className={"text-[var(--legend-color)]"}
+          >
+            {`${k}  ${formatTime(v)}`}
+          </p>;
         }
         return <></>;
       })}
---
+++ next-frontend/state/categories.ts
@@ -0,0 +1,8 @@
+import { atom } from "recoil";
+
+export const categoryColors = atom<{ [label: string]: string }>({
+  key: "categories",
+  default: {},
+});
+
+
