e2c21738d3613ae790eedae0df286ccb1b308ba2
Kobu <thekobu@gmail.com>
2023-09-27 21:20:19 +0200

    feat: introduce global colors for Tag component

--- next-frontend/stories/SessionTag/SessionTag.tsx
+++ next-frontend/stories/SessionTag/SessionTag.tsx
@@ -1,38 +1,28 @@
 import { Badge } from "@/components/ui/badge";
-import { cn } from "@/lib/utils";
+import { cn, randomColor } from "@/lib/utils";
+import { tagColors } from "@/state/tags";
 import { FC } from "react";
+import { useRecoilState } from "recoil";
 
 type SessionTagProps = {
-  color?: keyof typeof colors
   value: string
 }
 
-const colors = {
-  "blue": "bg-sky-500",
-  "red": "bg-rose-400",
-  "violet": "bg-violet-600",
-  "amber": "bg-amber-600",
-  "green": "bg-green-400",
-  "emerald": "bg-emerald-500",
-  "cyan": "bg-cyan-400",
-  "fuchsia": "bg-fuchsia-400",
-};
-
-const getColor = (color: keyof typeof colors | undefined): string => {
-  if (color === undefined) {
-    const values = Object.values(colors);
-    return values[values.length * Math.random() << 0] || "bg-cyan-400";
-  }
-
-  return colors[color];
-};
-
 export const SessionTag: FC<SessionTagProps> = (props) => {
+  const [colors, setColors] = useRecoilState(tagColors);
+
+  if (colors[props.value] === undefined) {
+    setColors({ ...colors, [props.value]: randomColor() });
+  };
+
   return (
-    <Badge className={cn(
-      "h-min text-white hover:scale-110 hover:transition",
-      getColor(props.color),
-      "hover:" + getColor(props.color))}
-    >{props.value}</Badge>
+    <Badge
+      style={{ "--tag-color": colors[props.value] } as React.CSSProperties}
+      className={cn(
+        "h-min bg-[var(--tag-color)] text-white hover:scale-110 hover:bg-[var(--tag-color)] hover:transition ")
+      }
+    >
+      {props.value}
+    </Badge>
   );
 };
