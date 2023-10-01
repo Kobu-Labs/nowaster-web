484c5d778da9e8d5f0e2584a5a650a605953fdb4
Kobu <thekobu@gmail.com>
2023-10-01 17:36:16 +0200

    fix: delete overpadding in navbar

--- next-frontend/components/site-header.tsx
+++ next-frontend/components/site-header.tsx
@@ -3,8 +3,8 @@ import { MainNav } from "@/components/main-nav";
 
 export function SiteHeader() {
   return (
-    <header className="sticky top-0 z-40 w-full border-b bg-background">
-      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+    <header className="bg-background sticky top-0 z-40 w-full border-b">
+      <div className="flex h-16 items-center space-x-4 pl-16 pr-8 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
       </div>
     </header>
