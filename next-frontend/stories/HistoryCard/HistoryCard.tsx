3468a6ea8c0bbe053066576dec6e096edabeeb33
Kobu <thekobu@gmail.com>
2023-10-03 19:17:41 +0200

    fix: use cva for managing border of HistoryCard

--- next-frontend/stories/HistoryCard/HistoryCard.tsx
+++ next-frontend/stories/HistoryCard/HistoryCard.tsx
@@ -1,5 +1,7 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
 import { ScheduledSession } from "@/validation/models";
+import { cva, VariantProps } from "class-variance-authority";
 import { differenceInMilliseconds } from "date-fns";
 import { FC } from "react";
 import { SessionTag } from "../SessionTag/SessionTag";
@@ -20,26 +22,40 @@ const formatTimeDiff = (startTime: Date, endTime: Date): string => {
 
 type HistoryCardProps = {
   session: ScheduledSession,
-  hideBorder?: boolean
-}
+} & VariantProps<typeof historyCardVariants>
 
-export const HistoryCard: FC<HistoryCardProps> = ({ session, hideBorder }) => {
+const historyCardVariants = cva(
+  "hover:bg-accent hover:text-accent-foreground hover:cursor-pointer",
+  {
+    variants: {
+      variant: {
+        default: "border border-input",
+        borderless: "border-hidden",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+);
+
+export const HistoryCard: FC<HistoryCardProps> = (props) => {
   return (
-    <Card className={(hideBorder ? "border-hidden" : "border border-input") + " hover:bg-accent hover:text-accent-foreground hover:cursor-pointer"}>
+    <Card className={cn(historyCardVariants({ variant: props.variant }))}>
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
         <CardTitle className="text-2xl font-bold">
-          {session.category}
+          {props.session.category}
         </CardTitle>
       </CardHeader>
       <CardContent className="flex grow-0">
         <div>
-          <p className="text-sm text-muted-foreground">{session.description}</p>
+          <p className="text-muted-foreground text-sm">{props.session.description}</p>
           <div className="mt-1 flex" >
-            {session.tags.map((val) => <SessionTag key={val.id} value={val.label}></SessionTag>)}
+            {props.session.tags.map((val) => <SessionTag key={val.id} value={val.label}></SessionTag>)}
           </div>
         </div>
         <div className="grow" />
-        <div className="ml-4 font-medium">{formatTimeDiff(session.startTime, session.endTime)}</div>
+        <div className="ml-4 font-medium">{formatTimeDiff(props.session.startTime, props.session.endTime)}</div>
       </CardContent>
     </Card>
   );
