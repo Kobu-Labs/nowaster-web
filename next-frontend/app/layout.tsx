b4ae287b42f7838044616fb3c14abf9c336540c3
Kobu <thekobu@gmail.com>
2023-10-03 19:17:38 +0200

    feat: use toast notifications upon session creation

--- next-frontend/app/layout.tsx
+++ next-frontend/app/layout.tsx
@@ -7,6 +7,7 @@ import { SiteHeader } from "@/components/site-header";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ReactQueryProvider } from "./ReactQueryProvider";
 import { RecoilRoot } from "recoil";
+import { Toaster } from "@/components/ui/toaster";
 
 
 interface RootLayoutProps {
@@ -29,7 +30,8 @@ export default function RootLayout({ children }: RootLayoutProps) {
             <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
               <div className="relative flex min-h-screen flex-col">
                 <SiteHeader />
-                <div className="flex-1">{children}</div>
+                <main className="flex-1">{children}</main>
+                <Toaster />
               </div>
             </ThemeProvider>
           </body>
