4db2b51754d2ed35bca4be8ca66815efe6ad7975
Kobu <thekobu@gmail.com>
2024-02-06 15:09:02 +0100

    style: reorded tailwind classes

--- next-frontend/components/site-header.tsx
+++ next-frontend/components/site-header.tsx
@@ -3,7 +3,7 @@ import { MainNav } from "@/components/main-nav";
 
 export function SiteHeader() {
   return (
-    <header className="bg-background sticky top-0 z-40 w-full border-b">
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="flex h-16 items-center space-x-4 pl-16 pr-8 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
       </div>
