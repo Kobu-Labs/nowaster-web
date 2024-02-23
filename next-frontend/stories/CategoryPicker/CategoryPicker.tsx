b14c05ba3d216c4b5d99d2d25836b39552d8ba6c
Kobu <thekobu@gmail.com>
2024-02-23 21:26:45 +0100

    refactor: change names to better represent the variables

--- next-frontend/stories/CategoryPicker/CategoryPicker.tsx
+++ next-frontend/stories/CategoryPicker/CategoryPicker.tsx
@@ -9,26 +9,37 @@ import { Check, ChevronsUpDown } from "lucide-react";
 import { FC, useState } from "react";
 
 type CategoryPickerProps = {
-  onCategorySelected: (category: string | null) => void
+  onCategorySelected: (category: string | undefined) => void
 }
 
 export const CategoryPicker: FC<CategoryPickerProps> = (props) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [value, setValue] = useState<string | null>(null);
+  const [currentCategory, setCurrentCategory] = useState<string | undefined>(undefined);
 
-  const { data, isLoading, isError } = useQuery({
+  const { data: categories, isLoading, isError } = useQuery({
     queryKey: ["categories"],
     retry: false,
     queryFn: async () => await ScheduledSessionApi.getCategories(),
   });
 
-  if (!data || isLoading || isError) {
+  if (!categories || isLoading || isError) {
     return <div >Something bad happenned</div>;
   }
 
-  if (data.isErr) {
-    return <div>{data.error.message}</div>;
+  if (categories.isErr) {
+    return <div>{categories.error.message}</div>;
   }
+
+  const onSelect = (category: string) => {
+    setCurrentCategory(category);
+    props.onCategorySelected(category);
+    setIsOpen(false);
+  };
+
+  const onDeselect = () => {
+    setCurrentCategory(undefined);
+    props.onCategorySelected(undefined);
+  };
 
   return (
     <Popover open={isOpen} onOpenChange={setIsOpen}>
@@ -39,40 +50,38 @@ export const CategoryPicker: FC<CategoryPickerProps> = (props) => {
           aria-expanded={isOpen}
           className="w-[200px] justify-between"
         >
-          {value || "Select category"}
+          {currentCategory || "Select category"}
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-[200px] p-0">
         <Command shouldFilter={false}>
-          <CommandInput onValueChange={setValue} placeholder={"Search categories"} value={value ?? undefined} />
+          <CommandInput onValueChange={setCurrentCategory} placeholder={"Search categories"} value={currentCategory} />
           <CommandEmpty
             className="cursor-pointer py-6 text-center text-sm"
             onClick={() => {
-              props.onCategorySelected(value);
+              props.onCategorySelected(currentCategory);
               setIsOpen(false);
             }}>
-            {"Create '" + value + "'"}
+            {"Create '" + currentCategory + "'"}
           </CommandEmpty>
           <CommandGroup>
-            {data.value.filter(c => prefixBasedMatch(c, value || "", { caseInsensitive: true })).map((category) => (
-              <CommandItem
-                key={category}
-                onSelect={(currentValue) => {
-                  setValue(currentValue === value ? null : currentValue);
-                  props.onCategorySelected(currentValue === value ? null : currentValue);
-                  setIsOpen(false);
-                }}
-              >
-                <Check
-                  className={cn(
-                    "mr-2 h-4 w-4",
-                    value === category ? "opacity-100" : "opacity-0"
-                  )}
-                />
-                {category}
-              </CommandItem>
-            ))}
+            {categories.value
+              .filter(category => prefixBasedMatch(category, currentCategory || "", { caseInsensitive: true }))
+              .map((category) => (
+                <CommandItem
+                  key={category}
+                  onSelect={selectedValue => (selectedValue === currentCategory) ? onDeselect() : onSelect(selectedValue)}
+                >
+                  <Check
+                    className={cn(
+                      "mr-2 h-4 w-4",
+                      currentCategory === category ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                  {category}
+                </CommandItem>
+              ))}
           </CommandGroup>
         </Command>
       </PopoverContent>
