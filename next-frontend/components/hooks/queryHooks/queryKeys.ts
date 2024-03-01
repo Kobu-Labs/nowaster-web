2fd2162462398d4dfa3138cd018f1d5340566937
Kobu <thekobu@gmail.com>
2024-03-01 16:38:31 +0200

    feat: add query key factory for active session

--- next-frontend/components/hooks/queryHooks/queryKeys.ts
+++ next-frontend/components/hooks/queryHooks/queryKeys.ts
@@ -5,8 +5,12 @@ import { createQueryKeys, mergeQueryKeys } from "@lukemorales/query-key-factory"
 const sessionkeys = createQueryKeys("sessions", {
   filtered: (filters?: Partial<GetSessionsRequest>) => ({
     queryKey: [filters ?? {}],
-    queryFn: async () => await ScheduledSessionApi.getSessions(filters)
-  })
+    queryFn: async () => await ScheduledSessionApi.getSessions(filters),
+  }),
+  active: {
+    queryKey: ["active"],
+    queryFn: async () => await ScheduledSessionApi.getActiveSessions(),
+  },
 });
 
 export const queryKeys = mergeQueryKeys(sessionkeys);
