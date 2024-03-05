cf46315a2e519b96824e3922d911649fde25e308
Kobu <thekobu@gmail.com>
2024-03-05 08:41:10 +0100

    feat: add category api on frontend

--- next-frontend/api/index.ts
+++ next-frontend/api/index.ts
@@ -1,3 +1,4 @@
 export * as ScheduledSessionApi from "./scheduledSessionApi";
 export * as StatisticsApi from "./statisticsApi";
 export * as TagApi from "./tagApi";
+export * as CategoryApi from "./categoryApi";
