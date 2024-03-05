a240c9cd2e4bd9774b20ae882d379bc40f348d79
Kobu <thekobu@gmail.com>
2024-03-05 08:41:14 +0200

    feat: create category controller

---
+++ backend/src/controllers/CategoryController.ts
@@ -0,0 +1,54 @@
+import { Router } from "express";
+import { handleOkResp, handleResultErrorResp } from "@/src/controllers/utils/handleResponse";
+import { validate } from "@/src/middleware/validation";
+import categoryRepo from "@/src/repositories/category";
+import { CategoryRequestSchema } from "@kobu-labs/nowaster-js-typing";
+
+
+export const CategoriesController = Router();
+
+// read many categories
+CategoriesController.get("/", validate({ query: CategoryRequestSchema.readMany }), async (req, res) => {
+  const categories = await categoryRepo.read.many(req.query);
+
+  if (categories.isErr) {
+    return handleResultErrorResp(500, res, categories.error);
+  }
+
+  return handleOkResp(categories.value, res);
+});
+
+
+// create new study session
+CategoriesController.post("/", validate({ body: CategoryRequestSchema.create }), async (req, res) => {
+  const category = await categoryRepo.create(req.body);
+
+  if (category.isErr) {
+    return handleResultErrorResp(500, res, category.error);
+  }
+  return handleOkResp(category.value, res);
+});
+
+
+// get category by name
+CategoriesController.get("/:name", validate({ params: CategoryRequestSchema.readByName }), async (req, res) => {
+  const category = await categoryRepo.read.single(req.params);
+
+  if (category.isErr) {
+    return handleResultErrorResp(500, res, category.error);
+  }
+
+  return handleOkResp(category.value, res);
+});
+
+
+// update category
+CategoriesController.put("/", validate({ body: CategoryRequestSchema.update }), async (req, res) => {
+  const category = await categoryRepo.update(req.body);
+
+  if (category.isErr) {
+    return handleResultErrorResp(500, res, category.error);
+  }
+
+  return handleOkResp(category.value, res);
+});
