bb2d77642d2a329bb932272b804f36cf338ca60d
Kobu <thekobu@gmail.com>
2024-03-08 08:41:15 +0200

    fix: provide correct CategoryController url on frontend

--- next-frontend/api/categoryApi.ts
+++ next-frontend/api/categoryApi.ts
@@ -2,9 +2,9 @@ import baseApi, { handleResponse } from "@/api/baseApi";
 import { Result } from "@badrap/result";
 import { CategoryResponse, CategoryResponseSchema } from "@kobu-labs/nowaster-js-typing";
 
-const BASE_URL = "categories/";
+const BASE_URL = "category/";
 
 export const getCategories = async (): Promise<Result<CategoryResponse["readMany"]>> => {
-  const { data } = await baseApi.get(BASE_URL + "/categories");
+  const { data } = await baseApi.get(BASE_URL);
   return await handleResponse(data, CategoryResponseSchema.readMany);
 };
