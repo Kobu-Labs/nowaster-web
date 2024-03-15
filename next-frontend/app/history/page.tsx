7056672bbb3ad271870e9105c4036952ac4df28e
Kobu <thekobu@gmail.com>
2024-03-15 22:18:38 +0200

    fix: fix layout on history page

--- next-frontend/app/history/page.tsx
+++ next-frontend/app/history/page.tsx
@@ -1,15 +1,12 @@
-"use client";
+"use client"
 
-import { Card, CardContent } from "@/components/shadcn/card";
-import { BaseSessionTableColumns } from "@/components/visualizers/sessions/session-table/BaseSessionColumns";
-import { BaseSessionTable } from "@/components/visualizers/sessions/session-table/BaseSessionTable";
+import { BaseSessionTableColumns } from "@/components/visualizers/sessions/session-table/BaseSessionColumns"
+import { BaseSessionTable } from "@/components/visualizers/sessions/session-table/BaseSessionTable"
 
 export default function HistoryPage() {
   return (
-    <Card >
-      <CardContent className="m-8" >
-        <BaseSessionTable columns={BaseSessionTableColumns} />
-      </CardContent>
-    </Card>
-  );
+    <div className="grow m-8">
+      <BaseSessionTable columns={BaseSessionTableColumns} />
+    </div>
+  )
 }
