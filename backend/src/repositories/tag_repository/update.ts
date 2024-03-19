d576b5cc0628693b2be88c2b83961a6d8ce08448
Kobu <thekobu@gmail.com>
2024-03-19 22:56:15 +0200

    feat: add DB call to remove allowed category on a tag

--- backend/src/repositories/tag_repository/update.ts
+++ backend/src/repositories/tag_repository/update.ts
@@ -1,6 +1,6 @@
 import type { Tag } from "@prisma/client";
 import { Result } from "@badrap/result";
-import type { AsyncResult } from "@/src/repositories/types";
+import { UserVisibleError, type AsyncResult } from "@/src/repositories/types";
 import client from "@/src/repositories/client";
 import type { TagRequest, TagResponse } from "@kobu-labs/nowaster-js-typing";
 
@@ -23,6 +23,42 @@ const single = async (params: UpdateTagParams): AsyncResult<Tag> => {
       },
     });
     return Result.ok(tag);
+  } catch (error) {
+    return Result.err(error as Error);
+  }
+};
+
+const removeAllowedCategory = async (
+  params: TagRequest["removeAllowedCategory"],
+): AsyncResult<TagResponse["removeAllowedCategory"]> => {
+  try {
+    await client.tagToAllowedCategory.deleteMany({
+      where: {
+        tagId: params.tagId,
+        name: params.category,
+      },
+    });
+
+    const updatedTag = await client.tag.findFirst({
+      where: {
+        id: params.tagId,
+      },
+      include: {
+        TagToAllowedCategory: true,
+      },
+    });
+
+    if (updatedTag === null) {
+      return Result.err(new UserVisibleError("Tag was not found"));
+    }
+
+    const result = {
+      allowedCategories: updatedTag.TagToAllowedCategory,
+      label: updatedTag.label,
+      id: updatedTag.id,
+    };
+
+    return Result.ok(result);
   } catch (error) {
     return Result.err(error as Error);
   }
@@ -60,6 +96,7 @@ const addAllowedCategory = async (
 
 const update = {
   addAllowedCategory,
+  removeAllowedCategory,
   single,
 };
 
