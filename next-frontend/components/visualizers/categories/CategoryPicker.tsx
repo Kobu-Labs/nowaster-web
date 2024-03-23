867f966f494c57bc0cb2159dc3db376e6fb42197
Kobu <thekobu@gmail.com>
2024-03-23 17:36:55 +0200

    feat: create category picker for multiple categories

--- next-frontend/components/visualizers/categories/CategoryPicker.tsx
+++ next-frontend/components/visualizers/categories/CategoryPicker.tsx
@@ -1,34 +1,24 @@
 import { FC, useState } from "react"
 import { useQuery } from "@tanstack/react-query"
-import { Check, ChevronsUpDown } from "lucide-react"
 
-import { prefixBasedMatch } from "@/lib/searching"
-import { cn } from "@/lib/utils"
 import { queryKeys } from "@/components/hooks/queryHooks/queryKeys"
-import { Button } from "@/components/shadcn/button"
 import {
-  Command,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-} from "@/components/shadcn/command"
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/shadcn/popover"
-import { ScrollArea } from "@/components/shadcn/scroll-area"
+  MultipleCategoryPickerUiProvider,
+  MultipleCategoryPickerUiProviderProps,
+  SingleCategoryPickerUiProvider,
+  SingleCategoryPickerUiProviderProps,
+} from "@/components/ui-providers/CategoryPickerUiProvider"
 
-type CategoryPickerProps = {
-  onCategorySelected: (category: string | undefined) => void
-}
+type MultipleCategoryPickerProps = {
+  onSelectedCategoriesChanged: (newCategories: string[]) => void
+} & Omit<
+  MultipleCategoryPickerUiProviderProps,
+  "availableCategories" | "selectedCategories" | "onSelectCategory"
+>
 
-export const CategoryPicker: FC<CategoryPickerProps> = (props) => {
-  const [isOpen, setIsOpen] = useState<boolean>(false)
-  const [currentCategory, setCurrentCategory] = useState<string | undefined>(
-    undefined
-  )
-
+export const MultipleCategoryPicker: FC<MultipleCategoryPickerProps> = (
+  props
+) => {
   const {
     data: categories,
     isLoading,
@@ -38,6 +28,8 @@ export const CategoryPicker: FC<CategoryPickerProps> = (props) => {
     retry: false,
   })
 
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([])
+
   if (!categories || isLoading || isError) {
     return <div>Something bad happenned</div>
   }
@@ -46,95 +38,72 @@ export const CategoryPicker: FC<CategoryPickerProps> = (props) => {
     return <div>{categories.error.message}</div>
   }
 
-  const onSelect = (category: string) => {
-    setCurrentCategory(category)
-    props.onCategorySelected(category)
-    setIsOpen(false)
-  }
-
-  const onDeselect = () => {
-    setCurrentCategory(undefined)
-    props.onCategorySelected(undefined)
-  }
-
-  const shouldShowAddButton = () => {
-    return (
-      currentCategory &&
-      categories.value.every(
-        (cat) => !prefixBasedMatch(cat.name, currentCategory)
-      )
-    )
+  const onSelectCategory = (category: string) => {
+    let newCategories
+    if (selectedCategories.some((t) => t === category)) {
+      // deselect
+      newCategories = selectedCategories.filter((t) => t !== category)
+    } else {
+      // select
+      newCategories = [category, ...selectedCategories]
+    }
+    setSelectedCategories(newCategories)
+    props.onSelectedCategoriesChanged(newCategories)
   }
 
   return (
-    <Popover open={isOpen} onOpenChange={setIsOpen}>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          role="combobox"
-          aria-expanded={isOpen}
-          className="w-[200px] justify-between"
-        >
-          {currentCategory || "Select category"}
-          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
-        <Command shouldFilter={false}>
-          <CommandInput
-            onValueChange={setCurrentCategory}
-            placeholder={"Search categories"}
-            value={currentCategory}
-          />
-          {shouldShowAddButton() && (
-            <CommandItem
-              className="cursor-pointer py-6 text-center text-sm hover:bg-accent"
-              onSelect={() => {
-                props.onCategorySelected(currentCategory)
-                setIsOpen(false)
-              }}
-            >
-              {"Create '" + currentCategory + "'"}
-            </CommandItem>
-          )}
-          <CommandGroup>
-            <ScrollArea
-              type="always"
-              className="max-h-48 overflow-y-auto rounded-md border-none"
-            >
-              {categories.value
-                .filter(
-                  (category) =>
-                    currentCategory === undefined ||
-                    prefixBasedMatch(category.name, currentCategory, {
-                      caseInsensitive: true,
-                    })
-                )
-                .map((category) => (
-                  <CommandItem
-                    value={category.name}
-                    key={category.name}
-                    onSelect={(selectedValue) =>
-                      selectedValue === currentCategory
-                        ? onDeselect()
-                        : onSelect(category.name)
-                    }
-                  >
-                    <Check
-                      className={cn(
-                        "mr-2 h-4 w-4",
-                        currentCategory === category.name
-                          ? "opacity-100"
-                          : "opacity-0"
-                      )}
-                    />
-                    {category.name}
-                  </CommandItem>
-                ))}
-            </ScrollArea>
-          </CommandGroup>
-        </Command>
-      </PopoverContent>
-    </Popover>
+    <MultipleCategoryPickerUiProvider
+      modal={props.modal}
+      availableCategories={categories.value.map((cat) => cat.name)}
+      selectedCategories={selectedCategories}
+      onSelectCategory={onSelectCategory}
+      categoryMatchStrategy={props.categoryMatchStrategy}
+      categoryDisplayStrategy={props.categoryDisplayStrategy}
+    />
+  )
+}
+
+type SingleCategoryPickerProps = {
+  onSelectedCategoriesChanged: (newCategory: string) => void
+} & Omit<
+  SingleCategoryPickerUiProviderProps,
+  "availableCategories" | "selectedCategory" | "onSelectCategory"
+>
+
+// TODO: just dynamically allow only one element in selectedCategories[] you dumb ass
+export const SingleCategoryPicker: FC<SingleCategoryPickerProps> = (props) => {
+  const {
+    data: categories,
+    isLoading,
+    isError,
+  } = useQuery({
+    ...queryKeys.categories.all,
+    retry: false,
+  })
+
+  const [selectedCategory, setSelectedCategory] = useState<string>("")
+
+  if (!categories || isLoading || isError) {
+    return <div>Something bad happenned</div>
+  }
+
+  if (categories.isErr) {
+    return <div>{categories.error.message}</div>
+  }
+
+  const onSelectCategory = (category: string) => {
+    setSelectedCategory(category)
+    props.onSelectedCategoriesChanged(category)
+  }
+
+  return (
+    <SingleCategoryPickerUiProvider
+      modal={props.modal}
+      availableCategories={categories.value.map((cat) => cat.name)}
+      selectedCategory={selectedCategory}
+      onSelectCategory={onSelectCategory}
+      categoryMatchStrategy={props.categoryMatchStrategy}
+      categoryDisplayStrategy={props.categoryDisplayStrategy}
+    />
   )
 }
