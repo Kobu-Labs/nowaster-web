fa26096f6c4431b555458c967c6df1fcefb50734
Kobu <thekobu@gmail.com>
2024-03-30 18:46:37 +0200

    style: run eslint on backend

--- backend/src/middleware/validation.ts
+++ backend/src/middleware/validation.ts
@@ -15,22 +15,22 @@ export const validate =
   <TParams = unknown, TBody = unknown, TQuery = unknown, TAny = unknown>(
     validation: Validations<TParams, TBody, TQuery>,
   ): RequestHandler<TParams, TAny, TBody, TQuery> =>
-  async (req, res, next) => {
-    try {
-      if (validation.body) {
-        req.body = validation.body.parse(req.body);
+    async (req, res, next) => {
+      try {
+        if (validation.body) {
+          req.body = validation.body.parse(req.body);
+        }
+        if (validation.params) {
+          req.params = validation.params.parse(req.params);
+        }
+        if (validation.query) {
+          req.query = validation.query.parse(req.query);
+        }
+        return next();
+      } catch (error) {
+        if (error instanceof ZodError) {
+          return handleValidationErrorResp(error, res);
+        }
+        return handleErrorResp(500, res, `Unknown error ${error}`);
       }
-      if (validation.params) {
-        req.params = validation.params.parse(req.params);
-      }
-      if (validation.query) {
-        req.query = validation.query.parse(req.query);
-      }
-      return next();
-    } catch (error) {
-      if (error instanceof ZodError) {
-        return handleValidationErrorResp(error, res);
-      }
-      return handleErrorResp(500, res, `Unknown error ${error}`);
-    }
-  };
+    };
