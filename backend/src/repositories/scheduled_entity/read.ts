b65027a06d92fa07c4dd5380788efa1f30bd47d8
Kobu <thekobu@gmail.com>
2024-04-01 16:58:04 +0200

    fix: use correct prisma keyword

--- backend/src/repositories/scheduled_entity/read.ts
+++ backend/src/repositories/scheduled_entity/read.ts
@@ -2,126 +2,128 @@ import { Result } from "@badrap/result";
 import type { AsyncResult } from "@/src/repositories/types";
 import client from "@/src/repositories/client";
 import type {
-  ScheduledSessionRequest,
-  ScheduledSessionResponse,
+    ScheduledSessionRequest,
+    ScheduledSessionResponse,
 } from "@kobu-labs/nowaster-js-typing";
 
 const many = async (
-  params: ScheduledSessionRequest["readMany"],
+    params: ScheduledSessionRequest["readMany"],
 ): AsyncResult<ScheduledSessionResponse["readMany"]> => {
-  try {
-    // TODO: too hacky
-    let tagsFilter = undefined;
-    if (params.tags?.label?.some) {
-      tagsFilter = {
-        some: { tag: { label: { in: params.tags.label.some } } },
-      };
-    } else if (params.tags?.label?.every) {
-      tagsFilter = {
-        every: { tag: { label: { in: params.tags.label.every } } },
-      };
-    }
+    try {
+        // TODO: too hacky
+        let tagsFilter = undefined;
+        if (params.tags?.label?.some) {
+            tagsFilter = {
+                tags: { some: { tag: { label: { in: params.tags.label.some } } } },
+            };
+        } else if (params.tags?.label?.every) {
+            tagsFilter = {
+                AND: params.tags.label.every.map((label) => ({
+                    tags: { some: { tag: { label: { in: label } } } },
+                })),
+            };
+        }
 
-    // TODO: too hacky
-    let categoryFilter = undefined;
-    if (params.category?.label?.exact) {
-      categoryFilter = { name: params.category.label.exact };
-    }
-    if (params.category?.label?.some) {
-      categoryFilter = { name: { in: params.category.label.some } };
-    }
+        // TODO: too hacky
+        let categoryFilter = undefined;
+        if (params.category?.label?.exact) {
+            categoryFilter = { name: params.category.label.exact };
+        }
+        if (params.category?.label?.some) {
+            categoryFilter = { name: { in: params.category.label.some } };
+        }
 
-    const scheduledEntity = await client.scheduledEntity.findMany({
-      take: params.limit,
-      orderBy: { startTime: "desc" },
-      include: {
-        tags: {
-          include: {
-            tag: {
-              include: {
-                TagToAllowedCategory: true,
-              },
+        const scheduledEntity = await client.scheduledEntity.findMany({
+            take: params.limit,
+            orderBy: { startTime: "desc" },
+            include: {
+                tags: {
+                    include: {
+                        tag: {
+                            include: {
+                                TagToAllowedCategory: true,
+                            },
+                        },
+                    },
+                },
+                category: true,
             },
-          },
-        },
-        category: true,
-      },
-      where: {
-        category: categoryFilter,
-        tags: tagsFilter,
-        startTime: {
-          gte: params.fromStartTime,
-          lte: params.toStartTime,
-        },
-        endTime: {
-          gte: params.fromEndTime,
-          lte: params.toEndTime,
-        },
-      },
-    });
-
-    return Result.ok(
-      scheduledEntity.map((session) => {
-        const { tags, ...rest } = session;
-        const mappedTags = tags.map((tagData) => {
-          const { TagToAllowedCategory, ...data } = tagData.tag;
-          return {
-            ...data,
-            allowedCategories: TagToAllowedCategory,
-          };
+            where: {
+                category: categoryFilter,
+                ...tagsFilter,
+                startTime: {
+                    gte: params.fromStartTime,
+                    lte: params.toStartTime,
+                },
+                endTime: {
+                    gte: params.fromEndTime,
+                    lte: params.toEndTime,
+                },
+            },
         });
-        return { tags: mappedTags, ...rest };
-      }),
-    );
-  } catch (error) {
-    return Result.err(error as Error);
-  }
+
+        return Result.ok(
+            scheduledEntity.map((session) => {
+                const { tags, ...rest } = session;
+                const mappedTags = tags.map((tagData) => {
+                    const { TagToAllowedCategory, ...data } = tagData.tag;
+                    return {
+                        ...data,
+                        allowedCategories: TagToAllowedCategory,
+                    };
+                });
+                return { tags: mappedTags, ...rest };
+            }),
+        );
+    } catch (error) {
+        return Result.err(error as Error);
+    }
 };
 
 const single = async (
-  params: ScheduledSessionRequest["readById"],
+    params: ScheduledSessionRequest["readById"],
 ): AsyncResult<ScheduledSessionResponse["readById"]> => {
-  try {
-    const scheduledEntity = await client.scheduledEntity.findFirst({
-      where: {
-        id: params.id,
-      },
-      include: {
-        tags: {
-          select: {
-            tag: {
-              include: {
-                TagToAllowedCategory: true,
-              },
+    try {
+        const scheduledEntity = await client.scheduledEntity.findFirst({
+            where: {
+                id: params.id,
             },
-          },
-        },
-        category: true,
-      },
-    });
+            include: {
+                tags: {
+                    select: {
+                        tag: {
+                            include: {
+                                TagToAllowedCategory: true,
+                            },
+                        },
+                    },
+                },
+                category: true,
+            },
+        });
 
-    if (!scheduledEntity) {
-      return Result.ok(null);
+        if (!scheduledEntity) {
+            return Result.ok(null);
+        }
+
+        const { tags, ...rest } = scheduledEntity;
+        const mappedTags = tags.map((tagData) => {
+            const { TagToAllowedCategory, ...data } = tagData.tag;
+            return {
+                ...data,
+                allowedCategories: TagToAllowedCategory,
+            };
+        });
+
+        return Result.ok({ tags: mappedTags, ...rest });
+    } catch (error) {
+        return Result.err(error as Error);
     }
-
-    const { tags, ...rest } = scheduledEntity;
-    const mappedTags = tags.map((tagData) => {
-      const { TagToAllowedCategory, ...data } = tagData.tag;
-      return {
-        ...data,
-        allowedCategories: TagToAllowedCategory,
-      };
-    });
-
-    return Result.ok({ tags: mappedTags, ...rest });
-  } catch (error) {
-    return Result.err(error as Error);
-  }
 };
 
 const read = {
-  single,
-  many,
+    single,
+    many,
 };
 
 export default read;
