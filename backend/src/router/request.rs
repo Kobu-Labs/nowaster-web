4799a80149d60bebe4ebf264c864ce4da21e732e
Kobu <thekobu@gmail.com>
2025-02-08 15:56:14 +0200

    fix: remove user_id column as its not used yet

--- backend/src/router/request.rs
+++ backend/src/router/request.rs
@@ -3,7 +3,7 @@ use axum::{
     Json,
 };
 use serde::de::DeserializeOwned;
-use serde_json::{json, Value};
+use serde_json::Value;
 use validator::Validate;
 
 use super::response::ApiResponse;
@@ -18,14 +18,12 @@ where
 {
     type Rejection = ApiResponse<()>;
     async fn from_request(req: Request, state: &S) -> Result<Self, Self::Rejection> {
-        let Json(mut value): Json<Value> =
+        let Json(value): Json<Value> =
             Json::<Value>::from_request(req, state)
                 .await
                 .map_err(|rejection| ApiResponse::Error {
                     message: rejection.body_text(),
                 })?;
-
-        value["user_id"] = json!("8fa94312-788e-4c71-a5f3-125f10c218bb");
 
         let parsed_value: T =
             serde_json::from_value(value).map_err(|rejection| ApiResponse::Error {
