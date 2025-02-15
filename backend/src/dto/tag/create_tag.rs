da13cf8ba49721b27a0b2fd5e9b859c0d01de720
Kobu <thekobu@gmail.com>
2025-02-15 21:16:09 +0200

    fix: add minimal length to relevant fields

--- backend/src/dto/tag/create_tag.rs
+++ backend/src/dto/tag/create_tag.rs
@@ -5,11 +5,13 @@ use validator::Validate;
 #[derive(Clone, Deserialize, Serialize, Debug, Validate)]
 pub struct CreateTagDto {
     pub id: Uuid,
+    #[validate(length(min = 1))]
     pub label: String,
 }
 
 #[derive(Clone, Deserialize, Serialize, Debug, Validate)]
 pub struct UpsertTagDto {
     pub id: Option<Uuid>,
+    #[validate(length(min = 1))]
     pub label: String,
 }
