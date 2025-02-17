242ab9c0ec54afa488857900e221d28d9546a2d3
Kobu <thekobu@gmail.com>
2025-02-17 17:04:06 +0200

    feat: implement backend side of adding allowed categories on tags

--- next-frontend/api/definitions/models/tag.ts
+++ next-frontend/api/definitions/models/tag.ts
@@ -1,13 +1,16 @@
+import { CategorySchema } from "@/api/definitions/models/category";
 import { HasID } from "@/api/definitions/utils";
 import { z } from "zod";
 
 export const TagSchema = z.object({
   label: z.string().trim().min(1),
-  //INFO: temporary remove this: allowedCategories: z.array(CategorySchema),
 });
 
-
 export const TagWithIdSchema = TagSchema.merge(HasID);
+export const TagDetailsSchema = TagWithIdSchema.extend({
+  allowedCategories: z.array(CategorySchema),
+});
 
-export type Tag = z.infer<typeof TagSchema>
-export type TagWithId = z.infer<typeof TagWithIdSchema>
+export type Tag = z.infer<typeof TagSchema>;
+export type TagWithId = z.infer<typeof TagWithIdSchema>;
+export type TagDetails = z.infer<typeof TagDetailsSchema>;
