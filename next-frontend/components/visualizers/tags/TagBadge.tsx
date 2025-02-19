ecafe874e0a7680ac8f43f42a7b1cbfc999fdf39
Kobu <thekobu@gmail.com>
2025-02-19 12:22:12 +0200

    feat: allow manually passing color value to tag badge

--- next-frontend/components/visualizers/tags/TagBadge.tsx
+++ next-frontend/components/visualizers/tags/TagBadge.tsx
@@ -6,19 +6,22 @@ import { cn, randomColor } from "@/lib/utils";
 import { Badge } from "@/components/shadcn/badge";
 
 type TagBadgeProps = {
-  value: string
-}
+  value: string;
+  colors?: string;
+};
 
 export const TagBadge: FC<TagBadgeProps> = (props) => {
   const [colors, setColors] = useRecoilState(tagColors);
 
-  if (colors[props.value] === undefined) {
+  if (colors[props.value] === undefined && !props.colors) {
     setColors({ ...colors, [props.value]: randomColor() });
   }
 
+  const color = props.colors ?? colors[props.value];
+
   return (
     <Badge
-      style={{ "--tag-color": colors[props.value] } as React.CSSProperties}
+      style={{ "--tag-color": color } as React.CSSProperties}
       className={cn(
         "h-min bg-[var(--tag-color)] text-white hover:scale-110 hover:bg-[var(--tag-color)] hover:transition ",
       )}
