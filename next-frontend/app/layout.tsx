5c6d431d65f38c3144172ae0699e93a5cc2c3318
Kobu <thekobu@gmail.com>
2025-02-25 13:29:32 +0200

    feat: add clerk on frontend

--- next-frontend/app/layout.tsx
+++ next-frontend/app/layout.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import "@/styles/globals.css";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RecoilRoot } from "recoil";
@@ -10,34 +9,44 @@ import { SiteHeader } from "@/components/pages/site-header";
 import { ThemeProvider } from "@/components/pages/theme-provider";
 import { Toaster } from "@/components/shadcn/toaster";
 import { ReactQueryProvider } from "@/app/ReactQueryProvider";
+import { ClerkProvider } from "@clerk/nextjs";
+import { AxiosInterceptor } from "@/api/baseApi";
 
 interface RootLayoutProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <RecoilRoot>
-      <ReactQueryProvider>
-        <html lang="en" suppressHydrationWarning>
-          <head />
-          <body
-            className={cn(
-              "min-h-screen bg-background font-sans antialiased",
-              fontSans.variable,
-            )}
-          >
-            <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-              <div className="relative flex min-h-screen flex-col">
-                <SiteHeader />
-                <main className="flex grow">{children}</main>
-                <Toaster />
-                <ReactQueryDevtools initialIsOpen={false} />
-              </div>
-            </ThemeProvider>
-          </body>
-        </html>
-      </ReactQueryProvider>
-    </RecoilRoot>
+    <ClerkProvider>
+      <AxiosInterceptor>
+        <RecoilRoot>
+          <ReactQueryProvider>
+            <html lang="en" suppressHydrationWarning>
+              <head />
+              <body
+                className={cn(
+                  "min-h-screen bg-background font-sans antialiased",
+                  fontSans.variable,
+                )}
+              >
+                <ThemeProvider
+                  attribute="class"
+                  defaultTheme="dark"
+                  enableSystem
+                >
+                  <div className="relative flex min-h-screen flex-col">
+                    <SiteHeader />
+                    <main className="flex grow">{children}</main>
+                    <Toaster />
+                    <ReactQueryDevtools initialIsOpen={false} />
+                  </div>
+                </ThemeProvider>
+              </body>
+            </html>
+          </ReactQueryProvider>
+        </RecoilRoot>
+      </AxiosInterceptor>
+    </ClerkProvider>
   );
 }
