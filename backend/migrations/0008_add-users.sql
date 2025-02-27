6936a309f3e44196b4f7bf265c82e97f751bfffa
Kobu <thekobu@gmail.com>
2025-02-27 13:59:28 +0200

    fix: update backend with user ID

--- backend/migrations/0008_add-users.sql
+++ backend/migrations/0008_add-users.sql
@@ -5,15 +5,15 @@ CREATE TABLE IF NOT EXISTS "user" (
 );
 
 ALTER TABLE session
-ADD COLUMN user_id VARCHAR,
+ADD COLUMN IF NOT EXISTS user_id VARCHAR,
 ADD CONSTRAINT fk_session_user FOREIGN KEY (user_id) REFERENCES "user"(id);
 
 ALTER TABLE category
-ADD COLUMN created_by VARCHAR,
+ADD COLUMN IF NOT EXISTS created_by VARCHAR,
 ADD CONSTRAINT fk_category_user FOREIGN KEY (created_by) REFERENCES "user"(id);
 
 ALTER TABLE tag
-ADD COLUMN created_by VARCHAR,
+ADD COLUMN IF NOT EXISTS created_by VARCHAR,
 ADD CONSTRAINT fk_tag_user FOREIGN KEY (created_by) REFERENCES "user"(id);
 
-DROP TABLE IF EXISTS "user";
+DROP TABLE IF EXISTS "users";
