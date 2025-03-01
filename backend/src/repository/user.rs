4235b412c1295c16250fcb402d75245dc1288016
Kobu <thekobu@gmail.com>
2025-03-01 18:30:14 +0200

    feat: upsert unsers upon requests

--- backend/src/repository/user.rs
+++ backend/src/repository/user.rs
@@ -21,6 +21,7 @@ pub struct ReadUserRow {
 pub trait UserRepositoryTrait {
     fn new(db_conn: &Arc<Database>) -> Self;
     async fn create(&self, dto: CreateUserDto) -> Result<User>;
+    async fn upsert(&self, dto: CreateUserDto) -> Result<Option<User>>;
     fn mapper(&self, row: ReadUserRow) -> User;
 }
 
@@ -52,6 +53,30 @@ impl UserRepositoryTrait for UserRepository {
         User {
             id: row.id,
             username: row.displayname,
+        }
+    }
+
+    async fn upsert(&self, dto: CreateUserDto) -> Result<Option<User>> {
+        let row = sqlx::query_as!(
+            ReadUserRow,
+            r#"
+                INSERT INTO "user" (id, displayname)
+                VALUES ($1, $2)
+                ON CONFLICT (id) DO NOTHING
+                RETURNING id, displayname
+            "#,
+            dto.clerk_user_id,
+            dto.displayname
+        )
+        .fetch_optional(self.db_conn.get_pool())
+        .await?;
+
+        match row {
+            Some(row) => {
+                let user = self.mapper(row);
+                Ok(Some(user))
+            }
+            None => Ok(None),
         }
     }
 }
