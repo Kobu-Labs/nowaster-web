c996938d9fed2d41ed1f60971915a10094ae67da
Kobu <thekobu@gmail.com>
2025-03-12 22:09:48 +0200

    refactor: add loading states to KPI cards

--- next-frontend/components/ui-providers/KpiCardUiProvider.tsx
+++ next-frontend/components/ui-providers/KpiCardUiProvider.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { ReactNode } from "react";
 import { VariantProps, cva } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
@@ -9,13 +9,18 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/shadcn/card";
+import { Skeleton } from "@/components/shadcn/skeleton";
+import { Frown } from "lucide-react";
 
-interface KpiCardUiProviderProps extends VariantProps<typeof cardVariants> {
-  value: string | number
-  title: string
-  description?: string
-  children?: React.ReactNode
-}
+type KpiCardUiProviderProps<T extends ReactNode> = {
+  title: string;
+  description?: string;
+  children?: React.ReactNode;
+  loading?: boolean;
+  value?: T;
+  error?: boolean;
+  mapper?: (value: T) => string | number;
+} & VariantProps<typeof cardVariants>;
 
 const cardVariants = cva(
   "font-bold group-hover:scale-y-125 group-hover:text-pink-300 group-hover:transition",
@@ -32,8 +37,10 @@ const cardVariants = cva(
   },
 );
 
-/* TODO: this should be refactored into separate components using forward ref to extend Card.* component */
-export const KpiCardUiProvider: FC<KpiCardUiProviderProps> = (props) => {
+/* TODO: this component got violated beyond comprehension */
+export function KpiCardUiProvider<T extends ReactNode>(
+  props: KpiCardUiProviderProps<T>,
+) {
   return (
     <Card className="group flex grow flex-col hover:cursor-pointer hover:bg-accent hover:text-accent-foreground">
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
@@ -41,7 +48,15 @@ export const KpiCardUiProvider: FC<KpiCardUiProviderProps> = (props) => {
         <div>{props.children}</div>
       </CardHeader>
       <CardContent className={cn(cardVariants({ variant: props.variant }))}>
-        {props.value}
+        {props.error ? (
+          <Frown className="text-red-500 grow" />
+        ) : props.loading ? (
+          <Skeleton className="w-full h-full min-h-20" />
+        ) : props.mapper && props.value != undefined ? (
+          (props.mapper(props.value) as React.ReactNode)
+        ) : (
+          props.value
+        )}
       </CardContent>
       {props.description && (
         <CardFooter>
@@ -50,4 +65,4 @@ export const KpiCardUiProvider: FC<KpiCardUiProviderProps> = (props) => {
       )}
     </Card>
   );
-};
+}
--- next-frontend/components/visualizers/TotalSessionsKpiCard.tsx
+++ next-frontend/components/visualizers/TotalSessionsKpiCard.tsx
@@ -3,27 +3,26 @@ import { FC } from "react";
 import { KpiCardUiProvider } from "@/components/ui-providers/KpiCardUiProvider";
 import { queryKeys } from "@/components/hooks/queryHooks/queryKeys";
 import { useQuery } from "@tanstack/react-query";
-import { Skeleton } from "@/components/shadcn/skeleton";
 
-type TotalSessionsKpiCardProps = Record<string, never>
+type TotalSessionsKpiCardProps = Record<string, never>;
 
 export const TotalSessionsKpiCard: FC<TotalSessionsKpiCardProps> = () => {
   const stats = useQuery({
     ...queryKeys.statistics.dashboard,
     retry: false,
+    select: (data) => {
+      if (data.isErr) {
+        throw new Error(data.error.message);
+      }
+      return data.value.session_count;
+    },
   });
-
-  if (stats.isPending) {
-    return <Skeleton />;
-  }
-
-  if (stats.isError || stats.data.isErr) {
-    return <span>Error occured</span>;
-  }
 
   return (
     <KpiCardUiProvider
-      value={stats.data.value.session_count}
+      value={stats.data}
+      loading={stats.isLoading}
+      error={stats.isError}
       title={"Total Sessions"}
       description={"Many to go.."}
     >
