bffb0dd21ead60194e6079550a4a8807a732b93e
Kobu <thekobu@gmail.com>
2025-03-20 12:04:55 +0200

    fix: update stopwatch session type to match with scheduled session

--- next-frontend/api/definitions/models/recorded-session.ts
+++ next-frontend/api/definitions/models/recorded-session.ts
@@ -1,13 +1,17 @@
-import { CategorySchema } from "@/api/definitions/models/category";
+import { CategoryWithIdSchema } from "@/api/definitions/models/category";
+import { TagWithIdSchema } from "@/api/definitions/models/tag";
 import { HasID } from "@/api/definitions/utils";
 import { z } from "zod";
 
-export const RecordedSessionSchema = z.object({
+export const StopwatchSessionSchema = z.object({
   startTime: z.coerce.date(),
-  category: CategorySchema,
-  description: z.string().nullable(),
+  category: CategoryWithIdSchema.optional(),
+  description: z.string().optional(),
+  tags: z.array(TagWithIdSchema).optional(),
 });
-export const RecordedSessionWithIdSchema = RecordedSessionSchema.merge(HasID);
+export const StopwatchSessionWithIdSchema = StopwatchSessionSchema.merge(HasID);
 
-export type RecordedSession = z.infer<typeof RecordedSessionSchema>;
-export type RecordedSessionWithId = z.infer<typeof RecordedSessionWithIdSchema>;
+export type StopwatchSession = z.infer<typeof StopwatchSessionSchema>;
+export type StopwatchSessionWithId = z.infer<
+  typeof StopwatchSessionWithIdSchema
+>;
