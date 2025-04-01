831897cf2abbfd7ba420621c4e113f4c37c0a29f
Kobu <thekobu@gmail.com>
2025-04-01 21:52:06 +0200

    refactor: remove stateless tag picker

--- next-frontend/components/visualizers/tags/TagPicker.tsx
+++ next-frontend/components/visualizers/tags/TagPicker.tsx
@@ -43,36 +43,3 @@ export const SimpleTagPicker: FC<SimpleTagPickerProps> = (props) => {
     />
   );
 };
-
-type StatelessTagPickerProps = Omit<TagPickerUiProviderProps, "availableTags">;
-
-export const StatelessTagPicker: FC<StatelessTagPickerProps> = (props) => {
-  const {
-    data: tags,
-    isLoading,
-    isError,
-  } = useQuery({
-    ...queryKeys.tags.all,
-    retry: false,
-    staleTime: 5 * 60 * 1000,
-  });
-
-  if (!tags || isLoading || isError) {
-    return <div>Something bad happenned</div>;
-  }
-
-  if (tags.isErr) {
-    return <div>{tags.error.message}</div>;
-  }
-
-  return (
-    <TagPickerUiProvider
-      availableTags={tags.value}
-      selectedTags={props.selectedTags}
-      onSelectTag={props.onSelectTag}
-      modal={props.modal}
-      tagsDisplayStrategy={props.tagsDisplayStrategy}
-      tagMatchStrategy={props.tagMatchStrategy}
-    />
-  );
-};
