770a38f548f997842b481756aaa9d76c3403d908
Kobu <thekobu@gmail.com>
2025-04-10 12:02:24 +0200

    feat: add favicon

--- next-frontend/app/layout.tsx
+++ next-frontend/app/layout.tsx
@@ -10,9 +10,10 @@ import { SiteHeader } from "@/components/pages/site-header";
 import { ThemeProvider } from "@/components/pages/theme-provider";
 import { Toaster } from "@/components/shadcn/toaster";
 import { ReactQueryProvider } from "@/app/ReactQueryProvider";
+import Head from "next/head";
 
 interface RootLayoutProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
@@ -20,7 +21,21 @@ export default function RootLayout({ children }: RootLayoutProps) {
     <RecoilRoot>
       <ReactQueryProvider>
         <html lang="en" suppressHydrationWarning>
-          <head />
+          <Head>
+            <link rel="icon" href="/favicon.ico" sizes="any" />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="32x32"
+              href="/favicon-32x32.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="16x16"
+              href="/favicon-16x16.png"
+            />
+          </Head>
           <body
             className={cn(
               "min-h-screen bg-background font-sans antialiased",
