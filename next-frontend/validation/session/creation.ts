e738ead43f221b89a06227ac092ca359cabd4f2d
Kobu <thekobu@gmail.com>
2025-04-10 14:02:49 +0200

    fix: provide correct nullability to schema

--- next-frontend/validation/session/creation.ts
+++ next-frontend/validation/session/creation.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 export const sessionPrecursor = z.object({
   startTime: z.coerce.date(),
   endTime: z.coerce.date(),
-  category: CategoryWithIdSchema.nullish(),
+  category: CategoryWithIdSchema.optional(),
   description: z.string().nullable(),
   tags: z.array(TagWithIdSchema),
   session_type: z.literal("fixed"),
