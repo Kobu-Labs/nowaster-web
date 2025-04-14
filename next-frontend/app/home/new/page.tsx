2b5fcadf00f623522a034d7856a76959f1dbfebe
Kobu <thekobu@gmail.com>
2025-04-14 12:12:16 +0200

    feat: add metadata to pages

--- next-frontend/app/home/new/page.tsx
+++ next-frontend/app/home/new/page.tsx
@@ -1,35 +1,23 @@
 "use client";
 
-import { queryKeys } from "@/components/hooks/queryHooks/queryKeys";
 import { ScrollArea, ScrollBar } from "@/components/shadcn/scroll-area";
 import { Separator } from "@/components/shadcn/separator";
-import { DataTable } from "@/components/ui-providers/DataTable";
 import { ScheduledSessionCreationForm } from "@/components/visualizers/sessions/ScheduledSessionCreationForm";
-import { BaseSessionTableColumns } from "@/components/visualizers/sessions/session-table/BaseSessionColumns";
-import { useQuery } from "@tanstack/react-query";
+import { BaseSessionTable } from "@/components/visualizers/sessions/session-table/BaseSessionTable";
 import { subHours } from "date-fns";
 import { useMemo } from "react";
 
 const NewSessionPage = () => {
   const filter = useMemo(
     () => ({
-      endTimeTo: { value: new Date() },
-      endTimeFrom: { value: subHours(new Date(), 48) },
+      data: {
+        endTimeTo: { value: new Date() },
+        endTimeFrom: { value: subHours(new Date(), 48) },
+      },
+      settings: {},
     }),
     [],
   );
-  const pastSessionQuery = useQuery({
-    ...queryKeys.sessions.filtered({
-      settings: {},
-      data: filter,
-    }),
-    select: (data) => {
-      if (data.isErr) {
-        throw new Error(data.error.message);
-      }
-      return data.value.slice(0, 20);
-    },
-  });
 
   return (
     <div className="my-8 flex flex-col h-fit grow justify-center items-center gap-4">
@@ -42,11 +30,7 @@ const NewSessionPage = () => {
           Some of the last sessions you had in the past 48 hours!
         </h2>
         <ScrollArea className="h-[600px]" type="always">
-          <DataTable
-            loading={pastSessionQuery.isLoading}
-            columns={BaseSessionTableColumns}
-            data={pastSessionQuery.data ?? []}
-          />
+          <BaseSessionTable filter={filter} />
           <ScrollBar />
         </ScrollArea>
       </div>
