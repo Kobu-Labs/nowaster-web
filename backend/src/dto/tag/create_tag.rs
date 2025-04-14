28f912cda6ef314870135dad99f0664601be7064
Kobu <thekobu@gmail.com>
2025-04-14 13:34:28 +0200

    fix: allow tags to have duplicate labels

--- backend/src/dto/tag/create_tag.rs
+++ backend/src/dto/tag/create_tag.rs
@@ -1,18 +1,10 @@
 use serde::{Deserialize, Serialize};
-use uuid::Uuid;
 use validator::Validate;
 
 use crate::dto::category::read_category::ReadCategoryDto;
 
-#[derive(Clone, Deserialize, Serialize, Debug, Validate)]
-pub struct CreateTagDto {
-    pub id: Uuid,
-    #[validate(length(min = 1))]
-    pub label: String,
-}
-
 #[derive(Clone, Deserialize, Serialize, Validate)]
-pub struct UpsertTagDto {
+pub struct CreateTagDto {
     pub label: String,
     #[serde(rename = "allowedCategories")]
     pub allowed_categories: Vec<ReadCategoryDto>,
