2762ae5a892a1eaebfb73e81fc573c1003571fa8
Kobu <thekobu@gmail.com>
2025-04-15 18:14:48 +0200

    refactor: remove unused component

--- next-frontend/components/visualizers/tags/TagPicker.tsx
+++ next-frontend/components/visualizers/tags/TagPicker.tsx
@@ -6,31 +6,45 @@ import {
   TagPickerUiProvider,
   TagPickerUiProviderProps,
 } from "@/components/ui-providers/TagPickerUiProvider";
+import { Skeleton } from "@/components/shadcn/skeleton";
+import { Frown } from "lucide-react";
 
 type SimpleTagPickerProps = Omit<TagPickerUiProviderProps, "availableTags">;
 
 export const SimpleTagPicker: FC<SimpleTagPickerProps> = (props) => {
   const {
     data: tags,
-    isLoading,
+    isPending,
     isError,
   } = useQuery({
     ...queryKeys.tags.all,
     retry: false,
+    select: (data) => {
+      if (data.isErr) {
+        throw new Error(data.error.message);
+      }
+      return data.value;
+    },
   });
 
-  if (!tags || isLoading || isError) {
-    return <div>Something bad happenned</div>;
+  if (isError) {
+    return (
+      <div className="flex items-center justify-center  w-full grow h-screen m-20">
+        <Frown className="text-red-500 grow h-1/2 w-1/2" />
+      </div>
+    );
   }
 
-  if (tags.isErr) {
-    return <div>{tags.error.message}</div>;
+  if (isPending) {
+    return (
+      <Skeleton className="flex items-center justify-center  w-full grow  m-10"></Skeleton>
+    );
   }
 
   return (
     <TagPickerUiProvider
       disabled={props.disabled}
-      availableTags={tags.value}
+      availableTags={tags}
       forCategory={props.forCategory}
       selectedTags={props.selectedTags}
       onNewTagsSelected={props.onNewTagsSelected}
