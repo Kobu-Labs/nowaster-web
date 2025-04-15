16db03c5fa7abf0789b63bc1a1541ca602f3fc4c
Kobu <thekobu@gmail.com>
2025-04-15 13:33:14 +0200

    feat: add user table

---
+++ backend/migrations/0008_add-users.sql
@@ -0,0 +1,17 @@
+CREATE TABLE "user" (
+    id VARCHAR PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    displayname VARCHAR NOT NULL
+);
+
+ALTER TABLE session
+ADD COLUMN user_id VARCHAR,
+ADD CONSTRAINT fk_session_user FOREIGN KEY (user_id) REFERENCES "user"(id);
+
+ALTER TABLE category
+ADD COLUMN created_by VARCHAR,
+ADD CONSTRAINT fk_category_user FOREIGN KEY (created_by) REFERENCES "user"(id);
+
+ALTER TABLE tag
+ADD COLUMN created_by VARCHAR,
+ADD CONSTRAINT fk_tag_user FOREIGN KEY (created_by) REFERENCES "user"(id);
