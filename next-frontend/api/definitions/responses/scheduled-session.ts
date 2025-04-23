18577bbe5aec76eafe8b69e7e68f58688ead129d
Kobu <thekobu@gmail.com>
2025-04-23 12:19:35 +0200

    fix: pass correct arguments to delete session call

--- next-frontend/api/definitions/responses/scheduled-session.ts
+++ next-frontend/api/definitions/responses/scheduled-session.ts
@@ -5,11 +5,13 @@ const create = ScheduledSessionWithIdSchema;
 const readMany = z.array(ScheduledSessionWithIdSchema);
 const readById = ScheduledSessionWithIdSchema.nullable();
 const update = ScheduledSessionWithIdSchema;
-const remove = ScheduledSessionWithIdSchema;
+const remove = z.null();
 
 export type ScheduledSessionResponse = {
-    [Property in (keyof typeof ScheduledSessionResponseSchema)]: z.infer<typeof ScheduledSessionResponseSchema[Property]>
-}
+  [Property in keyof typeof ScheduledSessionResponseSchema]: z.infer<
+    (typeof ScheduledSessionResponseSchema)[Property]
+  >;
+};
 
 export const ScheduledSessionResponseSchema = {
   create,
