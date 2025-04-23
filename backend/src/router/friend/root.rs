28d2b75f7624525cb81e5c3fc699c44f8de4913f
Kobu <thekobu@gmail.com>
2025-04-23 23:17:38 +0200

    feat: add router for handling friends

---
+++ backend/src/router/friend/root.rs
@@ -0,0 +1,137 @@
+use axum::{
+    extract::State,
+    routing::{delete, patch},
+    Router,
+};
+
+use crate::{
+    repository::friends::UpdateFriendRequestDto,
+    router::{clerk::ClerkUser, request::ValidatedRequest, response::ApiResponse, root::AppState},
+    service::friend_service::{
+        AcceptFriendRequestDto, CancelFriendRequestDto, CreateFriendRequestDto,
+        FriendRequestStatus, ProcessFriendRequestDto, ReadFriendRequestDto, ReadFriendRequestsDto,
+        ReadFriendshipAsActorDto, ReadFriendshipDto, RejectFriendRequestDto, RemoveFriendDto,
+    },
+};
+
+pub fn friend_router() -> Router<AppState> {
+    Router::new()
+        .route(
+            "/request",
+            patch(update_friend_request_handler)
+                .post(create_friend_request_handler)
+                .get(list_friend_requests_handler),
+        )
+        .route(
+            "/friend",
+            delete(remove_friend_handler).get(list_friends_handler),
+        )
+}
+
+async fn update_friend_request_handler(
+    State(state): State<AppState>,
+    actor: ClerkUser,
+    ValidatedRequest(payload): ValidatedRequest<UpdateFriendRequestDto>,
+) -> ApiResponse<ReadFriendRequestDto> {
+    let result = match payload.status {
+        FriendRequestStatus::Accepted => {
+            state
+                .friend_service
+                .accept_friend_request(
+                    AcceptFriendRequestDto {
+                        request_id: payload.id,
+                    },
+                    actor,
+                )
+                .await
+        }
+        FriendRequestStatus::Rejected => {
+            state
+                .friend_service
+                .reject_friend_request(
+                    RejectFriendRequestDto {
+                        request_id: payload.id,
+                    },
+                    actor,
+                )
+                .await
+        }
+        FriendRequestStatus::Cancelled => {
+            state
+                .friend_service
+                .cancel_friend_request(
+                    CancelFriendRequestDto {
+                        request_id: payload.id,
+                    },
+                    actor,
+                )
+                .await
+        }
+        FriendRequestStatus::Pending => Err(anyhow::anyhow!(
+            "You cannot update a pending friend request"
+        )),
+    };
+
+    ApiResponse::from_result(result)
+}
+
+async fn create_friend_request_handler(
+    State(state): State<AppState>,
+    actor: ClerkUser,
+    ValidatedRequest(payload): ValidatedRequest<ProcessFriendRequestDto>,
+) -> ApiResponse<ReadFriendRequestDto> {
+    let recipient = state
+        .user_service
+        .get_user_by_name(payload.recipient_name)
+        .await;
+
+    let recipient = match recipient {
+        Ok(user) => user,
+        Err(e) => {
+            return ApiResponse::Error {
+                message: e.to_string(),
+            }
+        }
+    };
+
+    let result = state
+        .friend_service
+        .create_friend_request(
+            CreateFriendRequestDto {
+                recipient_id: recipient.id,
+                introduction_message: payload.introduction_message,
+            },
+            actor,
+        )
+        .await;
+    ApiResponse::from_result(result)
+}
+
+async fn list_friend_requests_handler(
+    State(state): State<AppState>,
+    actor: ClerkUser,
+    ValidatedRequest(payload): ValidatedRequest<ReadFriendRequestsDto>,
+) -> ApiResponse<Vec<ReadFriendRequestDto>> {
+    let result = state
+        .friend_service
+        .list_friend_requests(actor, payload)
+        .await;
+    ApiResponse::from_result(result)
+}
+
+async fn list_friends_handler(
+    State(state): State<AppState>,
+    actor: ClerkUser,
+) -> ApiResponse<Vec<ReadFriendshipAsActorDto>> {
+    let result = state.friend_service.list_friends_as_actor(actor).await;
+    ApiResponse::from_result(result)
+}
+
+async fn remove_friend_handler(
+    State(state): State<AppState>,
+    actor: ClerkUser,
+    ValidatedRequest(payload): ValidatedRequest<RemoveFriendDto>,
+) -> ApiResponse<()> {
+    let result = state.friend_service.remove_friend(payload, actor).await;
+    ApiResponse::from_result(result)
+}
