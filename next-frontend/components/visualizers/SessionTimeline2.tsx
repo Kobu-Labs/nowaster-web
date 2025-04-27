7c333d65e1293dce0fc9841d5451177c8724bc2c
Kobu <thekobu@gmail.com>
2025-04-27 20:24:56 +0200

    feat: implement component with percentage tracker

--- next-frontend/components/visualizers/SessionTimeline2.tsx
+++ next-frontend/components/visualizers/SessionTimeline2.tsx
@@ -73,6 +73,53 @@ export const sessionPrecursor = z.object({
   session_type: z.literal("fixed"),
 });
 
+// add props with children
+type HoverPercentageBarProps = {
+  formatter: (percentage: number) => string;
+};
+const HoverPercentageBar: FC<PropsWithChildren<HoverPercentageBarProps>> = (
+  props,
+) => {
+  const [hoverX, setHoverX] = useState<number | null>(null);
+  const [percentage, setPercentage] = useState<number | null>(null);
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    setHoverX(x);
+    setPercentage(Math.min(100, Math.max(0, (x / rect.width) * 100)));
+  };
+
+  const handleMouseLeave = () => {
+    setHoverX(null);
+    setPercentage(null);
+  };
+
+  return (
+    <div
+      className="relative w-full h-full border rounded-2xl overflow-hidden"
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+    >
+      {hoverX !== null && (
+        <>
+          <div
+            className="absolute top-0 bottom-0 w-0 border-2  border-dashed"
+            style={{ left: hoverX }}
+          />
+          <div
+            className="absolute top-0 bottom-0"
+            style={{ left: hoverX + 10 }}
+          >
+            {percentage && props.formatter(percentage)}
+          </div>
+        </>
+      )}
+      {props.children}
+    </div>
+  );
+};
+
 type SessionPrecursor = z.infer<typeof sessionPrecursor>;
 export function SessionTimeline({
   activities: initialActivities,
