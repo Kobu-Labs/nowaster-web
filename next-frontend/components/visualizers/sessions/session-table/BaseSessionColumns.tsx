ee17a656cc3056177fbde1754f4f3c9c6e78672b
Kobu <thekobu@gmail.com>
2025-04-28 14:00:24 +0200

    feat: add editing to table

--- next-frontend/components/visualizers/sessions/session-table/BaseSessionColumns.tsx
+++ next-frontend/components/visualizers/sessions/session-table/BaseSessionColumns.tsx
@@ -1,56 +1,104 @@
-import { FC } from "react";
-import { ScheduledSessionApi } from "@/api";
 import { ScheduledSessionWithId } from "@/api/definitions";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { ColumnDef } from "@tanstack/react-table";
 import { differenceInMinutes, format } from "date-fns";
-import { Trash2 } from "lucide-react";
+import { Edit, Trash2 } from "lucide-react";
+import { FC, useState } from "react";
 
-import { formatTime } from "@/lib/utils";
-import { queryKeys } from "@/components/hooks/queryHooks/queryKeys";
-import { DataTableColumnHeader } from "@/components/shadcn/column-header";
-import { useToast } from "@/components/shadcn/use-toast";
-import { CategoryLabel } from "@/components/visualizers/categories/CategoryLabel";
-import { TagBadge } from "@/components/visualizers/tags/TagBadge";
+import { useDeleteScheduledSession } from "@/components/hooks/session/fixed/useDeleteSession";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/shadcn/alert-dialog";
 import { Button } from "@/components/shadcn/button";
+import { DataTableColumnHeader } from "@/components/shadcn/column-header";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/shadcn/dialog";
+import { CategoryLabel } from "@/components/visualizers/categories/CategoryLabel";
+import { EditScheduledSession } from "@/components/visualizers/sessions/EditScheduledSessionForm";
+import { TagBadge } from "@/components/visualizers/tags/TagBadge";
+import { formatTime } from "@/lib/utils";
 
 type DeleteSessionIconProps = {
   sessionId: string;
 };
 
 const DeleteSessionIcon: FC<DeleteSessionIconProps> = (props) => {
-  const { toast } = useToast();
-  const { mutate: deleteSession } = useMutation({
-    mutationFn: async () =>
-      await ScheduledSessionApi.deleteSingle({ id: props.sessionId }),
-    onSuccess: async (result) => {
-      if (result.isErr) {
-        toast({
-          title: "Session deletion failed",
-          description: result.error.message,
-          variant: "destructive",
-        });
-      } else {
-        toast({
-          title: "Session deleted succesfully",
-          variant: "default",
-        });
-        await queryClient.invalidateQueries({
-          queryKey: queryKeys.sessions._def,
-        });
-      }
-    },
-  });
-  const queryClient = useQueryClient();
+  const [isDeleteAlertOpen, setIsDeleteAlertOpen] = useState(false);
+  const deleteSession = useDeleteScheduledSession();
 
   return (
-    <Button
-      onClick={() => deleteSession()}
-      className="group cursor-pointer p-0 m-0 aspect-square"
-      variant="ghost"
-    >
-      <Trash2 className="group-hover:text-red-500 group-hover:scale-110 group-hover:transition" />
-    </Button>
+    <AlertDialog open={isDeleteAlertOpen} onOpenChange={setIsDeleteAlertOpen}>
+      <AlertDialogTrigger>
+        <Button
+          className="group cursor-pointer p-0 m-0 aspect-square"
+          variant="ghost"
+        >
+          <Trash2 className="group-hover:text-red-500 group-hover:scale-110 group-hover:transition" />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Delete session</AlertDialogTitle>
+          <AlertDialogDescription>
+            Are you sure you want to delete this session? This action cannot be
+            undone.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={async () =>
+              await deleteSession.mutateAsync(props.sessionId, {
+                onSuccess: () => setIsDeleteAlertOpen(false),
+              })
+            }
+            className="bg-destructive text-destructive-foreground hover:bg-destructive/90"
+          >
+            Delete
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+const EditSessionButton: FC<{ session: ScheduledSessionWithId }> = (props) => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  return (
+    <Dialog modal={false} open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+      <DialogTrigger>
+        <Button
+          className="group cursor-pointer p-0 m-0 aspect-square"
+          variant="ghost"
+        >
+          <Edit className="group-hover:scale-110 group-hover:transition" />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="w-full max-w-[60%]">
+        <DialogHeader>
+          <DialogTitle>Edit Session</DialogTitle>
+        </DialogHeader>
+        <EditScheduledSession
+          session={props.session}
+          onCancel={() => setIsDialogOpen(false)}
+          onSave={() => setIsDialogOpen(false)}
+          onDelete={() => setIsDialogOpen(false)}
+        />
+      </DialogContent>
+    </Dialog>
   );
 };
 
@@ -110,7 +158,12 @@ export const BaseSessionTableColumns: ColumnDef<ScheduledSessionWithId>[] = [
     },
   },
   {
-    id: "delete-column",
-    cell: (stuff) => <DeleteSessionIcon sessionId={stuff.row.original.id} />,
+    id: "actions",
+    cell: (data) => (
+      <>
+        <EditSessionButton session={data.row.original} />
+        <DeleteSessionIcon sessionId={data.row.original.id} />
+      </>
+    ),
   },
 ];
