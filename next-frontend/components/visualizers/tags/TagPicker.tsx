dc8651a04babc1f03d38fae3fd1031330a8ebdb0
Kobu <thekobu@gmail.com>
2025-05-01 23:16:55 +0200

    refactor: extract tags fetching to a hook

--- next-frontend/components/visualizers/tags/TagPicker.tsx
+++ next-frontend/components/visualizers/tags/TagPicker.tsx
@@ -1,33 +1,24 @@
 import { FC } from "react";
-import { useQuery } from "@tanstack/react-query";
-
-import { queryKeys } from "@/components/hooks/queryHooks/queryKeys";
 import {
   TagPickerUiProvider,
   TagPickerUiProviderProps,
 } from "@/components/ui-providers/TagPickerUiProvider";
 import { Skeleton } from "@/components/shadcn/skeleton";
 import { Frown } from "lucide-react";
+import { useTags } from "@/components/hooks/tag/useTags";
 
 type SimpleTagPickerProps = Omit<TagPickerUiProviderProps, "availableTags">;
 
 export const SimpleTagPicker: FC<SimpleTagPickerProps> = (props) => {
-  const {
-    data: tags,
-    isPending,
-    isError,
-  } = useQuery({
-    ...queryKeys.tags.all,
-    retry: false,
-  });
+  const tags = useTags();
 
-  if (isError) {
+  if (tags.isError) {
     return (
       <Frown className="flex w-full items-center justify-center h-10 grow text-red-500" />
     );
   }
 
-  if (isPending) {
+  if (tags.isPending) {
     return (
       <Skeleton className="flex items-center justify-center w-full grow h-10" />
     );
@@ -36,7 +27,7 @@ export const SimpleTagPicker: FC<SimpleTagPickerProps> = (props) => {
   return (
     <TagPickerUiProvider
       disabled={props.disabled}
-      availableTags={tags}
+      availableTags={tags.data}
       forCategory={props.forCategory}
       selectedTags={props.selectedTags}
       onNewTagsSelected={props.onNewTagsSelected}
