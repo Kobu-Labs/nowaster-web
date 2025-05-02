3a6a5433c0f632a8393ee6c6e429757f912e648d
Kobu <thekobu@gmail.com>
2025-05-02 13:09:29 +0200

    refactor: extract EditStopwatchSessionForm to separate component

--- next-frontend/components/visualizers/sessions/StartSession.tsx
+++ next-frontend/components/visualizers/sessions/StartSession.tsx
@@ -5,16 +5,7 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/shadcn/tooltip";
-import { useForm } from "react-hook-form";
-import {
-  addHours,
-  addMinutes,
-  isAfter,
-  isBefore,
-  setMinutes,
-  subHours,
-  subMinutes,
-} from "date-fns";
+import { subHours } from "date-fns";
 import {
   Dialog,
   DialogContent,
@@ -24,48 +15,28 @@ import {
   DialogTrigger,
 } from "@/components/shadcn/dialog";
 import { FC, useEffect, useMemo, useRef, useState } from "react";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/shadcn/form";
 
 import { useActiveSessions } from "@/components/hooks/useActiveSessions";
 import { differenceInSeconds } from "date-fns";
-import {
-  CategoryWithIdSchema,
-  StopwatchSessionWithId,
-  TagWithIdSchema,
-} from "@/api/definitions";
+import { StopwatchSessionWithId } from "@/api/definitions";
 import { cn, formatTime } from "@/lib/utils";
 import { CategoryBadge } from "@/components/visualizers/categories/CategoryBadge";
 import { CircleCheck, Play } from "lucide-react";
 import { Button } from "@/components/shadcn/button";
-import { Card, CardContent } from "@/components/shadcn/card";
-import { StopwatchSessionRequest } from "@/api/definitions/requests/stopwatch-session";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { SingleCategoryPicker } from "@/components/visualizers/categories/CategoryPicker";
-import { Input } from "@/components/shadcn/input";
-import {
-  DateTimePicker,
-  QuickOption,
-} from "@/components/visualizers/DateTimePicker";
-import { SimpleTagPicker } from "@/components/visualizers/tags/TagPicker";
-import { z } from "zod";
-import { useUpdateSession } from "@/components/hooks/session/useUpdateSession";
+import { Card } from "@/components/shadcn/card";
 import { Separator } from "@/components/shadcn/separator";
 import { useFinishStopwatchSession } from "@/components/hooks/session/stopwatch/useFinishStopwatchSession";
 import { useCreateStopwatchSession } from "@/components/hooks/session/stopwatch/useCreateStopwatchSession";
-import React from "react";
 import { StopwatchApi } from "@/api";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { queryKeys } from "@/components/hooks/queryHooks/queryKeys";
 import { DataTable } from "@/components/ui-providers/DataTable";
 import { BaseSessionTableColumns } from "@/components/visualizers/sessions/table/BaseSessionColumns";
 import { ScrollArea, ScrollBar } from "@/components/shadcn/scroll-area";
+import {
+  EditSessionFormHandle,
+  EditStopwatchSession,
+} from "@/components/visualizers/sessions/form/EditStopwatchSessionForm";
 
 const formatTimeDifference = (seconds: number) => {
   const diffInSeconds = seconds;
@@ -79,33 +50,6 @@ const formatTimeDifference = (seconds: number) => {
     seconds2.toString().padStart(2, "0"),
   ].join(":");
 };
-
-const creationFormQuickOptions: QuickOption[] = [
-  {
-    label: "now",
-    increment: () => new Date(),
-  },
-  {
-    label: "clamp",
-    increment: (date) => setMinutes(date, 0),
-  },
-  {
-    label: "+ 15m",
-    increment: (date) => addMinutes(date, 15),
-  },
-  {
-    label: "- 15m",
-    increment: (date) => subMinutes(date, 15),
-  },
-  {
-    label: "+ 1h",
-    increment: (date) => addHours(date, 1),
-  },
-  {
-    label: "- 1h",
-    increment: (date) => subHours(date, 1),
-  },
-];
 
 export const SessionTimer: FC = () => {
   const activeSessions = useActiveSessions();
@@ -187,7 +131,7 @@ const StopwatchSessionActive: FC<{ session: StopwatchSessionWithId }> = ({
 
   const finishSession = useFinishStopwatchSession();
   const [globalErr, setGlobalErr] = useState(false);
-  const formRef = useRef<FormHandle>(null);
+  const formRef = useRef<EditSessionFormHandle>(null);
   const filter = useMemo(
     () => ({
       endTimeTo: { value: new Date() },
@@ -327,188 +271,3 @@ const StopwatchSessionActive: FC<{ session: StopwatchSessionWithId }> = ({
     </Card>
   );
 };
-
-const updatePrecursor = z.object({
-  id: z.string().uuid(),
-  startTime: z.coerce.date().nullish(),
-  category: CategoryWithIdSchema.nullish(),
-  description: z.string().nullish(),
-  tags: z.array(TagWithIdSchema).nullish(),
-});
-
-export type FormHandle = {
-  validate: () => Promise<boolean>;
-  prepareData: () => StopwatchSessionRequest["update"];
-};
-
-interface FormComponentProps {
-  onSubmit?: (values: z.infer<typeof updatePrecursor>) => void;
-  session: StopwatchSessionWithId;
-  hideBorder?: boolean;
-}
-
-const EditStopwatchSession = React.forwardRef<FormHandle, FormComponentProps>(
-  ({ session, hideBorder }, ref) => {
-    const form = useForm<z.infer<typeof updatePrecursor>>({
-      resolver: zodResolver(updatePrecursor),
-      defaultValues: {
-        id: session.id,
-        startTime: session.startTime,
-        category: session.category,
-        description: session.description,
-        tags: session.tags ?? [],
-      },
-    });
-
-    // Expose the validate method to the parent
-    React.useImperativeHandle(ref, () => ({
-      validate: async () => await form.trigger(),
-      prepareData: () => {
-        const values = form.getValues();
-        const updateData: StopwatchSessionRequest["update"] = {
-          id: values.id,
-          startTime: values.startTime,
-          category_id: values.category?.id,
-          description: values.description,
-          tag_ids: values.tags?.map((tag) => tag.id),
-        };
-        return updateData;
-      },
-    }));
-    const updateSession = useUpdateSession("stopwatch");
-
-    async function onSubmit(values: z.infer<typeof updatePrecursor>) {
-      if (values.startTime && isBefore(new Date(), values.startTime)) {
-        form.setError("startTime", {
-          message: "Start time must be in the past",
-        });
-        return;
-      }
-
-      const updateData: StopwatchSessionRequest["update"] = {
-        id: values.id,
-        startTime: values.startTime,
-        category_id: values.category?.id,
-        description: values.description,
-        tag_ids: values.tags?.map((tag) => tag.id),
-      };
-
-      await updateSession.mutateAsync(updateData);
-    }
-
-    return (
-      <Card className={cn(hideBorder && "border-0")}>
-        <CardContent className="mt-3">
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              <FormField
-                control={form.control}
-                name="category"
-                render={({ field }) => (
-                  <FormItem className="flex flex-col gap-2">
-                    <FormLabel>Category</FormLabel>
-                    <FormControl>
-                      <SingleCategoryPicker
-                        value={field.value ?? undefined}
-                        onSelectedCategoriesChanged={(category) =>
-                          field.onChange(category)
-                        }
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <FormField
-                control={form.control}
-                name="description"
-                render={({ field }) => (
-                  <FormItem className="flex flex-col gap-2">
-                    <FormLabel>Description (Optional)</FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="Insert your description"
-                        value={field.value ?? ""}
-                        onChange={field.onChange}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <div className="flex items-center gap-4">
-                <FormField
-                  name="startTime"
-                  control={form.control}
-                  render={({ field }) => {
-                    const value = form.watch("startTime");
-
-                    return (
-                      <FormItem className="flex flex-col gap-2">
-                        <FormLabel className="flex items-center gap-2">
-                          {`Start Time`}
-                          {value &&
-                            isAfter(new Date(), value) &&
-                            ` (${formatTimeDifference(differenceInSeconds(new Date(), value))} ago)`}
-                        </FormLabel>
-                        <FormControl>
-                          <DateTimePicker
-                            quickOptions={creationFormQuickOptions}
-                            selected={value ?? undefined}
-                            onSelect={(val) => {
-                              if (val) {
-                                if (val && isBefore(new Date(), val)) {
-                                  form.setError("startTime", {
-                                    message:
-                                      "Cannot set start time in the future",
-                                  });
-                                  return;
-                                }
-                              }
-
-                              field.onChange(val);
-                              form.clearErrors("startTime");
-                            }}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    );
-                  }}
-                />
-              </div>
-
-              <FormField
-                name="tags"
-                control={form.control}
-                render={({ field }) => (
-                  <FormItem className="flex flex-col gap-2">
-                    <FormLabel className="block">Tags</FormLabel>
-                    <FormControl>
-                      <SimpleTagPicker
-                        selectedTags={
-                          field.value?.map((t) => ({
-                            ...t,
-                            usages: 0,
-                            allowedCategories: [],
-                          })) ?? []
-                        }
-                        forCategory={form.watch("category") ?? undefined}
-                        disabled={form.getValues("category") === undefined}
-                        onNewTagsSelected={(tags) => field.onChange(tags)}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </form>
-          </Form>
-        </CardContent>
-      </Card>
-    );
-  },
-);
-EditStopwatchSession.displayName = "EditStopwatchSession";
