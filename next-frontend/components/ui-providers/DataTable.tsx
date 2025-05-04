fe7f990849fb77da8f5ae98e1ab8962d948e8fb1
Kobu <thekobu@gmail.com>
2025-05-04 13:49:53 +0200

    feat: add sorting and hidden columns to table

--- next-frontend/components/ui-providers/DataTable.tsx
+++ next-frontend/components/ui-providers/DataTable.tsx
@@ -1,55 +1,132 @@
 "use client";
 
-import React from "react";
 import {
   ColumnDef,
   ColumnFiltersState,
-  SortingState,
+  ColumnSort,
+  RowSelectionState,
+  VisibilityState,
   flexRender,
   getCoreRowModel,
   getFilteredRowModel,
+  getPaginationRowModel,
   getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 
+import { Skeleton } from "@/components/shadcn/skeleton";
 import {
   Table,
   TableBody,
   TableCell,
+  TableFooter,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/shadcn/table";
-import { Skeleton } from "@/components/shadcn/skeleton";
+import React, { useEffect, useState } from "react";
 
-interface DataTableProps<TData, TValue> {
-  columns: ColumnDef<TData, TValue>[];
+export type ExtractIdsAsArray<T extends { id?: string }[]> = Array<
+  T[number] extends { id: infer R extends string } ? R : never
+>;
+
+export type ManagedTableProps<
+  TData,
+  TColumns extends ColumnDef<TData, TValue>[],
+  TValue = unknown,
+> = Omit<DataTableProps<TData, TColumns, TValue>, "columns">;
+
+export type DataTableProps<
+  TData,
+  TColumns extends ColumnDef<TData, TValue>[],
+  TValue = unknown,
+> = {
+  columns: TColumns;
   data: TData[];
   loading?: boolean;
+  options: {
+    getRowId?: (value: TData) => string;
+    loading?: boolean;
+    enableMultiRowSelection?: boolean;
+    initialSorting?: {
+      id: ExtractIdsAsArray<TColumns>[number];
+      desc: boolean;
+    }[];
+    hiddenColumns?: ExtractIdsAsArray<TColumns>;
+  };
+};
+
+type TableSelection<T> = {
+  initialSelection?: T[];
+  onSelect: (val: T[]) => void;
+  getRowId: (val: T) => string;
+  data: T[];
+};
+
+// TODO: this should not be used right now, needs refactoring
+export function useTableSelection<T>(opts: TableSelection<T>) {
+  const selection: RowSelectionState = {};
+  opts.initialSelection?.forEach((val) => {
+    selection[opts.getRowId(val)] = true;
+  });
+
+  const [selectedRows, setSelectedRows] =
+    useState<RowSelectionState>(selection);
+
+  useEffect(() => {
+    const keys = Object.keys(selectedRows);
+    const val = opts.data.filter((v) => keys.includes(opts.getRowId(v)));
+    opts.onSelect(val);
+  }, [selectedRows]);
+
+  return { selectedRows, setSelectedRows };
 }
 
-export function DataTable<TData, TValue>({
-  columns,
-  data,
-  loading,
-}: DataTableProps<TData, TValue>) {
-  const [sorting, setSorting] = React.useState<SortingState>([]);
+export function DataTable<
+  TData,
+  TValue,
+  TColumns extends ColumnDef<TData, TValue>[],
+>({ columns, data, options }: DataTableProps<TData, TColumns, TValue>) {
+  const [sorting, setSorting] = React.useState<ColumnSort[]>(
+    options.initialSorting ?? [],
+  );
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
     [],
   );
 
+  const columnVisibilityInitial =
+    React.useMemo(
+      () =>
+        options.hiddenColumns?.reduce(
+          (acc, id) => {
+            acc[id as string] = false;
+            return acc;
+          },
+          {} as Record<string, boolean>,
+        ),
+      [options.hiddenColumns],
+    ) ?? {};
+
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>(columnVisibilityInitial);
+
   const table = useReactTable({
     data,
     columns,
-    getCoreRowModel: getCoreRowModel(),
+    getRowId: options.getRowId,
     onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
     getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    getPaginationRowModel: getPaginationRowModel(),
+    enableMultiRowSelection: options?.enableMultiRowSelection,
     state: {
       sorting,
       columnFilters,
+      columnVisibility,
     },
-    onColumnFiltersChange: setColumnFilters,
-    getFilteredRowModel: getFilteredRowModel(),
   });
 
   return (
@@ -64,9 +141,9 @@ export function DataTable<TData, TValue>({
                     {header.isPlaceholder
                       ? null
                       : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext(),
-                      )}
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
                   </TableHead>
                 );
               })}
@@ -87,7 +164,7 @@ export function DataTable<TData, TValue>({
                 ))}
               </TableRow>
             ))
-          ) : loading ? (
+          ) : options.loading ? (
             <TableRow>
               <TableCell colSpan={columns.length} className="h-24 text-center">
                 <Skeleton className="w-full min-h-32" />
@@ -101,6 +178,9 @@ export function DataTable<TData, TValue>({
             </TableRow>
           )}
         </TableBody>
+        <TableFooter>
+          Selected values: {table.getSelectedRowModel().rows.length}
+        </TableFooter>
       </Table>
     </div>
   );
