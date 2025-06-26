265fe57e4f1c1421ad289198f93e233e816872cf
Kobu <thekobu@gmail.com>
2025-06-26 14:21:05 +0200

    feat: extract common date utilities

---
+++ next-frontend/lib/date-utils.ts
@@ -0,0 +1,30 @@
+export const numberToDay = (value: number): string => {
+  switch (value) {
+    case 0:
+      return "Sunday";
+    case 1:
+      return "Monday";
+    case 2:
+      return "Tuesday";
+    case 3:
+      return "Wednesday";
+    case 4:
+      return "Thursday";
+    case 5:
+      return "Friday";
+    case 6:
+      return "Saturday";
+    default:
+      throw new Error("Invalid value for day of the week");
+  }
+};
+
+export const daysOfWeek = [
+  { short: "Mon", full: "Monday", value: 1 },
+  { short: "Tue", full: "Tuesday", value: 2 },
+  { short: "Wed", full: "Wednesday", value: 3 },
+  { short: "Thu", full: "Thursday", value: 4 },
+  { short: "Fri", full: "Friday", value: 5 },
+  { short: "Sat", full: "Saturday", value: 6 },
+  { short: "Sun", full: "Sunday", value: 0 },
+] as const;
