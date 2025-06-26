14b3ae1f3e01fb2e766053925a1c8e208557e489
Kobu <thekobu@gmail.com>
2025-06-26 14:21:05 +0200

    feat: finilize template creation forms

--- next-frontend/components/visualizers/sessions/templates/form/RecurringSessionForm.tsx
+++ next-frontend/components/visualizers/sessions/templates/form/RecurringSessionForm.tsx
@@ -1,18 +1,11 @@
 "use client";
 
-import { FC } from "react";
 import { CategoryWithIdSchema, TagWithIdSchema } from "@/api/definitions";
 import {
   RecurringSessionInterval,
   RecurringSessionIntervalSchema,
 } from "@/api/definitions/models/session-template";
-import { Button } from "@/components/shadcn/button";
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@/components/shadcn/card";
+import { Card } from "@/components/shadcn/card";
 import {
   FormControl,
   FormField,
@@ -21,17 +14,10 @@ import {
   FormMessage,
 } from "@/components/shadcn/form";
 import { Input } from "@/components/shadcn/input";
-import { ScrollArea, ScrollBar } from "@/components/shadcn/scroll-area";
-import { Separator } from "@/components/shadcn/separator";
+import { IntervalBasedPicker } from "@/components/ui-providers/date-pickers/interval/IntevalBasedPicker";
 import { SingleCategoryPicker } from "@/components/visualizers/categories/CategoryPicker";
 import { SimpleTagPicker } from "@/components/visualizers/tags/TagPicker";
-import { cn, numberToDay, zeroPad } from "@/lib/utils";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "components/shadcn/popover";
-import { CalendarIcon } from "lucide-react";
+import { FC } from "react";
 import { Control } from "react-hook-form";
 import { z } from "zod";
 
@@ -61,201 +47,14 @@ export const templateSessionPrecursor = z.object({
   sessions: z.array(recurringSessionPrecursor),
 });
 
-export type SlideAnswersProps = {
+export type RecurringSessionFormProps = {
   control: Control<z.infer<typeof templateSessionPrecursor>>;
   intervalStart: Date;
   interval: RecurringSessionInterval;
   parentFieldIndex: number;
 };
 
-type IntervalBasedDatePickerProps = {
-  interval_start: Date;
-  interval: RecurringSessionInterval;
-  onSelect: (value: { day: number; hours: number; minutes: number }) => void;
-  selected?: { day: number; hours: number; minutes: number };
-  orientation?: "horizontal" | "vertical";
-};
-
-const daysOfWeek = [
-  { short: "Mon", full: "Monday", value: 1 },
-  { short: "Tue", full: "Tuesday", value: 2 },
-  { short: "Wed", full: "Wednesday", value: 3 },
-  { short: "Thu", full: "Thursday", value: 4 },
-  { short: "Fri", full: "Friday", value: 5 },
-  { short: "Sat", full: "Saturday", value: 6 },
-  { short: "Sun", full: "Sunday", value: 0 },
-];
-
-type WeeklyIntervalDatePickerProps = {
-  onSelect: (value: { day: number; hours: number; minutes: number }) => void;
-  selected?: { day: number; hours: number; minutes: number };
-  orientation?: "horizontal" | "vertical";
-};
-
-export const WeeklyIntervalDatePicker: FC<WeeklyIntervalDatePickerProps> = (
-  props,
-) => {
-  return (
-    <Card className="w-full ">
-      <CardHeader>
-        <CardTitle>Select a Day</CardTitle>
-      </CardHeader>
-      <CardContent className="space-y-4 w-full">
-        <div className="grid grid-cols-7 gap-2">
-          {daysOfWeek.map((day) => (
-            <Button
-              type="button"
-              key={day.value}
-              size="sm"
-              className="h-12 text-xs font-medium"
-              variant={props.selected?.day === day.value ? "default" : "ghost"}
-              onClick={() =>
-                props.onSelect({
-                  day: day.value,
-                  hours: props.selected?.hours ?? 0,
-                  minutes: props.selected?.minutes ?? 0,
-                })
-              }
-            >
-              <span className="hidden sm:inline">{day.short}</span>
-              <span className="sm:hidden">{day.short.charAt(0)}</span>
-            </Button>
-          ))}
-        </div>
-        <Separator className="my-4" />
-        <TimePicker
-          orientation={props.orientation}
-          selected={props.selected}
-          onSelect={(val) =>
-            props.onSelect({
-              day: props.selected?.day ?? 0,
-              hours: val.hours,
-              minutes: val.minutes,
-            })
-          }
-        />
-      </CardContent>
-    </Card>
-  );
-};
-
-const toggleOrientation = (orientation: "horizontal" | "vertical") => {
-  return orientation === "horizontal" ? "vertical" : "horizontal";
-};
-
-type DatePickerDemoProps = {
-  onSelect: (value: { day: number; hours: number; minutes: number }) => void;
-  selected?: { day: number; hours: number; minutes: number };
-  orientation?: "horizontal" | "vertical";
-};
-
-export const TimePicker: FC<DatePickerDemoProps> = (props) => {
-  const hours = Array.from({ length: 24 }, (_, i) => i);
-  const orientation = props.orientation ?? "vertical";
-
-  return (
-    <div
-      className={cn(
-        "flex w-full",
-        orientation === "horizontal" && "flex-col ",
-        orientation === "vertical" && "flex-row ",
-      )}
-    >
-      <ScrollArea>
-        <div
-          className={cn(
-            "flex p-2 gap-1",
-            orientation === "horizontal" && "flex-row items-center w-[300px]",
-            orientation === "vertical" && "flex-col h-[300px] items-center",
-          )}
-        >
-          <div className="relative">
-            <p className="top-0 text-muted-foreground sticky">HH</p>
-          </div>
-          <Separator orientation={toggleOrientation(orientation)} />
-          {hours.map((hour) => (
-            <Button
-              key={hour}
-              size="icon"
-              type="button"
-              className="shrink-0 aspect-square"
-              variant={props.selected?.hours === hour ? "default" : "ghost"}
-              onClick={() =>
-                props.onSelect({
-                  hours: hour,
-                  minutes: props.selected?.minutes ?? 0,
-                  day: props.selected?.day ?? 0,
-                })
-              }
-            >
-              {zeroPad(hour)}
-            </Button>
-          ))}
-        </div>
-        <ScrollBar orientation={orientation} />
-      </ScrollArea>
-      <ScrollArea>
-        <div
-          className={cn(
-            "flex p-2 gap-1",
-            orientation === "horizontal" && "flex-row items-center w-[300px]",
-            orientation === "vertical" && "flex-col items-center h-[300px]",
-          )}
-        >
-          <p className="text-muted-foreground">MM</p>
-          <Separator orientation={toggleOrientation(orientation)} />
-          {Array.from({ length: 12 }, (_, i) => i * 5).map((minute) => (
-            <Button
-              key={minute}
-              size="icon"
-              type="button"
-              className="shrink-0 aspect-square"
-              variant={props.selected?.minutes === minute ? "default" : "ghost"}
-              onClick={() =>
-                props.onSelect({
-                  minutes: minute,
-                  hours: props.selected?.hours ?? 0,
-                  day: props.selected?.day ?? 0,
-                })
-              }
-            >
-              {minute.toString().padStart(2, "0")}
-            </Button>
-          ))}
-        </div>
-        <ScrollBar orientation={orientation} />
-      </ScrollArea>
-    </div>
-  );
-};
-
-export const IntervalBasedDatePicker: FC<IntervalBasedDatePickerProps> = (
-  props,
-) => {
-  if (props.interval === "weekly") {
-    return (
-      <WeeklyIntervalDatePicker
-        onSelect={props.onSelect}
-        selected={props.selected}
-        orientation={props.orientation}
-      />
-    );
-  }
-
-  if (props.interval === "daily") {
-    return (
-      <TimePicker
-        orientation={props.orientation}
-        onSelect={props.onSelect}
-        selected={props.selected}
-      />
-    );
-  }
-
-  return null;
-};
-
-export const RecurringSessionForm: FC<SlideAnswersProps> = (props) => {
+export const RecurringSessionForm: FC<RecurringSessionFormProps> = (props) => {
   return (
     <Card className="p-4">
       <fieldset className="flex flex-col gap-2">
@@ -286,35 +85,14 @@ export const RecurringSessionForm: FC<SlideAnswersProps> = (props) => {
               <FormItem className="flex flex-col gap-2">
                 <FormLabel className="block">Start</FormLabel>
                 <FormControl>
-                  <Popover modal={false}>
-                    <PopoverTrigger className="p-1">
-                      <Button
-                        type="button"
-                        variant={"outline"}
-                        className={cn(
-                          "w-[240px] justify-start text-left font-normal",
-                        )}
-                      >
-                        <CalendarIcon className="mr-2 size-4" />
-                        <span>
-                          {field.value === undefined
-                            ? "Pick a value"
-                            : `${numberToDay(field.value.day).substring(0, 3)}, ${zeroPad(field.value.hours)}:${zeroPad(field.value.minutes)}`}
-                        </span>
-                      </Button>
-                    </PopoverTrigger>
-                    <PopoverContent className="p-0 m-0 border-none w-[400px]">
-                      <IntervalBasedDatePicker
-                        selected={field.value}
-                        orientation="horizontal"
-                        interval={props.interval}
-                        interval_start={props.intervalStart}
-                        onSelect={(val) => {
-                          field.onChange(val);
-                        }}
-                      />
-                    </PopoverContent>
-                  </Popover>
+                  <IntervalBasedPicker
+                    selected={field.value}
+                    orientation="horizontal"
+                    interval={props.interval}
+                    onSelect={(val) => {
+                      field.onChange(val);
+                    }}
+                  />
                 </FormControl>
                 <FormMessage />
               </FormItem>
@@ -328,35 +106,14 @@ export const RecurringSessionForm: FC<SlideAnswersProps> = (props) => {
               <FormItem className="flex flex-col gap-2">
                 <FormLabel className="block">End</FormLabel>
                 <FormControl>
-                  <Popover modal={false}>
-                    <PopoverTrigger className="p-1">
-                      <Button
-                        type="button"
-                        variant={"outline"}
-                        className={cn(
-                          "w-[240px] justify-start text-left font-normal",
-                        )}
-                      >
-                        <CalendarIcon className="mr-2 size-4" />
-                        <span>
-                          {field.value === undefined
-                            ? "Pick a value"
-                            : `${numberToDay(field.value.day).substring(0, 3)}, ${zeroPad(field.value.hours)}:${zeroPad(field.value.minutes)}`}
-                        </span>
-                      </Button>
-                    </PopoverTrigger>
-                    <PopoverContent className="p-0 m-0 border-none w-[400px]">
-                      <IntervalBasedDatePicker
-                        selected={field.value}
-                        orientation="horizontal"
-                        interval={props.interval}
-                        interval_start={props.intervalStart}
-                        onSelect={(val) => {
-                          field.onChange(val);
-                        }}
-                      />
-                    </PopoverContent>
-                  </Popover>
+                  <IntervalBasedPicker
+                    selected={field.value}
+                    orientation="horizontal"
+                    interval={props.interval}
+                    onSelect={(val) => {
+                      field.onChange(val);
+                    }}
+                  />
                 </FormControl>
                 <FormMessage />
               </FormItem>
--- next-frontend/components/visualizers/sessions/templates/form/TemplateForm.tsx
+++ next-frontend/components/visualizers/sessions/templates/form/TemplateForm.tsx
@@ -1,6 +1,19 @@
 "use client";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/shadcn/dialog";
 import { SessionTemplateApi } from "@/api";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/shadcn/tooltip";
 import { RecurringSessionInterval } from "@/api/definitions/models/session-template";
 import { SessionTemplateRequest } from "@/api/definitions/requests/session-template";
 import { Button } from "@/components/shadcn/button";
@@ -27,33 +40,27 @@ import {
   templateSessionPrecursor,
 } from "@/components/visualizers/sessions/templates/form/RecurringSessionForm";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { addDays, differenceInMinutes, isBefore } from "date-fns";
 import { Plus, Trash2 } from "lucide-react";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { useFieldArray, useForm } from "react-hook-form";
 import { z } from "zod";
-
-type TemplateIntervalSelectProps = {
-  interval: RecurringSessionInterval;
-  onSelect: (interval: RecurringSessionInterval) => void;
-};
+import React from "react";
+import { Separator } from "@/components/shadcn/separator";
 
 const IntervalToLabel: Record<RecurringSessionInterval, string> = {
   daily: "Daily",
   weekly: "Weekly",
 } as const;
 
-export const TemplateIntervalSelect: FC<TemplateIntervalSelectProps> = (
-  props,
-) => {
+export const TestIntervalSelect = React.forwardRef<
+  React.ElementRef<typeof Select>,
+  React.ComponentPropsWithoutRef<typeof Select>
+>(({ ...props }) => {
   return (
-    <Select
-      onValueChange={(a: RecurringSessionInterval) => {
-        props.onSelect(a);
-      }}
-    >
-      <SelectTrigger className="w-[180px]">
+    <Select {...props}>
+      <SelectTrigger>
         <SelectValue
           placeholder="Select an interval!"
           className="text-muted-foreground"
@@ -70,15 +77,19 @@ export const TemplateIntervalSelect: FC<TemplateIntervalSelectProps> = (
       </SelectContent>
     </Select>
   );
-};
+});
 
 export const TemplateForm: FC = () => {
   const form = useForm<z.infer<typeof templateSessionPrecursor>>({
     resolver: zodResolver(templateSessionPrecursor),
   });
+  const queryClient = useQueryClient();
 
   const mutation = useMutation({
     mutationKey: ["session-template"],
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ["session-templates"] });
+    },
     mutationFn: async (data: z.infer<typeof templateSessionPrecursor>) => {
       const interval_start = data.start_date;
 
@@ -112,7 +123,6 @@ export const TemplateForm: FC = () => {
           }
         }
 
-        console.log(newDate);
         return differenceInMinutes(newDate, interval_start);
       };
 
@@ -132,13 +142,26 @@ export const TemplateForm: FC = () => {
         interval: data.interval,
         start_date: data.start_date,
         end_date: data.end_date,
-        sessions: data.sessions.map((session) => ({
-          category_id: session.category!.id!,
-          tag_ids: session.tags.map((tag) => tag.id),
-          description: session.description ?? undefined,
-          start_minute_offset: onFieldChange(session.start_date_time),
-          end_minute_offset: onFieldChange(session.end_date_time),
-        })),
+        sessions: data.sessions.map((session) => {
+          const session_start = onFieldChange(session.start_date_time);
+          let session_end = onFieldChange(session.end_date_time);
+
+          if (session_start >= session_end) {
+            if (data.interval === "daily") {
+              session_end = session_start + 24 * 60; // add 24 hours in minutes
+            } else {
+              session_end += 7 * 24 * 60;
+            }
+          }
+
+          return {
+            category_id: session.category!.id!,
+            tag_ids: session.tags.map((tag) => tag.id),
+            description: session.description ?? undefined,
+            start_minute_offset: session_start,
+            end_minute_offset: session_end,
+          };
+        }),
       };
 
       await SessionTemplateApi.create(translatedData);
@@ -147,50 +170,59 @@ export const TemplateForm: FC = () => {
 
   const submitForm = async (data: z.infer<typeof templateSessionPrecursor>) => {
     await mutation.mutateAsync(data);
+    form.reset();
+    form.reset({ name: "" });
+    fieldArray.remove();
   };
 
   const fieldArray = useFieldArray({ control: form.control, name: "sessions" });
-  console.log("interval", form.watch("interval"));
+
+  const preventContinue =
+    mutation.isPending ||
+    form.watch("interval") === undefined ||
+    form.watch("start_date") === undefined ||
+    form.watch("end_date") === undefined;
+
   return (
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(submitForm, console.error)}
         className="m-8 flex flex-col items-center justify-center gap-8"
       >
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Template Name </FormLabel>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+        <div className="grid w-full grid-cols-2 gap-4 content-center">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Template Name </FormLabel>
+                <FormControl>
+                  <Input {...field} placeholder="Enter template name" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <FormField
-          control={form.control}
-          name="interval"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Inteval</FormLabel>
-              <FormControl>
-                <TemplateIntervalSelect
-                  onSelect={(val) => {
-                    field.onChange(val);
-                  }}
-                  interval={field.value}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+          <FormField
+            control={form.control}
+            name="interval"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Inteval</FormLabel>
+                <FormControl>
+                  <TestIntervalSelect
+                    {...field}
+                    key={field.value}
+                    onValueChange={field.onChange}
+                    value={field.value}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <div className="flex items-center gap-4">
           <FormField
             name="start_date"
             control={form.control}
@@ -230,39 +262,97 @@ export const TemplateForm: FC = () => {
           />
         </div>
 
-        {fieldArray.fields.map((field, index) => (
-          <div className="flex items-center gap-2" key={field.id}>
-            <div className="rounded-md border p-4">
-              <Trash2
-                className="cursor-pointer hover:text-red-500"
-                type="button"
-                onClick={() => fieldArray.remove(index)}
+        <Separator className="w-full" />
+
+        <div className="flex flex-col gap-4 mt-8 items-center">
+          {fieldArray.fields.map((field, index) => (
+            <div
+              className="flex items-center gap-2 col-span-full"
+              key={field.id}
+            >
+              <div className="rounded-md border p-4">
+                <Trash2
+                  className="cursor-pointer hover:text-red-500"
+                  type="button"
+                  onClick={() => fieldArray.remove(index)}
+                />
+              </div>
+              <RecurringSessionForm
+                interval={form.watch("interval")}
+                intervalStart={form.watch("start_date")}
+                control={form.control}
+                parentFieldIndex={index}
               />
             </div>
-            <RecurringSessionForm
-              interval={form.watch("interval")}
-              intervalStart={form.watch("start_date")}
-              control={form.control}
-              parentFieldIndex={index}
-            />
-          </div>
-        ))}
-        <Plus
-          className="cursor-pointer hover:text-green-400"
-          type="button"
-          onClick={() =>
-            fieldArray.append({
-              // @ts-expect-error - .append expects a full object, but we are providing a partial one because category and tags are not set yet
-              category: undefined,
-              tags: [],
-              description: undefined,
-            })
-          }
-        />
-        <Button type="submit" className="w-full" variant="default" size="lg">
-          Submit
-        </Button>
+          ))}
+          <TooltipProvider delayDuration={preventContinue ? 0 : 50000}>
+            <Tooltip>
+              <TooltipTrigger className="cursor-not-allowed">
+                <Button
+                  type="button"
+                  variant="outline"
+                  className="group flex items-center gap-2 "
+                  disabled={preventContinue}
+                  onClick={() =>
+                    fieldArray.append({
+                      // @ts-expect-error - .append expects a full object, but we are providing a partial one because category and tags are not set yet
+                      category: undefined,
+                      tags: [],
+                      description: undefined,
+                    })
+                  }
+                >
+                  Add Session
+                  <Plus
+                    className="cursor-pointer group-hover:text-green-400"
+                    type="button"
+                  />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent className="text-nowrap">
+                Fill out form details first!
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
+
+        <div className="flex items-center justify-center w-full gap-4">
+          <div className="grow" />
+          <Button
+            type="submit"
+            className="w-fit"
+            variant="default"
+            size="lg"
+            loading={mutation.isPending}
+          >
+            Submit
+          </Button>
+        </div>
       </form>
     </Form>
+  );
+};
+
+export const TemplateFormDialog: FC = () => {
+  const [open, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <Button className="w-fit" onClick={() => setIsOpen(true)}>
+        Create template
+      </Button>
+      <Dialog modal={false} onOpenChange={setIsOpen} open={open}>
+        <DialogContent className="max-w-4xl max-h-[90vh] overflow-y-auto">
+          <DialogHeader>
+            <DialogTitle>Create New Template</DialogTitle>
+            <DialogDescription>
+              Create a template with recurring sessions that will repeat based
+              on your schedule.
+            </DialogDescription>
+          </DialogHeader>
+          <TemplateForm />
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
