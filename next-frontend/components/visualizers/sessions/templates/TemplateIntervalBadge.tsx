5b1253f90135af299dea8a8d577089c13746a0e2
Kobu <thekobu@gmail.com>
2025-06-27 14:21:05 +0200

    fix: use CalendarSync icon in TemplateIntervalBadge

--- next-frontend/components/visualizers/sessions/templates/TemplateIntervalBadge.tsx
+++ next-frontend/components/visualizers/sessions/templates/TemplateIntervalBadge.tsx
@@ -1,5 +1,7 @@
 import { RecurringSessionInterval } from "@/api/definitions/models/session-template";
 import { Badge } from "@/components/shadcn/badge";
+import { cn } from "@/lib/utils";
+import { CalendarSync } from "lucide-react";
 import { FC } from "react";
 
 const getIntervalBadgeColor = (interval: string) => {
@@ -19,7 +21,13 @@ export const TemplateIntervalBadge: FC<{
   interval: RecurringSessionInterval;
 }> = (props) => {
   return (
-    <Badge className={getIntervalBadgeColor(props.interval)}>
+    <Badge
+      className={cn(
+        getIntervalBadgeColor(props.interval),
+        "flex items-center gap-2",
+      )}
+    >
+      <CalendarSync className="size-3" />
       {props.interval}
     </Badge>
   );
