c8d2cb573fada3a3f68f8e0e08c75aa633e14e02
Kobu <thekobu@gmail.com>
2025-07-21 16:27:06 +0200

    refactor: remove ClerkProvider from root layout

--- next-frontend/app/layout.tsx
+++ next-frontend/app/layout.tsx
@@ -1,48 +1,41 @@
-"use client";
-import "@/styles/globals.css";
+"use server";
+
 import { fontSans } from "@/lib/fonts";
 import { cn } from "@/lib/utils";
-import { ClerkProvider } from "@clerk/nextjs";
+import "@/styles/globals.css";
 import Head from "next/head";
-import { ThemeProvider } from "@/components/pages/theme-provider";
 
 interface RootLayoutProps {
   children: React.ReactNode;
 }
 
-export default function RootLayout({ children }: RootLayoutProps) {
+export default async function RootLayout({ children }: RootLayoutProps) {
   return (
-    <ClerkProvider>
-      <html lang="en" suppressHydrationWarning>
-        <Head>
-          <link rel="icon" href="/favicon.ico" sizes="any" />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
-        </Head>
+    <html lang="en" suppressHydrationWarning className="dark">
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+      </Head>
 
-        <body
-          className={cn(
-            "min-h-screen bg-background font-sans antialiased",
-            fontSans.variable,
-          )}
-        >
-          <main>
-            <ThemeProvider attribute="class" defaultTheme="dark">
-              {children}
-            </ThemeProvider>
-          </main>
-        </body>
-      </html>
-    </ClerkProvider>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
+        <main>{children}</main>
+      </body>
+    </html>
   );
 }
