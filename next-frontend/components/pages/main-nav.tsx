70712ef11ca7569fc8fcfc579761d454801ab345
Kobu <thekobu@gmail.com>
2025-07-21 16:26:06 +0200

    refactor: simplify Navigation component

--- next-frontend/components/pages/main-nav.tsx
+++ next-frontend/components/pages/main-nav.tsx
@@ -1,16 +1,10 @@
 "use client";
 
-import { SignedIn, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-import { SignInButton } from "@/components/pages/SignInButton";
-import { ThemeToggle } from "@/components/pages/ThemeToggle";
-import { SessionTimer } from "@/components/visualizers/sessions/StartSession";
-import { siteConfig } from "@/config/site";
 import { cn } from "@/lib/utils";
 import { NavItem } from "@/types/nav";
-import Image from "next/image";
 import { FC, PropsWithChildren } from "react";
 
 interface NavigationProps {
@@ -49,37 +43,3 @@ export const Navigation: FC<PropsWithChildren<NavigationProps>> = (props) => {
     </nav>
   );
 };
-
-export const NowasterLogo: FC<{ href?: string }> = (props) => {
-  return (
-    <Link
-      href={props.href ?? "/"}
-      className="flex items-center space-x-2 hover:scale-110 hover:transition"
-    >
-      <Image
-        src="/logo.png"
-        alt="Logo"
-        className="h-8 w-8"
-        width={80}
-        height={80}
-      />
-      <span className="inline-block font-bold">{siteConfig.name}</span>
-    </Link>
-  );
-};
-
-export function MainNavigation({ items }: NavigationProps) {
-  return (
-    <div className="flex w-full flex-row gap-6">
-      <NowasterLogo href="/home/" />
-      <Navigation items={items} />
-      <div className="grow"></div>
-      <SessionTimer />
-      <ThemeToggle />
-      <SignInButton />
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
-  );
-}
