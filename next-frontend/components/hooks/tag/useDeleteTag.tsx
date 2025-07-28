58d54b9fd9530e38192bfed5dfaab816afed98e4
Kobu <thekobu@gmail.com>
2025-07-28 14:53:44 +0200

    feat: add tag deletion

---
+++ next-frontend/components/hooks/tag/useDeleteTag.tsx
@@ -0,0 +1,15 @@
+import * as TagApi from "@/api/tagApi";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { queryKeys } from "../queryHooks/queryKeys";
+
+export const useDeleteTag = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (params: { id: string }) =>
+      await TagApi.deleteTag(params),
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: queryKeys.tags._def });
+    },
+  });
+};
