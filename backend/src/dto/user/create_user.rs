674bdc5592dca0ac56258781a7a6d544d467187d
Kobu <thekobu@gmail.com>
2025-08-04 19:35:50 +0200

    feat: store avatars in main DB

--- backend/src/dto/user/create_user.rs
+++ backend/src/dto/user/create_user.rs
@@ -3,6 +3,8 @@ use validator::Validate;
 
 #[derive(Clone, Deserialize, Serialize, Debug, Validate)]
 pub struct CreateUserDto {
+    #[serde(rename = "avatarUrl")]
+    pub avatar_url: Option<String>,
     pub username: String,
     pub id: String,
 }
