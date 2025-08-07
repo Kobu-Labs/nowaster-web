c05283441037cc20b73aae120fdc5ae227c45430
Kobu <thekobu@gmail.com>
2025-08-07 23:26:03 +0200

    feat: implemet frontend interface to manage subscriptions

---
+++ next-frontend/app/home/settings/layout.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { SettingsSidebar } from "@/components/pages/settings/SettingsSidebar";
+import { SidebarInset, SidebarProvider } from "@/components/shadcn/sidebar";
+
+interface SettingsLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function SettingsLayout({ children }: SettingsLayoutProps) {
+  return (
+    <div className="flex h-full">
+      <SidebarProvider defaultOpen className="flex-1">
+        <SettingsSidebar />
+        <SidebarInset className="flex-1">
+          <div className="p-6">{children}</div>
+        </SidebarInset>
+      </SidebarProvider>
+    </div>
+  );
+}
