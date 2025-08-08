91cdeeab30e8f719ec303392281a05fd20f84c98
Kobu <thekobu@gmail.com>
2025-08-08 23:26:23 +0200

    feat: create settings page

---
+++ next-frontend/components/pages/settings/SettingsSidebar.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { Rss, Settings, User } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+} from "@/components/shadcn/sidebar";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const settingsNavItems = [
+  {
+    title: "Feed",
+    url: "/home/settings/feed",
+    icon: Rss,
+    description: "Manage your feed subscriptions",
+  },
+] as const;
+
+export function SettingsSidebar() {
+  const pathname = usePathname();
+
+  return (
+    <Sidebar
+      variant="sidebar"
+      className="w-64 border-r bg-transparent"
+      collapsible="none"
+    >
+      <SidebarHeader className="p-4">
+        <div className="flex items-center gap-2">
+          <Settings className="h-5 w-5" />
+          <h2 className="text-lg font-semibold">Settings</h2>
+        </div>
+      </SidebarHeader>
+
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Social</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {settingsNavItems.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild isActive={pathname === item.url}>
+                    <Link href={item.url} className="flex items-start gap-1">
+                      <div className="flex items-center gap-2">
+                        <item.icon className="h-4 w-4" />
+                        <span>{item.title}</span>
+                      </div>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
