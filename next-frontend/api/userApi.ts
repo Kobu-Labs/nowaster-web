18fb713aa5ffa34c9da2085dc0abd04038cf0837
Kobu <thekobu@gmail.com>
2025-08-09 14:09:58 +0200

    feat: add hook to fetch the current user

--- next-frontend/api/userApi.ts
+++ next-frontend/api/userApi.ts
@@ -19,3 +19,24 @@ export const create = async (
   const { data } = await baseApi.post(BASE_URL, params);
   return await parseResponseUnsafe(data, UserResponseSchema.create);
 };
+
+export const updateVisibility = async (
+  params: UserRequest["updateVisibility"],
+) => {
+  const { data } = await baseApi.patch(BASE_URL + "/visibility", params);
+  return await parseResponseUnsafe(data, UserResponseSchema.updateVisibility);
+};
+
+export const getCurrentUser = async () => {
+  const { data } = await baseApi.get(BASE_URL);
+  return await parseResponseUnsafe(data, UserResponseSchema.getProfile);
+};
+
+export const getProfile = async (
+  params?: UserRequest["getProfile"],
+) => {
+  const { data } = await baseApi.get(BASE_URL + "/profile", {
+    params: params ? { id: params.id } : undefined
+  });
+  return await parseResponseUnsafe(data, UserResponseSchema.getProfile);
+};
