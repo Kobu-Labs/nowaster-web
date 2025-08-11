580a3ef44e728967ec79ba39740b1ae66a1573ca
Kobu <thekobu@gmail.com>
2025-08-11 15:12:24 +0200

    feat: optimistically update running session

--- next-frontend/components/hooks/session/useUpdateSession.ts
+++ next-frontend/components/hooks/session/useUpdateSession.ts
@@ -3,9 +3,20 @@ import {
   ScheduledSessionRequest,
   StopwatchSessionRequest,
 } from "@/api/definitions";
+import {
+  CategoryWithId,
+  ScheduledSessionWithIdSchema,
+  StopwatchSessionWithIdSchema,
+  TagWithId,
+} from "@/api/definitions/models";
 import { queryKeys } from "@/components/hooks/queryHooks/queryKeys";
 import { useToast } from "@/components/shadcn/use-toast";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { z } from "zod";
+
+type ActiveSession =
+  | z.infer<typeof ScheduledSessionWithIdSchema>
+  | z.infer<typeof StopwatchSessionWithIdSchema>;
 
 export function useUpdateSession(
   sessionType: "stopwatch",
@@ -36,6 +47,58 @@ const useUpdateStopwatchSession = () => {
     mutationFn: async (data: StopwatchSessionRequest["update"]) => {
       return await StopwatchApi.update(data);
     },
+    onMutate: async (newData) => {
+      await queryClient.cancelQueries({
+        queryKey: queryKeys.sessions.active.queryKey,
+      });
+
+      const previousActiveSessions = queryClient.getQueryData(
+        queryKeys.sessions.active.queryKey,
+      );
+
+      const tags: TagWithId[] =
+        queryClient.getQueryData(queryKeys.tags.all.queryKey) ?? [];
+
+      const categories: CategoryWithId[] =
+        queryClient.getQueryData(queryKeys.categories.all.queryKey) ?? [];
+
+      let newCategories = undefined;
+      if (newData.category_id !== undefined) {
+        newCategories = categories.find(
+          (cat) => cat.id === newData.category_id,
+        );
+      }
+
+      let newTags = undefined;
+      if (newData.tag_ids !== undefined && newData !== null) {
+        newTags = tags.filter((tag) => newData.tag_ids?.includes(tag.id));
+      }
+
+      queryClient.setQueryData(
+        queryKeys.sessions.active.queryKey,
+        (old: ActiveSession[] | undefined) => {
+          if (!old) {
+            return undefined;
+          }
+
+          return old.map((session) => {
+            if (session.id === newData.id) {
+              const sessionTags = newTags ?? session.tags;
+              const sessionCategory = newCategories ?? session.category;
+              return {
+                ...session,
+                ...newData,
+                tags: sessionTags,
+                category: sessionCategory,
+              };
+            }
+            return session;
+          });
+        },
+      );
+
+      return { previousActiveSessions };
+    },
     onSuccess: async () => {
       await queryClient.invalidateQueries({
         queryKey: queryKeys.sessions.active.queryKey,
@@ -45,11 +108,22 @@ const useUpdateStopwatchSession = () => {
         variant: "default",
       });
     },
-    onError: (error) => {
+    onError: (error, _, context) => {
+      if (context?.previousActiveSessions) {
+        queryClient.setQueryData(
+          queryKeys.sessions.active.queryKey,
+          context.previousActiveSessions,
+        );
+      }
       toast({
         title: "Error updating session",
         description: error.message,
         variant: "destructive",
+      });
+    },
+    onSettled: async () => {
+      await queryClient.invalidateQueries({
+        queryKey: queryKeys.sessions.active.queryKey,
       });
     },
   });
