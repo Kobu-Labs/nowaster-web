53c8d036421eeecbc49151473045c58d8119d021
Kobu <thekobu@gmail.com>
2025-08-13 07:07:05 +0200

    feat: redesign dashboard page

--- next-frontend/app/home/DashboardPage.tsx
+++ next-frontend/app/home/DashboardPage.tsx
@@ -1,16 +1,15 @@
 "use client";
 
-import { CategoryWithId, ScheduledSession } from "@/api/definitions";
+import { CategoryWithId } from "@/api/definitions";
 import { queryKeys } from "@/components/hooks/queryHooks/queryKeys";
 import { Card, CardContent, CardHeader } from "@/components/shadcn/card";
-import { SessionPieChartUiProvider } from "@/components/ui-providers/session/charts/SessionPieChartUiProvider";
 import { CategoryBadge } from "@/components/visualizers/categories/CategoryBadge";
 import { Feed } from "@/components/visualizers/feed/Feed";
 import { FilteredSessionAreaChart } from "@/components/visualizers/sessions/charts/FilteredSessionAreaChart";
 import {
+  ActiveIndexContext,
   AmountByCategory,
-  groupData,
-  SessionPieChartProps,
+  SessionPieChart,
 } from "@/components/visualizers/sessions/charts/SessionPieChart";
 import { CurrentStreakKpiCard } from "@/components/visualizers/sessions/kpi/CurrentStreakKpiCard";
 import { TotalSessionTimeKpiCard } from "@/components/visualizers/sessions/kpi/TotalMinutesSpentKpiCard";
@@ -31,7 +30,7 @@ import {
   subHours,
 } from "date-fns";
 import { TrendingDown, TrendingUp, TrendingUpDown } from "lucide-react";
-import { createContext, FC, useContext, useMemo, useState } from "react";
+import { useContext, useMemo } from "react";
 
 export default function DashboardPage() {
   const timelineFilter: SessionFilterPrecursor = useMemo(
@@ -97,14 +96,14 @@ export default function DashboardPage() {
     if (yesterdayMinutes === 0) {
       if (todayMinutes === 0) {
         return (
-          <div className="flex gap-2">
+          <div className="flex gap-2 items-center">
             <TrendingUpDown className="text-red-500" />
             +0%
           </div>
         );
       }
       return (
-        <div className="flex gap-2">
+        <div className="flex gap-2 items-center">
           <TrendingUp className="text-green-400" />
           +100%
         </div>
@@ -114,7 +113,7 @@ export default function DashboardPage() {
     const change = ((todayMinutes - yesterdayMinutes) / yesterdayMinutes) * 100;
     if (change >= 0) {
       return (
-        <div className="flex gap-2">
+        <div className="flex gap-2 items-center">
           <TrendingUp className="text-green-400" />
           {`+${Math.round(change)}% vs yesterday`}
         </div>
@@ -122,7 +121,7 @@ export default function DashboardPage() {
     }
 
     return (
-      <div className="flex gap-2">
+      <div className="flex gap-2 items-center">
         <TrendingDown className="text-red-500" />
         {`${Math.round(change)}% vs yesterday`}
       </div>
@@ -131,7 +130,7 @@ export default function DashboardPage() {
 
   return (
     <div className="flex grow flex-col p-4 md:p-8 gap-4 md:gap-8">
-      <h2 className="mb-8 text-3xl font-bold tracking-tight">Dashboard</h2>
+      <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
         <TotalSessionTimeCard
           filter={todayFilter}
@@ -153,18 +152,21 @@ export default function DashboardPage() {
             <FilteredSessionTimeline />
           </div>
         </FilterContextProvider>
-        <Card className="col-span-full md:col-span-2">
-          <CardHeader className="p-2">
+        <Card className="col-span-full md:col-span-2 flex flex-col">
+          <CardHeader className="p-0 pt-4 pl-4">
             <h2 className="text-xl font-bold tracking-tight">
               Today&apos;s categories
             </h2>
           </CardHeader>
-          <CardContent className="p-2">
-            <SessionPieChartExpanded
+          <CardContent className="p-2 flex flex-col justify-center grow">
+            <SessionPieChart
               filter={todayFilter}
               getKey={(session) => ({
                 key: session.category.name,
-                metadata: { category: session.category },
+                metadata: {
+                  category: session.category,
+                  color: session.category.color,
+                },
               })}
               renderLegend={(props: {
                 data: AmountByCategory<{
@@ -172,7 +174,7 @@ export default function DashboardPage() {
                 }>[];
               }) => {
                 return (
-                  <div className="flex flex-col items-start flex-grow w-full gap-1">
+                  <div className="flex flex-col items-center flex-grow w-full gap-1">
                     {props.data.map((val, i) => {
                       const context = useContext(ActiveIndexContext);
 
@@ -181,7 +183,7 @@ export default function DashboardPage() {
                           key={val.key}
                           onMouseEnter={() => context?.setIndex(i)}
                           onMouseLeave={() => context?.setIndex(null)}
-                          className="flex items-center gap-2"
+                          className="flex items-center gap-2 justify-between w-full hover:bg-pink-muted rounded px-2"
                         >
                           {val.metadata && (
                             <CategoryBadge
@@ -199,14 +201,14 @@ export default function DashboardPage() {
             />
           </CardContent>
         </Card>
-        <Card className="col-span-full md:col-span-2">
-          <CardHeader className="p-2">
+        <Card className="col-span-full md:col-span-2 flex flex-col">
+          <CardHeader className="p-0 pt-4 pl-4">
             <h2 className="text-xl font-bold tracking-tight">
               Today&apos;s tags
             </h2>
           </CardHeader>
-          <CardContent className="p-2">
-            <SessionPieChartExpanded
+          <CardContent className="p-2 flex flex-col justify-center grow">
+            <SessionPieChart
               filter={todayFilter}
               getKey={(session) =>
                 session.tags.length
@@ -226,7 +228,7 @@ export default function DashboardPage() {
                 }>[];
               }) => {
                 return (
-                  <div className="flex flex-col items-start flex-grow w-full gap-1">
+                  <div className="flex flex-col items-center flex-grow w-full gap-1">
                     {props.data.map((val, i) => {
                       const context = useContext(ActiveIndexContext);
 
@@ -235,7 +237,7 @@ export default function DashboardPage() {
                           key={val.key}
                           onMouseEnter={() => context?.setIndex(i)}
                           onMouseLeave={() => context?.setIndex(null)}
-                          className="flex items-center gap-2"
+                          className="flex items-center gap-2 justify-between w-full hover:bg-pink-muted rounded px-2"
                         >
                           {val.metadata && (
                             <TagBadge
@@ -254,77 +256,11 @@ export default function DashboardPage() {
             />
           </CardContent>
         </Card>
-        <div className="col-span-2">
+        <div className="col-span-full">
+          <h2 className="text-xl my-4 font-bold">See what others are doing</h2>
           <Feed />
         </div>
       </div>
     </div>
   );
 }
-type AdditionalProps = {
-  renderLegend?: FC<{ data: AmountByCategory[] }>;
-};
-
-type ActiveIndexContextType = {
-  index: number | null;
-  setIndex: (value: number | null) => void;
-};
-
-export const ActiveIndexContext = createContext<
-  ActiveIndexContextType | undefined
->(undefined);
-
-const SessionPieChartExpanded2 = (
-  props: SessionPieChartProps & AdditionalProps,
-) => {
-  // differenceInMinutes as default grouping method
-  const groupingMethod =
-    props.groupingMethod ??
-    ((session: ScheduledSession) =>
-      differenceInMinutes(session.endTime, session.startTime));
-
-  const { data: result } = useQuery({
-    ...queryKeys.sessions.filtered(props.filter),
-    retry: false,
-    select: (data) => {
-      return groupData(data, props.getKey, groupingMethod);
-    },
-    refetchOnWindowFocus: false,
-    refetchOnReconnect: false,
-  });
-
-  const context = useContext(ActiveIndexContext);
-  if (!context) {
-    return null;
-  }
-  const setIndex = (val: number | undefined) => {
-    if (val !== undefined) {
-      context.setIndex(val);
-    } else {
-      context.setIndex(null);
-    }
-  };
-
-  return (
-    <div className="flex items-center">
-      <SessionPieChartUiProvider
-        data={result ?? []}
-        activeIndex={context.index}
-        onActiveIndexChange={setIndex}
-      />
-      {props.renderLegend && props.renderLegend({ data: result ?? [] })}
-    </div>
-  );
-};
-
-export const SessionPieChartExpanded: FC<
-  SessionPieChartProps & AdditionalProps
-> = (props) => {
-  const [filter, setFilter] = useState<number | null>(null);
-
-  return (
-    <ActiveIndexContext.Provider value={{ index: filter, setIndex: setFilter }}>
-      <SessionPieChartExpanded2 {...props} />
-    </ActiveIndexContext.Provider>
-  );
-};
