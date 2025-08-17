080b23accdb3337b044b7174795eb2b64b7a91c6
Kobu <thekobu@gmail.com>
2025-08-17 16:44:35 +0200

    refactor: place notification items to separate folder

---
+++ next-frontend/components/notifications/items/NewFriendRequestNotification.tsx
@@ -0,0 +1,9 @@
+import { NewFriendRequestData } from "@/api/definitions/models/notification";
+import { NotificationItemProps } from "@/components/notifications/NotificationItem";
+import { FC } from "react";
+
+export const NewFriendRequestNotificationItem: FC<
+  NotificationItemProps & { data: NewFriendRequestData }
+> = (props) => {
+  return null;
+};
---
+++ next-frontend/components/notifications/items/NewReleaseNotification.tsx
@@ -0,0 +1,9 @@
+import { NewReleaseData } from "@/api/definitions/models/notification";
+import { NotificationItemProps } from "@/components/notifications/NotificationItem";
+import { FC } from "react";
+
+export const NewReleaseNotificationItem: FC<
+  NotificationItemProps & { data: NewReleaseData }
+> = (props) => {
+  return null;
+};
