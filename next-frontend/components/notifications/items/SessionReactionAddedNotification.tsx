3f68a0fe8334a0403587c3288517e8e72211aa49
Kobu <thekobu@gmail.com>
2025-08-17 16:44:35 +0200

    refactor: place notification items to separate folder

---
+++ next-frontend/components/notifications/items/SessionReactionAddedNotification.tsx
@@ -0,0 +1,9 @@
+import { SessionReactionAddedData } from "@/api/definitions/models/notification";
+import { NotificationItemProps } from "@/components/notifications/NotificationItem";
+import { FC } from "react";
+
+export const SessionReactionAddedNotificationItem: FC<
+  NotificationItemProps & { data: SessionReactionAddedData }
+> = (props) => {
+  return null;
+};
