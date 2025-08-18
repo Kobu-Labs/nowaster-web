e66b542c9b674dceb6df9232921681b311fbf2be
Kobu <thekobu@gmail.com>
2025-08-18 09:30:28 +0200

    feat: add tooltip with username to UserAvatar

--- next-frontend/components/visualizers/user/UserAvatar.tsx
+++ next-frontend/components/visualizers/user/UserAvatar.tsx
@@ -1,10 +1,15 @@
-import { getInitials } from "@/lib/utils";
 import {
   Avatar,
-  AvatarImage,
   AvatarFallback,
+  AvatarImage,
 } from "@/components/shadcn/avatar";
+import { getInitials } from "@/lib/utils";
 import { FC } from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/shadcn/tooltip";
 
 type UserAvatarProps = {
   avatar_url?: string | null;
@@ -13,11 +18,21 @@ type UserAvatarProps = {
 
 export const UserAvatar: FC<UserAvatarProps> = (props) => {
   return (
-    <Avatar className="h-6 w-6">
-      <AvatarImage src={props.avatar_url ?? undefined} alt={props.username} />
-      <AvatarFallback className="bg-primary/10 text-primary font-medium text-xs">
-        {getInitials(props.username)}
-      </AvatarFallback>
-    </Avatar>
+    <Tooltip>
+      <TooltipTrigger className="cursor-default">
+        <Avatar className="h-6 w-6">
+          <AvatarImage
+            src={props.avatar_url ?? undefined}
+            alt={props.username}
+          />
+          <AvatarFallback className="bg-primary/10 text-primary font-medium text-xs">
+            {getInitials(props.username)}
+          </AvatarFallback>
+        </Avatar>
+      </TooltipTrigger>
+      <TooltipContent>
+        <p>{props.username}</p>
+      </TooltipContent>
+    </Tooltip>
   );
 };
