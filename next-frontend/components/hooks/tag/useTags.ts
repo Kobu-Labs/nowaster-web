b3b6b2f9d237dc82fc1673175c338d026e5eb63c
Kobu <thekobu@gmail.com>
2025-08-21 11:32:56 +0200

    feat: keep previous data on tag refetches

--- next-frontend/components/hooks/tag/useTags.ts
+++ next-frontend/components/hooks/tag/useTags.ts
@@ -1,9 +1,10 @@
 import { queryKeys } from "@/components/hooks/queryHooks/queryKeys";
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 
 export const useTags = () => {
   return useQuery({
     ...queryKeys.tags.all,
     staleTime: Infinity,
+    placeholderData: keepPreviousData,
   });
 };
