7891dd984fcba6cf96218e701d5f86f2ca51c4e6
Kobu <thekobu@gmail.com>
2025-08-28 17:30:36 +0200

    feat: close sidebar on logo click

--- next-frontend/components/pages/NowasterLogo.tsx
+++ next-frontend/components/pages/NowasterLogo.tsx
@@ -1,13 +1,19 @@
+"use client";
+
 import { siteConfig } from "@/config/site";
 import Link from "next/link";
 import Image from "next/image";
 import { FC } from "react";
+import { useSidebar } from "@/components/shadcn/sidebar";
 
 export const NowasterLogo: FC<{ href?: string }> = (props) => {
+  const { setOpen } = useSidebar();
+
   return (
     <Link
       href={props.href ?? "/"}
       className="flex items-center space-x-2 hover:scale-110 hover:transition"
+      onClick={() => setOpen(false)}
     >
       <Image
         src="/logo.png"
