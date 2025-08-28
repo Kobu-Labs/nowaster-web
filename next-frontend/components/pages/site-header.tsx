962a647737836fcecc7f2084c7bbb9b3e4c72432
Kobu <thekobu@gmail.com>
2025-08-28 19:06:25 +0200

    fix: use simple link on site header

--- next-frontend/components/pages/site-header.tsx
+++ next-frontend/components/pages/site-header.tsx
@@ -1,9 +1,9 @@
 import { Navigation } from "@/components/pages/main-nav";
-import { NowasterLogo } from "@/components/pages/NowasterLogo";
 import { Button } from "@/components/shadcn/button";
 import { NavItem } from "@/types/nav";
 import { ArrowBigRight } from "lucide-react";
 import Link from "next/link";
+import Image from "next/image";
 
 const landingPageNav: NavItem[] = [] as const;
 
@@ -12,7 +12,19 @@ export function SiteHeader() {
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center justify-between w-full">
         <div className="flex items-center gap-2 w-full">
-          <NowasterLogo />
+          <Link
+            href="/"
+            className="flex items-center space-x-2 hover:scale-110 hover:transition"
+          >
+            <Image
+              src="/logo.png"
+              alt="Logo"
+              className="h-8 w-8"
+              width={80}
+              height={80}
+            />
+            <span className="inline-block font-bold">Nowaster</span>
+          </Link>
           <Navigation items={landingPageNav}></Navigation>
           <div className="grow"></div>
           <Link href="/home/">
