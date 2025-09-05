550c6334073f40c16d9c35a1f0fc90a16360ede1
Kobu <thekobu@gmail.com>
2025-09-05 18:56:56 +0200

    feat: make table header sticky

--- next-frontend/components/ui-providers/DataTable.tsx
+++ next-frontend/components/ui-providers/DataTable.tsx
@@ -55,7 +55,7 @@ export function DataTable<TData, TValue>({
   return (
     <div className="rounded-md border">
       <Table>
-        <TableHeader>
+        <TableHeader className="sticky top-0">
           {table.getHeaderGroups().map((headerGroup) => (
             <TableRow key={headerGroup.id}>
               {headerGroup.headers.map((header) => {
