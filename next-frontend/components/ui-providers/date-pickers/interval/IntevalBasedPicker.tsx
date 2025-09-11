643debfa902a0200d46bdcb490daba8f21c4071d
Kobu <thekobu@gmail.com>
2025-09-11 21:45:24 +0200

    feat: improve responsivness of templates

--- next-frontend/components/ui-providers/date-pickers/interval/IntevalBasedPicker.tsx
+++ next-frontend/components/ui-providers/date-pickers/interval/IntevalBasedPicker.tsx
@@ -7,14 +7,9 @@ import {
 } from "components/shadcn/popover";
 import type { RecurringSessionInterval } from "@/api/definitions/models/session-template";
 import { Card } from "@/components/shadcn/card";
-import type {
-  DailyIntervalPickerProps } from "@/components/ui-providers/date-pickers/interval/DailyIntervalPicker";
-import {
-  DailyIntervalPicker,
-} from "@/components/ui-providers/date-pickers/interval/DailyIntervalPicker";
-import {
-  WeeklyIntervalPicker,
-} from "@/components/ui-providers/date-pickers/interval/WeeklyIntervalPicker";
+import type { DailyIntervalPickerProps } from "@/components/ui-providers/date-pickers/interval/DailyIntervalPicker";
+import { DailyIntervalPicker } from "@/components/ui-providers/date-pickers/interval/DailyIntervalPicker";
+import { WeeklyIntervalPicker } from "@/components/ui-providers/date-pickers/interval/WeeklyIntervalPicker";
 import type { FC } from "react";
 import { Button } from "@/components/shadcn/button";
 import { cn } from "@/lib/utils";
@@ -41,19 +36,19 @@ export const IntervalBasedPicker: FC<IntervalBasedPickerProps> = (props) => {
   const Component = intervalToPicker[props.interval];
   return (
     <Popover modal={false}>
-      <PopoverTrigger className="p-1">
+      <PopoverTrigger className="p-1 w-full">
         <Button
-          className={cn("w-[240px] justify-start text-left font-normal")}
+          className={cn("w-full justify-start text-left font-normal")}
           type="button"
           variant="outline"
         >
-          <CalendarIcon className="mr-2 size-4" />
-          <span>
+          <CalendarIcon className="mr-2 size-4 shrink-0" />
+          <span className="truncate">
             {formatIntervalPickerLabel(props.interval, props.selected)}
           </span>
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="p-0 m-0 border-none w-[400px]">
+      <PopoverContent className="p-0 m-0 border-none w-auto min-w-[300px] max-w-[400px]">
         <Component {...props} />
       </PopoverContent>
     </Popover>
