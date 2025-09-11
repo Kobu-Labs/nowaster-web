fe0b63387a2f6a1655457fc22cf08f54fe1942e9
Kobu <thekobu@gmail.com>
2025-09-11 19:37:38 +0200

    fix: make HoverPercentageBar work with mobile

--- next-frontend/components/ui-providers/HoverPercentageBar.tsx
+++ next-frontend/components/ui-providers/HoverPercentageBar.tsx
@@ -11,14 +11,17 @@ export const HoverPercentageBar: FC<
   const [hoverX, setHoverX] = useState<null | number>(null);
   const [percentage, setPercentage] = useState<null | number>(null);
 
-  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
-    const rect = e.currentTarget.getBoundingClientRect();
-    const x = e.clientX - rect.left;
+  const handlePointerMove = (
+    clientX: number,
+    currentTarget: HTMLDivElement,
+  ) => {
+    const rect = currentTarget.getBoundingClientRect();
+    const x = clientX - rect.left;
     setHoverX(x);
     setPercentage(Math.min(100, Math.max(0, (x / rect.width) * 100)));
   };
 
-  const handleMouseLeave = () => {
+  const handlePointerLeave = () => {
     setHoverX(null);
     setPercentage(null);
   };
@@ -26,8 +29,15 @@ export const HoverPercentageBar: FC<
   return (
     <div
       className="relative w-full h-full border rounded-2xl overflow-hidden py-4 bg-transparent/30"
-      onMouseLeave={handleMouseLeave}
-      onMouseMove={handleMouseMove}
+      onMouseLeave={handlePointerLeave}
+      onMouseMove={(e) => handlePointerMove(e.clientX, e.currentTarget)}
+      onTouchEnd={handlePointerLeave}
+      onTouchMove={(e) => {
+        if (e.touches.length === 1) {
+          e.preventDefault();
+          handlePointerMove(e.touches[0]?.clientX ?? 0, e.currentTarget);
+        }
+      }}
     >
       {hoverX !== null && (
         <>
@@ -37,7 +47,10 @@ export const HoverPercentageBar: FC<
           />
           <div
             className="absolute top-0 bottom-0 text-nowrap"
-            style={{ left: hoverX + 10 }}
+            style={{
+              left: (percentage ?? 0) < 50 ? hoverX + 10 : hoverX - 10,
+              transform: (percentage ?? 0) >= 50 ? "translateX(-100%)" : "none",
+            }}
           >
             {percentage && props.formatter(percentage)}
           </div>
