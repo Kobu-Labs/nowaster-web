8e7ad4c037bddd2170739c42434db0e9ed5443de
Kobu <thekobu@gmail.com>
2025-09-11 20:07:44 +0200

    fix: dialog responsivness fixes

--- next-frontend/app/home/history/page.tsx
+++ next-frontend/app/home/history/page.tsx
@@ -9,15 +9,20 @@ export const metadata: Metadata = {
 
 export default function HistoryPage() {
   return (
-    <div className="m-8 grow">
-      <BaseSessionTable
-        filter={{
-          data: {
-            endTimeTo: { value: endOfDay(new Date()) },
-          },
-          settings: {},
-        }}
-      />
+    <div className="flex grow flex-col p-4 md:p-8 gap-4 md:gap-8">
+      <h2 className="text-3xl font-bold tracking-tight">Session History</h2>
+      <div className="grid grid-cols-1">
+        <div className="col-span-full">
+          <BaseSessionTable
+            filter={{
+              data: {
+                endTimeTo: { value: endOfDay(new Date()) },
+              },
+              settings: {},
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
