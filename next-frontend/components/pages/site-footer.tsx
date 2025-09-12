fd353dda5244cec3f5bbde3ea6a3f6d9d2fcc0c4
Kobu <thekobu@gmail.com>
2025-09-13 01:05:04 +0200

    feat: improve responsivness of landing page

--- next-frontend/components/pages/site-footer.tsx
+++ next-frontend/components/pages/site-footer.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/shadcn/button";
-import { Clock, Github } from "lucide-react";
+import { Github } from "lucide-react";
 import Link from "next/link";
 
 export const SiteFooter = () => {
@@ -7,11 +7,8 @@ export const SiteFooter = () => {
     <footer className="border-t py-6 md:py-0">
       <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
         <div className="flex flex-col items-center gap-4 md:flex-row md:gap-2">
-          <Clock className="h-6 w-6" />
           <p className="text-center text-sm leading-loose md:text-left">
-            &copy;
-            {new Date().getFullYear()}
-            Nowaster. All rights reserved.
+            &copy; All rights reserved
           </p>
         </div>
         <Button className="flex items-center gap-4" variant="link">
