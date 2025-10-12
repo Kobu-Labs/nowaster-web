f25689892abefd573c1ceac8df9da4ade37a4045
Kobu <thekobu@gmail.com>
2025-10-12 16:54:51 +0100

    feat: implement TogglableTooltip

---
+++ next-frontend/components/ui-providers/TogglableTooltip.tsx
@@ -0,0 +1,33 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/shadcn/tooltip";
+import { FC, PropsWithChildren, ReactNode } from "react";
+
+type TogglableTooltipProps = {
+  showTooltip: boolean;
+  tooltipContent?: ReactNode;
+};
+
+export const TogglableTooltip: FC<PropsWithChildren<TogglableTooltipProps>> = (
+  props,
+) => {
+  if (props.showTooltip) {
+    return (
+      <TooltipProvider delayDuration={0}>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <span className="inline-flex" tabIndex={0}>
+              {props.children}
+            </span>
+          </TooltipTrigger>
+          <TooltipContent>{props.tooltipContent}</TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    );
+  }
+
+  return props.children;
+};
