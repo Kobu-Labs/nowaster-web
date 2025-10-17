091d6a77f3fd6f04852266aecf49b3663fb502cb
Kobu <thekobu@gmail.com>
2025-10-18 00:55:15 +0100

    refactor: rewrite update call with COALESCE

--- backend/src/repository/user.rs
+++ backend/src/repository/user.rs
@@ -2,11 +2,12 @@ use anyhow::{anyhow, Result};
 use sqlx::{Postgres, QueryBuilder};
 use std::sync::Arc;
 use tracing::instrument;
+use validator::Validate;
 
 use crate::{
     config::database::{Database, DatabaseTrait},
     dto::user::{
-        create_user::CreateUserDto, update_user::UpdateUserDto,
+        update_user::UpdateUserDto,
         update_visibility::UpdateVisibilityDto,
     },
     entity::{user::User, visibility::VisibilityFlags},
@@ -46,63 +47,24 @@ impl UserRepository {
     }
 
     #[instrument(err, skip(self))]
-    pub async fn create(&self, dto: CreateUserDto) -> Result<User> {
-        let row = sqlx::query_as!(
+    pub async fn update(&self, dto: UpdateUserDto) -> Result<User> {
+        let result = sqlx::query_as!(
             ReadUserRow,
             r#"
-                    INSERT INTO "user" (id, displayname, avatar_url, visibility_flags)
-                    VALUES ($1, $2, $3, $4)
-                    RETURNING id, displayname, avatar_url, visibility_flags
+                UPDATE "user" u SET
+                    displayname = COALESCE($1, u.displayname),
+                    avatar_url = COALESCE($2, u.avatar_url)
+                WHERE u.id = $3
+                RETURNING u.id, u.displayname, u.avatar_url, u.visibility_flags
             "#,
-            dto.id,
             dto.username,
             dto.avatar_url,
-            VisibilityFlags::default().as_raw()
+            dto.id
         )
         .fetch_one(self.db_conn.get_pool())
         .await?;
 
-        Ok(self.mapper(row))
-    }
-
-    #[instrument(err, skip(self))]
-    pub async fn update(&self, dto: UpdateUserDto) -> Result<User> {
-        let mut should_execute = false;
-        let mut query: QueryBuilder<'_, Postgres> = QueryBuilder::new(
-            r#"
-                UPDATE "user" SET
-            "#,
-        );
-
-        if let Some(displayname) = dto.username {
-            if displayname.is_empty() {
-                return Err(anyhow::anyhow!("Display name cannot be empty"));
-            }
-            query.push(" displayname = ");
-            query.push_bind(displayname);
-            should_execute = true;
-        }
-
-        if let Some(avatar_url) = dto.avatar_url {
-            query.push(" avatarUrl = ");
-            query.push_bind(avatar_url);
-            should_execute = true;
-        }
-
-        query.push(" WHERE id = ");
-        query.push_bind(dto.id);
-        query.push(" RETURNING id, displayname, avatar_url, visibility_flags");
-
-        if !should_execute {
-            return Err(anyhow::anyhow!("No fields to update"));
-        }
-
-        let row = query
-            .build_query_as::<ReadUserRow>()
-            .fetch_one(self.db_conn.get_pool())
-            .await?;
-
-        Ok(self.mapper(row))
+        Ok(self.mapper(result))
     }
 
     fn mapper(&self, row: ReadUserRow) -> User {
@@ -111,32 +73,6 @@ impl UserRepository {
             username: row.displayname,
             avatar_url: row.avatar_url,
             visibility_flags: row.visibility_flags,
-        }
-    }
-
-    #[instrument(err, skip(self))]
-    pub async fn upsert(&self, dto: CreateUserDto) -> Result<Option<User>> {
-        let row = sqlx::query_as!(
-            ReadUserRow,
-            r#"
-                INSERT INTO "user" (id, displayname, visibility_flags)
-                VALUES ($1, $2, $3)
-                ON CONFLICT (id) DO NOTHING
-                RETURNING id, displayname, avatar_url, visibility_flags
-            "#,
-            dto.id,
-            dto.username,
-            VisibilityFlags::default().as_raw()
-        )
-        .fetch_optional(self.db_conn.get_pool())
-        .await?;
-
-        match row {
-            Some(row) => {
-                let user = self.mapper(row);
-                Ok(Some(user))
-            }
-            None => Ok(None),
         }
     }
 
@@ -157,13 +93,15 @@ impl UserRepository {
         .fetch_optional(self.db_conn.get_pool())
         .await?;
 
-        Ok(query.map(|q| (
-            Actor {
-                user_id: q.id.clone(),
-                role: q.role,
-            },
-            q.displayname
-        )))
+        Ok(query.map(|q| {
+            (
+                Actor {
+                    user_id: q.id.clone(),
+                    role: q.role,
+                },
+                q.displayname,
+            )
+        }))
     }
     #[instrument(err, skip(self))]
     pub async fn filter_users(&self, filter: FilterUsersDto) -> Result<Vec<User>> {
@@ -198,7 +136,9 @@ impl UserRepository {
 
         if let Some(name) = filter.name {
             let pattern = format!("%{}%", name);
-            separated.push("displayname ILIKE ").push_bind_unseparated(pattern);
+            separated
+                .push("displayname ILIKE ")
+                .push_bind_unseparated(pattern);
         }
 
         let rows = query
