a260fea527f8fda3b797491add4ab6685f74b71c
Kobu <thekobu@gmail.com>
2025-10-19 00:55:45 +0100

    fix: disallow usernames to be shorter than 4 chars

--- backend/src/dto/user/update_user.rs
+++ backend/src/dto/user/update_user.rs
@@ -6,5 +6,6 @@ pub struct UpdateUserDto {
     #[serde(rename = "avatarUrl")]
     pub avatar_url: Option<String>,
     pub id: String,
+    #[validate(length(min = 4, message="Username must be at least 4 characters long"))]
     pub username: Option<String>,
 }
