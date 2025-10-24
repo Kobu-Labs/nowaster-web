4827463ac419d1734c0c20220f0180dbd941da34
Kobu <thekobu@gmail.com>
2025-10-24 16:00:32 +0100

    feat: add releases link to site-header

--- next-frontend/components/pages/site-header.tsx
+++ next-frontend/components/pages/site-header.tsx
@@ -2,6 +2,7 @@ import { SignedIn } from "@/components/auth/SignedIn";
 import { SignedOut } from "@/components/auth/SignedOut";
 import { WelcomeBackButton } from "@/components/auth/WelcomBackButton";
 import { Button } from "@/components/shadcn/button";
+import { Newspaper } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -21,7 +22,16 @@ export function SiteHeader() {
         />
         <span className="inline-block font-bold hidden xs:block">Nowaster</span>
       </Link>
-      <nav aria-label="Main navigation" className="flex-1 flex justify-end">
+      <nav
+        aria-label="Main navigation"
+        className="flex-1 flex justify-end items-center gap-4"
+      >
+        <Button className="flex items-center gap-4" variant="link">
+          <Link className="flex items-center gap-1 text-lg" href="/releases">
+            <Newspaper className="h-4 w-4" />
+            <span>Releases</span>
+          </Link>
+        </Button>
         <SignedIn>
           <Link href="/home">
             <WelcomeBackButton />
