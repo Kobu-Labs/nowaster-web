b2dc95791c7b1af1e3ba2e27e60fe0ee8d3966fb
Kobu <thekobu@gmail.com>
2025-11-01 12:57:36 +0100

    fix: add missing definitions

--- backend/src/entity/mod.rs
+++ backend/src/entity/mod.rs
@@ -1,9 +1,11 @@
 pub mod category;
 pub mod feed;
 pub mod notification;
+pub mod project;
 pub mod release;
 pub mod session;
 pub mod session_template;
 pub mod tag;
+pub mod task;
 pub mod user;
 pub mod visibility;
--- backend/src/router/mod.rs
+++ backend/src/router/mod.rs
@@ -13,3 +13,5 @@ pub mod session;
 pub mod statistics;
 pub mod tag;
 pub mod user;
+pub mod project;
+pub mod task;
