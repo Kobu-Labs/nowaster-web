c816b0b8f52f906f6e7e0bbe957ac081c52e7f4f
Kobu <thekobu@gmail.com>
2025-11-03 23:26:28 +0100

    feat: add filtering to project sidebar

--- next-frontend/components/visualizers/projects/ProjectSidebar.tsx
+++ next-frontend/components/visualizers/projects/ProjectSidebar.tsx
@@ -11,19 +11,21 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
   SidebarRail,
+  SidebarSeparator,
 } from "@/components/shadcn/sidebar";
 import { usePathname } from "next/navigation";
 import type { ProjectWithTaskCount } from "@/api/definitions/models/project";
 import { Badge } from "@/components/shadcn/badge";
 import { ProjectAvatar } from "@/components/visualizers/projects/ProjectAvatar";
 import { cn } from "@/lib/utils";
-import { CheckCircle2 } from "lucide-react";
-import { createContext, useContext, useState, type FC } from "react";
+import { CheckCircle2, Search } from "lucide-react";
+import { createContext, useContext, useMemo, useState, type FC } from "react";
 import { useTasksByProject } from "@/components/hooks/project";
 import { Button } from "@/components/shadcn/button";
 import Link from "next/link";
 import { useQueryClient } from "@tanstack/react-query";
 import { queryKeys } from "@/components/hooks/queryHooks/queryKeys";
+import { Input } from "@/components/shadcn/input";
 
 type ProjectSidebarProps = {
   projects: ProjectWithTaskCount[];
@@ -78,14 +80,14 @@ export const ProjectSidebarInner: FC<ProjectSidebarProps> = ({ projects }) => {
             showingTasks ? "-translate-x-1/2" : "translate-x-0",
           )}
         >
-          <div className="w-1/2 h-full shrink-0">
+          <div className="w-1/2 h-full shrink-0 flex flex-col">
             <ProjectsSidebarContent
               projects={projects}
               onSelectProject={context.setProject}
             />
           </div>
 
-          <div className="w-1/2 h-full shrink-0">
+          <div className="w-1/2 h-full shrink-0 flex flex-col">
             {context.project && (
               <TasksSidebarContent projectId={context.project.id} />
             )}
@@ -100,6 +102,36 @@ const ProjectsSidebarContent: FC<
   ProjectSidebarProps & { onSelectProject: (val: ProjectWithTaskCount) => void }
 > = ({ projects, onSelectProject }) => {
   const pathname = usePathname();
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
+  const queryClient = useQueryClient();
+
+  const prefetchProjectTasks = (projectId: string) => {
+    queryClient.prefetchQuery({
+      ...queryKeys.tasks.byProject(projectId),
+      staleTime: 20_000,
+    });
+  };
+  const prefetchProjectDetails = (projectId: string) => {
+    queryClient.prefetchQuery({
+      ...queryKeys.projects.byId(projectId),
+      staleTime: 20_000,
+    });
+  };
+
+  const prefetch = (projectId: string) => {
+    prefetchProjectTasks(projectId);
+    prefetchProjectDetails(projectId);
+  };
+
+  const filteredProjects = useMemo(
+    () =>
+      projects.filter((project) =>
+        project.name.toLowerCase().includes(searchTerm.toLowerCase()),
+      ),
+    [[searchTerm]],
+  );
+
   return (
     <>
       <SidebarHeader className="p-4">
@@ -110,46 +142,62 @@ const ProjectsSidebarContent: FC<
       <SidebarContent>
         <SidebarGroup className="my-2">
           <SidebarGroupLabel>Projets</SidebarGroupLabel>
+
+          <div className="relative">
+            <Search className="absolute left-3 top-3 h-4 w-4 text-muted-foreground" />
+            <Input
+              className="pl-10"
+              placeholder="Search projects..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </div>
+
+          <SidebarSeparator className="my-2 mr-2 bg-pink-muted" />
           <SidebarGroupContent>
             <SidebarMenu>
-              {projects.map((project) => (
-                <Link href={`/home/projects/${project.id}`} key={project.id}>
+              {filteredProjects.map((project) => (
+                <Link
+                  href={`/home/projects/${project.id}`}
+                  key={project.id}
+                  onMouseEnter={() => prefetch(project.id)}
+                >
                   <SidebarMenuItem>
-                    <SidebarMenuButton
+                    <Button
+                      variant="secondary"
+                      className="w-full flex items-center justify-start  h-14"
                       asChild
                       onClick={() => onSelectProject(project)}
                     >
-                      <div key={project.id}>
-                        <div
-                          className={cn(
-                            "flex items-center gap-3 p-3 rounded-lg hover:bg-accent transition-colors cursor-pointer",
-                            pathname === project.id && "bg-accent",
-                          )}
-                        >
-                          <ProjectAvatar
-                            color={project.color}
-                            imageUrl={project.image_url}
-                            name={project.name}
-                            size={32}
-                          />
-                          <div className="flex-1 min-w-0">
-                            <div className="flex items-center gap-2">
-                              <p className="text-sm font-medium truncate">
-                                {project.name}
-                              </p>
-                              {project.completed && (
-                                <CheckCircle2 className="h-3 w-3 text-green-600 flex-shrink-0" />
-                              )}
-                            </div>
-                            <div className="flex items-center gap-2 mt-1">
-                              <Badge className="text-xs" variant="outline">
-                                {project.taskCount} tasks
-                              </Badge>
-                            </div>
+                      <div
+                        className={cn(
+                          "flex items-center gap-3 p-3 rounded-lg hover:bg-accent transition-colors cursor-pointer",
+                          pathname === project.id && "bg-accent",
+                        )}
+                      >
+                        <ProjectAvatar
+                          color={project.color}
+                          imageUrl={project.image_url}
+                          name={project.name}
+                          size={32}
+                        />
+                        <div>
+                          <div className="flex items-center gap-2">
+                            <p className="text-sm font-medium truncate">
+                              {project.name}
+                            </p>
+                            {project.completed && (
+                              <CheckCircle2 className="h-3 w-3 text-green-600 flex-shrink-0" />
+                            )}
+                          </div>
+                          <div className="flex items-center gap-2 my-1">
+                            <Badge className="text-xs" variant="outline">
+                              {project.taskCount} tasks
+                            </Badge>
                           </div>
                         </div>
                       </div>
-                    </SidebarMenuButton>
+                    </Button>
                   </SidebarMenuItem>
                 </Link>
               ))}
@@ -170,11 +218,11 @@ const TasksSidebarContent: FC<{ projectId: string }> = (props) => {
   }
   return (
     <>
-      <SidebarHeader className="p-4">
+      <SidebarHeader className="p-4 flex-shrink-0">
         <Button onClick={() => context.setProject(null)}>Projects</Button>
       </SidebarHeader>
 
-      <SidebarContent>
+      <SidebarContent className="overflow-y-auto flex-1">
         <SidebarGroup className="my-2">
           <SidebarGroupLabel>Projets</SidebarGroupLabel>
           <SidebarGroupContent>
