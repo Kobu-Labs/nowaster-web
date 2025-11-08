17ed3fc0a60aa94b86bfb99af6d806028dba17f5
Kobu <thekobu@gmail.com>
2025-11-08 13:05:41 +0100

    refactor: simplify rendering of ProjectSidebar

--- next-frontend/components/visualizers/projects/ProjectSidebar.tsx
+++ next-frontend/components/visualizers/projects/ProjectSidebar.tsx
@@ -15,14 +15,13 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
-  SidebarRail,
   SidebarSeparator,
 } from "@/components/shadcn/sidebar";
 import { ProjectAvatar } from "@/components/visualizers/projects/ProjectAvatar";
 import { prefetchProjectDetails } from "@/lib/prefetch/prefetchProjectDetails";
 import { prefetchProjectTasks } from "@/lib/prefetch/prefetchProjectTasks";
 import { cn } from "@/lib/utils";
-import { CheckCircle2, Folders, Search } from "lucide-react";
+import { CheckCircle2, ChevronLeft, Folders, Search } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { type FC, useMemo, useState } from "react";
@@ -42,22 +41,39 @@ export const ProjectSidebar: FC<ProjectSidebarProps> = ({ projects }) => {
       variant="sidebar"
     >
       <div className="relative h-full w-full overflow-hidden">
+        <SidebarHeader>
+          <Button
+            asChild
+            className="w-full flex items-center justify-start gap-2 py-1 m-0"
+            variant="secondary"
+            onClick={() => setSelectedProject(null)}
+          >
+            <div>
+              {selectedProject ? (
+                <ChevronLeft className="size-4" />
+              ) : (
+                <Folders className="size-4" />
+              )}
+              Project Dashboard
+            </div>
+          </Button>
+        </SidebarHeader>
         <div
           className={cn(
             "flex h-full w-[200%] transition-transform duration-300 ease-[cubic-bezier(0.22,1,0.36,1)]",
-            showingTasks ? "-translate-x-1/2" : "translate-x-0",
+            selectedProject ? "-translate-x-1/2" : "translate-x-0",
           )}
         >
           <div className="w-1/2 h-full shrink-0 flex flex-col">
             <ProjectsSidebarContent
-              onSelectProject={context.setProject}
+              onSelectProject={setSelectedProject}
               projects={projects}
             />
           </div>
 
           <div className="w-1/2 h-full shrink-0 flex flex-col">
-            {context.project && (
-              <TasksSidebarContent projectId={context.project.id} />
+            {selectedProject && (
+              <TasksSidebarContent projectId={selectedProject.id} />
             )}
           </div>
         </div>
@@ -67,7 +83,7 @@ export const ProjectSidebar: FC<ProjectSidebarProps> = ({ projects }) => {
 };
 
 const ProjectsSidebarContent: FC<
-  { onSelectProject: (val: ProjectWithTaskCount) => void; } & ProjectSidebarProps
+  { onSelectProject: (val: ProjectWithTaskCount) => void } & ProjectSidebarProps
 > = ({ onSelectProject, projects }) => {
   const pathname = usePathname();
   const [searchTerm, setSearchTerm] = useState<string>("");
@@ -86,147 +102,120 @@ const ProjectsSidebarContent: FC<
   );
 
   return (
-    <>
-      <SidebarContent>
-        <SidebarGroup>
-          <Link className="w-full" href="/home/projects">
-            <Button
-              asChild
-              className="w-full flex items-center justify-start gap-2 py-1 m-0"
-              variant="secondary"
-            >
-              <div>
-                <Folders className="size-4" />
-                Project Dashboard
-              </div>
-            </Button>
-          </Link>
-        </SidebarGroup>
-        <SidebarGroup>
-          <SidebarGroupLabel>Projets</SidebarGroupLabel>
+    <SidebarContent>
+      <SidebarGroup>
+        <SidebarGroupLabel>Projets</SidebarGroupLabel>
 
-          <div className="relative">
-            <Search className="absolute left-3 top-3 h-4 w-4 text-muted-foreground" />
-            <Input
-              className="pl-10"
-              onChange={(e) => setSearchTerm(e.target.value)}
-              placeholder="Search projects..."
-              value={searchTerm}
-            />
-          </div>
+        <div className="relative">
+          <Search className="absolute left-3 top-3 h-4 w-4 text-muted-foreground" />
+          <Input
+            className="pl-10"
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Search projects..."
+            value={searchTerm}
+          />
+        </div>
 
-          <SidebarSeparator className="my-2 mr-2 bg-pink-muted" />
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {filteredProjects.map((project) => (
-                <Link
-                  href={`/home/projects/${project.id}`}
-                  key={project.id}
-                  onMouseEnter={() => prefetch(project.id)}
-                >
-                  <SidebarMenuItem>
-                    <Button
-                      asChild
-                      className="w-full flex items-center justify-start  h-14"
-                      onClick={() => onSelectProject(project)}
-                      variant="secondary"
+        <SidebarSeparator className="my-2 mr-2 bg-pink-muted" />
+        <SidebarGroupContent>
+          <SidebarMenu>
+            {filteredProjects.map((project) => (
+              <Link
+                href={`/home/projects/${project.id}`}
+                key={project.id}
+                onMouseEnter={() => prefetch(project.id)}
+              >
+                <SidebarMenuItem>
+                  <Button
+                    asChild
+                    className="w-full flex items-center justify-start  h-14"
+                    onClick={() => onSelectProject(project)}
+                    variant="secondary"
+                  >
+                    <div
+                      className={cn(
+                        "flex items-center gap-3 p-3 rounded-lg hover:bg-accent transition-colors cursor-pointer",
+                        pathname === project.id && "bg-accent",
+                      )}
                     >
-                      <div
-                        className={cn(
-                          "flex items-center gap-3 p-3 rounded-lg hover:bg-accent transition-colors cursor-pointer",
-                          pathname === project.id && "bg-accent",
-                        )}
-                      >
-                        <ProjectAvatar
-                          color={project.color}
-                          imageUrl={project.image_url}
-                          name={project.name}
-                          size={32}
-                        />
-                        <div>
-                          <div className="flex items-center gap-2">
-                            <p className="text-sm font-medium truncate">
-                              {project.name}
-                            </p>
-                            {project.completed && (
-                              <CheckCircle2 className="h-3 w-3 text-green-600 flex-shrink-0" />
-                            )}
-                          </div>
-                          <div className="flex items-center gap-2 my-1">
-                            <Badge className="text-xs" variant="outline">
-                              {project.taskCount}
-                              {" "}
-                              tasks
-                            </Badge>
-                          </div>
+                      <ProjectAvatar
+                        color={project.color}
+                        imageUrl={project.image_url}
+                        name={project.name}
+                        size={32}
+                      />
+                      <div>
+                        <div className="flex items-center gap-2">
+                          <p className="text-sm font-medium truncate">
+                            {project.name}
+                          </p>
+                          {project.completed && (
+                            <CheckCircle2 className="h-3 w-3 text-green-600 flex-shrink-0" />
+                          )}
                         </div>
-                      </div>
-                    </Button>
-                  </SidebarMenuItem>
-                </Link>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
-      <SidebarRail />
-    </>
-  );
-};
-
-const TasksSidebarContent: FC<{ projectId: string; }> = (props) => {
-  const project = useTasksByProject(props.projectId);
-  const context = useSidebarContext();
-  if (!project.data || project.isPending) {
-    return null;
-  }
-  return (
-    <>
-      <SidebarHeader className="p-4 flex-shrink-0">
-        <Button onClick={() => context.setProject(null)}>Projects</Button>
-      </SidebarHeader>
-
-      <SidebarContent className="overflow-y-auto flex-1">
-        <SidebarGroup className="my-2">
-          <SidebarGroupLabel>Projets</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {project.data.map((project) => (
-                <SidebarMenuItem key={project.name}>
-                  <SidebarMenuButton asChild>
-                    <div key={project.id}>
-                      <div
-                        className={cn(
-                          "flex items-center gap-3 p-3 rounded-lg hover:bg-accent transition-colors cursor-pointer",
-                        )}
-                      >
-                        <div className="flex-1 min-w-0">
-                          <div className="flex items-center gap-2">
-                            <p className="text-sm font-medium truncate">
-                              {project.name}
-                            </p>
-                            {project.completed && (
-                              <CheckCircle2 className="h-3 w-3 text-green-600 flex-shrink-0" />
-                            )}
-                          </div>
-                          <div className="flex items-center gap-2 mt-1">
-                            <Badge
-                              className="text-xs"
-                              variant="outline"
-                            >
-                            </Badge>
-                          </div>
+                        <div className="flex items-center gap-2 my-1">
+                          <Badge className="text-xs" variant="outline">
+                            {project.taskCount} tasks
+                          </Badge>
                         </div>
                       </div>
                     </div>
-                  </SidebarMenuButton>
+                  </Button>
                 </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
-      <SidebarRail />
-    </>
+              </Link>
+            ))}
+          </SidebarMenu>
+        </SidebarGroupContent>
+      </SidebarGroup>
+    </SidebarContent>
+  );
+};
+
+const TasksSidebarContent: FC<{ projectId: string }> = (props) => {
+  const tasks = useTasksByProject(props.projectId);
+
+  if (!tasks.data || tasks.isPending) {
+    // TODO: show spinners
+    return null;
+  }
+  // TODO: prefetch task details on hover
+
+  return (
+    <SidebarContent className="overflow-y-auto flex-1">
+      <SidebarGroup className="my-2">
+        <SidebarGroupLabel>Tasks</SidebarGroupLabel>
+        <SidebarGroupContent>
+          <SidebarMenu>
+            {tasks.data.map((task) => (
+              <SidebarMenuItem key={task.name}>
+                <SidebarMenuButton asChild>
+                  <div key={task.id}>
+                    <div
+                      className={cn(
+                        "flex items-center gap-3 p-3 rounded-lg hover:bg-accent transition-colors cursor-pointer",
+                      )}
+                    >
+                      <div className="flex-1 min-w-0">
+                        <div className="flex items-center gap-2">
+                          <p className="text-sm font-medium truncate">
+                            {task.name}
+                          </p>
+                          {task.completed && (
+                            <CheckCircle2 className="h-3 w-3 text-green-600 flex-shrink-0" />
+                          )}
+                        </div>
+                        <div className="flex items-center gap-2 mt-1">
+                          <Badge className="text-xs" variant="outline"></Badge>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroupContent>
+      </SidebarGroup>
+    </SidebarContent>
   );
 };
