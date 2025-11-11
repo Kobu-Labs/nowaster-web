04f21cdc5d75717d06bf1cf96d6e19e7fa1fcb51
Kobu <thekobu@gmail.com>
2025-11-11 23:33:42 +0100

    fix: add project to SessionPrecursor

--- next-frontend/validation/session/creation.ts
+++ next-frontend/validation/session/creation.ts
@@ -1,13 +1,17 @@
 import { CategoryWithIdSchema, TagWithIdSchema } from "@/api/definitions";
 import { z } from "zod";
 
-export const sessionPrecursor = z.object({
-  category: CategoryWithIdSchema.optional(),
-  description: z.string().nullable(),
-  endTime: z.coerce.date<Date>(),
-  session_type: z.literal("fixed"),
-  startTime: z.coerce.date<Date>(),
-  tags: z.array(TagWithIdSchema),
-});
+export const sessionPrecursor = z
+  .object({
+    category: CategoryWithIdSchema.optional(),
+    description: z.string().nullable(),
+    endTime: z.coerce.date<Date>(),
+    project: z.object({ id: z.string().uuid() }).nullish(),
+    session_type: z.literal("fixed"),
+    startTime: z.coerce.date<Date>(),
+    tags: z.array(TagWithIdSchema),
+    task: z.object({ id: z.string().uuid() }).nullish(),
+  })
+  .partial();
 
 export type SessionPrecursor = z.infer<typeof sessionPrecursor>;
